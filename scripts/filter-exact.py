2f7fff206881582d5c734db27ef23c673a0cf5d3
C. Titus Brown <titus@idyll.org>
2010-07-06 01:40:08 -0400

    added a simple exact filtering script

---
+++ scripts/filter-exact.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+import sys
+
+import khmer
+
+KSIZE=15
+HASHTABLE_SIZE=4**KSIZE
+MIN_ABUNDANCE=5
+
+infilename = sys.argv[1]
+outfilename = sys.argv[2]
+
+print 'filtering %d-mers exactly;' % KSIZE
+print 'from file %s to file %s;' % (infilename, outfilename,)
+print 'looking for sequences with at least one k-mer with count > %d' \
+      % MIN_ABUNDANCE
+
+ht = khmer.new_hashtable(KSIZE, HASHTABLE_SIZE)
+total_reads, n_consumed = ht.consume_fasta(infilename)
+
+print 'just ate %d reads, %d k-mers' % (total_reads, n_consumed)
+
+print 'filtering...'
+
+minmax = ht.fasta_file_to_minmax(infilename, total_reads)
+readmask = ht.filter_fasta_file_max(infilename, minmax, MIN_ABUNDANCE)
+
+print 'keeping %d reads' % readmask.n_kept()
+
+print 'writing...'
+readmask.filter_fasta_file(infilename, outfilename)
+
+print 'hashtable occupancy:', ht.n_occupied() / float(HASHTABLE_SIZE)
