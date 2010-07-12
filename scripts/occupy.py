a16228d6e5d98a33ad288c062395a38c463205b6
C. Titus Brown <titus@idyll.org>
2010-07-12 12:32:27 -0400

    agglom script

---
+++ scripts/occupy.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env
+import sys
+import khmer
+
+filename = sys.argv[1]
+k = int(sys.argv[2])
+
+ht = khmer.new_hashtable(k, 4**k)
+ht.consume_fasta(filename)
+
+N = ht.n_occupied()
+
+for i in range(0, 4):
+    print 'q', i, ht.n_occupied(i * 4**(k-1), (i+1) * 4**(k-1))
