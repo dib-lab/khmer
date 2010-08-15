29893af587c0779f0650ce70159becf70ddd58ed
C. Titus Brown <titus@idyll.org>
2010-08-15 02:10:35 -0400

    add options and things to do-partition-calc

--- scripts/do-partition-calc.py
+++ scripts/do-partition-calc.py
@@ -1,9 +1,25 @@
 #! /usr/bin/env python
 import khmer, sys
+from optparse import OptionParser
 
-CHECKPOINT_PERIOD=1000000
+### defaults
+
+CHECKPOINT_PERIOD=1000000               # 1m
 K=32
-HASHTABLE_SIZE=int(4**15)+1
+HASHTABLE_SIZE=15
+
+###
+
+parser = OptionParser()
+parser.add_option("-k", "--k", dest="ksize", help="k-mer size", default=K,
+                  type=int)
+parser.add_option("-s", "--size", dest="htsize4", help="hashtable size",
+                  default=HASHTABLE_SIZE, type=int)
+parser.add_option("-c", "--checkpoint", dest="checkpoint",
+                  help="checkpoint period", 
+                  default=CHECKPOINT_PERIOD, type=int)
+
+###
 
 def make_reporting_fn(ht, filename, period):
 
@@ -18,14 +34,21 @@ def make_reporting_fn(ht, filename, period):
     return _report
 
 def main():
-    infile = sys.argv[1]
-    outfile = sys.argv[2]
-    k = K
-    hashtable_size = HASHTABLE_SIZE
-    checkpoint_period = CHECKPOINT_PERIOD
+    (options, args) = parser.parse_args()
 
-    print 'making hashtable: k=%d, hashtable size=%dbn' % (k,
-                                                          hashtable_size / 1e9)
+
+    (infile, outfile) = args
+    k = options.ksize
+    hashtable_size = 4**options.htsize4 + 1
+    checkpoint_period = options.checkpoint
+
+    if k > 32:
+        raise Exception, "invalid k; must be <= 32"
+
+    ###
+
+    print 'making hashtable: k=%d, hashtable size=%.2f bn' % (k,
+                                                 hashtable_size / float(1e9))
     ht = khmer.new_hashtable(k, hashtable_size)
 
     report_fn = make_reporting_fn(ht, outfile, checkpoint_period)
