cc452556e825fd5344213fdb08dacfe6b19b284a
C. Titus Brown <titus@idyll.org>
2010-08-31 21:37:24 -0400

    fixed do-th-subset-calc script for new divvy regime

--- scripts/do-th-subset-calc.py
+++ scripts/do-th-subset-calc.py
@@ -1,6 +1,6 @@
 import khmer, sys
 import threading
-ht = khmer.new_hashtable(32, 4**15+1)
+ht = khmer.new_hashtable(32, 4**15+11)
 
 FILENAME=sys.argv[1]
 N_THREADS=8
@@ -8,31 +8,29 @@ N_THREADS=8
 (total_reads, total_kmers) = ht.consume_fasta_and_tag(FILENAME)
 print total_reads
 
-subset_size = total_reads / N_THREADS
+subset_size = total_reads / N_THREADS + total_reads % N_THREADS
 
 results = []
-def threaded_calc(ht, filename, start, stop):
-    x = ht.do_subset_partition(filename, start, stop)
+def threaded_calc(ht, start, stop):
+    x = ht.do_subset_partition(start, stop)
     print 'done!'
     results.append(x)
+
+divvy = ht.divide_tags_into_subsets(subset_size)
+n_subsets = len(divvy)
+divvy.append(0)
         
 threads = []
-
 # start things.
-for i in range(N_THREADS - 1):
-    start = subset_size * i
-    end = start + subset_size
-    t = threading.Thread(target=threaded_calc, args=(ht, FILENAME, start, end))
+for i in range(n_subsets):
+    start = divvy[i]
+    end = divvy[i+1]
+    t = threading.Thread(target=threaded_calc, args=(ht, start, end))
     threads.append(t)
     
     print 'starting', i
     t.start()
 
-start = subset_size * (N_THREADS - 1)
-end = total_reads
-t = threading.Thread(target=threaded_calc, args=(ht, FILENAME, start, end))
-threads.append(t)
-t.start()
 print 'starting', N_THREADS - 1
 
 # wait for them all to end.
@@ -47,3 +45,5 @@ for i, x in enumerate(results):
     
 n_partitions = ht.output_partitions(FILENAME, FILENAME + '.part')
 print n_partitions, 'partitions kept'
+print 'n surrendered:', ht.count_partitions()[2]
+
