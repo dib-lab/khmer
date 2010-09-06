fd1b19d1e495ec9dab4642d66bca360fe8e09776
C. Titus Brown <titus@idyll.org>
2010-09-05 22:37:02 -0400

    added some more text output for filtering

--- scripts/filter-subsets-by-partsize.py
+++ scripts/filter-subsets-by-partsize.py
@@ -30,14 +30,16 @@ def main(tagset_filename, subset_filenames):
 
     # finally, filter each subset filename and save.
     for filename in subset_filenames:
-        print 'filtering:', filename
+        print 'loading x 2', filename
         subset = ht.load_subset_partitionmap(filename)
+        print 'filtering', filename
         ht.subset_filter_against_tags(subset, tagmap)
 
         dir = os.path.dirname(filename)
         new_filename = 'filtered_' + os.path.basename(filename)
         new_filename = os.path.join(dir, new_filename)
 
+        print 'saving', new_filename
         ht.save_subset_partitionmap(subset, new_filename)
 
         del subset
