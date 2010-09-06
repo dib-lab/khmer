5beb410442688c476682586b03fa3314670b8eee
C. Titus Brown <titus@idyll.org>
2010-09-05 22:30:10 -0400

    cleanup of filter script

--- scripts/filter-subsets-by-partsize.py
+++ scripts/filter-subsets-by-partsize.py
@@ -2,34 +2,48 @@
 import sys
 import khmer
 import gc
+import os
 
-MIN_PARTITION_SIZE = 3
+MIN_PARTITION_SIZE = 3          # 3 is the smallest reasonable threshold
 K = 32
 
-tagset_filename = sys.argv[1]
-subset_filenames = sys.argv[2:]
+def main(tagset_filename, subset_filenames):
+    print 'K', K
+    print 'MIN SIZE', MIN_PARTITION_SIZE
+    print '--'
 
-# 
-ht = khmer.new_hashtable(32, 1)
-tagmap = ht.load_tagmap(tagset_filename)
+    # create an empty hashtable & load in the tags
+    ht = khmer.new_hashtable(32, 1)
+    tagmap = ht.load_tagmap(tagset_filename)
 
-for filename in subset_filenames:
-    print 'maxifying:', filename
-    subset = ht.load_subset_partitionmap(filename)
-    ht.subset_maxify_partition_size(subset, tagmap)
-    del subset
-    gc.collect()
+    # find the maximum partition size for each tag, across all subsets
+    for filename in subset_filenames:
+        print 'maxifying:', filename
+        subset = ht.load_subset_partitionmap(filename)
+        ht.subset_maxify_partition_size(subset, tagmap)
+        del subset
+        gc.collect()
 
-print 'discarding'
-ht.discard_tags(tagmap, MIN_PARTITION_SIZE)
+    # filter tags based on the max partition size to which they belong
+    print 'discarding'
+    ht.discard_tags(tagmap, MIN_PARTITION_SIZE)
 
-for filename in subset_filenames:
-    print 'filtering:', filename
-    subset = ht.load_subset_partitionmap(filename)
-    ht.subset_filter_against_tags(subset, tagmap)
-    
-    new_filename = 'filtered_' + filename[:-5]
-    ht.save_subset_partitionmap(subset, new_filename + '.pmap')
+    # finally, filter each subset filename and save.
+    for filename in subset_filenames:
+        print 'filtering:', filename
+        subset = ht.load_subset_partitionmap(filename)
+        ht.subset_filter_against_tags(subset, tagmap)
 
-    del subset
-    gc.collect()
+        dir = os.path.dirname(filename)
+        new_filename = 'filtered_' + os.path.basename(filename)
+        new_filename = os.path.join(dir, new_filename)
+
+        ht.save_subset_partitionmap(subset, new_filename)
+
+        del subset
+        gc.collect()
+
+if __name__ == '__main__':
+    tagset_filename = sys.argv[1]
+    subset_filenames = sys.argv[2:]
+    main(tagset_filename, subset_filenames)
