8031b877678b753e7cb5ed002c6439fcafc02432
C. Titus Brown <titus@idyll.org>
2010-09-13 23:45:39 -0400

    removed broken graph-trim.py script

--- scripts/graph-size-py.py
+++ scripts/graph-size-py.py
@@ -10,7 +10,7 @@ threshold = int(sys.argv[2])
 outfile = sys.argv[3]
 
 print 'creating ht'
-ht = khmer.new_hashtable(K, HASHTABLE_SIZE)
+ht = khmer.new_hashbits(K, HASHTABLE_SIZE)
 print 'eating fa', infile
 total_reads, n_consumed = ht.consume_fasta(infile)
 outfp = open(outfile, 'w')
@@ -22,15 +22,3 @@ for record in screed.fasta.fasta_iter(open(infile)):
     size = ht.calc_connected_graph_size(kmer, threshold)
     if size >= threshold:
         print >>outfp, ">%s\n%s" % (record['name'], record['sequence'])
-
-#print 'trimming to', threshold
-#ht.trim_graphs(infile, threshold, outfile)
-
-#print 'hashtable occupancy:', ht.n_occupied() / float(HASHTABLE_SIZE)
-
-#fp = open('aaa.2', 'w')
-#total = 0
-#for n, i in enumerate(ht.graphsize_distribution(5000)):
-#    total += i
-#    print >>fp, n, i, total
-#fp.close()
