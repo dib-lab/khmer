386b98e280a78b9b704909acdcc128074609ce1e
C. Titus Brown <titus@idyll.org>
2010-09-13 22:38:17 -0400

    moved multiple ht functionality into hashbits

--- lib/hashbits.hh
+++ lib/hashbits.hh
@@ -6,7 +6,7 @@
 namespace khmer {
   class Hashbits : public Hashtable {
   protected:
-    // BoundedCounterType * _counts[N_TABLES];
+    BoundedCounterType * _counts[N_TABLES];
 
     virtual void _allocate_counters() {
       for (unsigned int i = 0; i < N_TABLES; i++) {
@@ -29,6 +29,76 @@ namespace khmer {
 	}
       }
       _clear_partitions();
+    }
+
+    virtual void save(std::string);
+    virtual void load(std::string);
+
+    // count number of occupied bins
+    virtual const HashIntoType n_occupied(HashIntoType start=0,
+				  HashIntoType stop=0) const {
+      HashIntoType n = 0;
+      if (stop == 0) { stop = _tablesize; }
+      for (HashIntoType i = start; i < stop; i++) {
+	unsigned int byte = i / 8;
+	unsigned char bit = i % 8;
+	if (_counts[0][byte] & (1 << bit)) {
+	  n++;
+	}
+      }
+      return n;
+    }
+
+    virtual void count(const char * kmer) {
+      HashIntoType hash = _hash(kmer, _ksize);
+
+      for (unsigned int i = 0; i < N_TABLES; i++) {
+	HashIntoType bin = hash % primes[i];
+	unsigned int byte = bin / 8;
+	unsigned char bit = bin % 8;
+
+	_counts[i][byte] |= (1 << bit);
+      }
+    }
+
+    virtual void count(HashIntoType khash) {
+      for (unsigned int i = 0; i < N_TABLES; i++) {
+	HashIntoType bin = khash % primes[i];
+	unsigned int byte = bin / 8;
+	unsigned char bit = bin % 8;
+
+	_counts[i][byte] |= (1 << bit);
+      }
+    }
+
+    // get the count for the given k-mer.
+    virtual const BoundedCounterType get_count(const char * kmer) const {
+      HashIntoType hash = _hash(kmer, _ksize);
+
+      for (unsigned int i = 0; i < N_TABLES; i++) {
+	HashIntoType bin = hash % primes[i];
+	unsigned int byte = bin / 8;
+	unsigned char bit = bin % 8;
+      
+	if (!(_counts[i][byte] & (1 << bit))) {
+	  return 0;
+	}
+      }
+      return 1;
+    }
+
+    // get the count for the given k-mer hash.
+    virtual const BoundedCounterType get_count(HashIntoType khash) const {
+      for (unsigned int i = 0; i < N_TABLES; i++) {
+	HashIntoType bin = khash % primes[i];
+	unsigned int byte = bin / 8;
+	unsigned char bit = bin % 8;
+      
+	if (!(_counts[i][byte] & (1 << bit))) {
+	  return 0;
+	}
+      }
+      return 1;
     }
   };
 
