d5f5101169fe355a27bc63838b3bc05a80bc7eba
C. Titus Brown <titus@idyll.org>
2010-09-20 00:31:19 -0400

    added paired-end combine and abyss running scripts

---
+++ scripts/combine-pe.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+import sys
+import khmer
+from screed.fasta import fasta_iter
+
+K = 32
+
+###
+
+def get_partition(record):
+    pid = record['name'].rsplit('\t', 1)[1]
+    return int(pid)
+
+###
+
+ht = khmer.new_hashbits(K, 1, 1)
+
+ht.consume_partitioned_fasta(sys.argv[1])
+before = ht.count_partitions()
+
+last_name = None
+last_record = None
+for n, record in enumerate(fasta_iter(open(sys.argv[1]), parse_description=False)):
+    if n % 10000 == 0:
+        print '...', n
+        
+    name = record['name'].split()[0]
+    name = name.split('/', 1)[0]
+
+    if name == last_name:
+        if 1:
+            pid_1 = ht.get_partition_id(last_record['sequence'][:K])
+            pid_2 = ht.get_partition_id(record['sequence'][:K])
+
+            ht.join_partitions(pid_1, pid_2)
+        else:                           # TEST
+            pid_1 = get_partition(last_record)
+            pid_2 = get_partition(record)
+            assert pid_1 == pid_2, (last_record, record, pid_1, pid_2)
+
+    last_name = name
+    last_record = record
+
+ht.output_partitions(sys.argv[1], sys.argv[1] + '.paired')
+print 'before:', before
+after = ht.count_partitions()
+print 'after:', after
+
+n_combined = before[0] - after[0]
+print 'combined:', n_combined
---
+++ scripts/multi-abyss.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+import sys, time
+import subprocess
+
+COMMAND="ABYSS -k45 %(filename)s -o %(filename)s.k41"
+
+N_PROCESSES = 4
+
+filenames = sys.argv[1:]
+
+running = []
+
+while 1:
+    while len(running) < N_PROCESSES:
+        filename = filenames.pop(0)
+        cmd = COMMAND % dict(filename=filename)
+        p = subprocess.Popen(cmd, shell=True)
+        print 'running:', cmd
+        running.append(p)
+
+    i = 0;
+    while i < len(running):
+        p = running[i]
+        returncode = p.poll()
+        if returncode is not None:
+            print 'done! removing', p
+            running.remove(p)
+        else:
+            i += 1
+
+    time.sleep(1)
+
+print '\n\n** done **\n'
