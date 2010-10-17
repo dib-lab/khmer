c52c81b9158315e6f09ed2c9d7d0a60b23ccc2f5
Jason Pell <jason.pell@gmail.com>
2010-10-16 20:55:01 -0400

    added temp change to get-occupied.py

--- scripts/get-occupancy.py
+++ scripts/get-occupancy.py
@@ -1,10 +1,27 @@
 import khmer
 import sys
 import screed
+import os
+import subprocess
+import glob
 
 K = 32
-#HASHTABLE_SIZE=int(256e9)
+#HASHTABLE_SIZE=int(512e9)
 HASHTABLE_SIZE = 1000000000
+
+def gzip(filename):
+   gzip_cmd = "gzip " + filename
+
+   proc = subprocess.Popen(gzip_cmd, shell=True)
+   proc.wait()
+
+def gunzip(filename):
+   gunzip_cmd = "gunzip " + filename
+
+   proc = subprocess.Popen(gunzip_cmd, shell=True)
+   proc.wait()
+
+   return filename.strip(".gz")
 
 print 'creating ht with size ' + str(HASHTABLE_SIZE)
 ht = khmer.new_hashbits(K, HASHTABLE_SIZE, 1)
@@ -13,7 +30,11 @@ read_count = 0
 
 fd = open(sys.argv[1], "w")
 
-for filename in sys.argv[2:]:
+files = glob.glob(sys.argv[2])
+
+for gzfilename in files:
+   filename = gunzip(gzfilename)
+
    print "processing file: " + filename + " reads processed: " + str(read_count)
  
    for n, record in enumerate(screed.fastq.fastq_iter(open(filename))):
@@ -23,6 +44,8 @@ for filename in sys.argv[2:]:
       if read_count % 1000000 == 0:
          fd.write(str(read_count) + " " + str(ht.n_occupied()) + "\n")
          fd.flush()
+
+   gzip(filename)
 
 fd.write(str(read_count) + " " + str(ht.n_occupied()) + "\n")
 fd.close()
