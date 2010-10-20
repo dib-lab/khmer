a14ff5a0263e1b38967096c9eb2c67c6687f8bc3
Qingpeng <qingpeng@gmail.com>
2010-10-20 01:10:03 -0400

    bloom_count.py added

---
+++ scripts/bloom_count.py
@@ -0,0 +1,26 @@
+## using bloom filter to count unique kmers
+
+import khmer
+import sys
+import screed
+from screed.fasta import fasta_iter
+
+filename = sys.argv[1]
+K = int(sys.argv[2]) # size of kmer
+HT_SIZE= int(sys.argv[3])# size of hashtable
+N_HT = int(sys.argv[4]) # number of hashtables
+
+ht = khmer.new_hashbits(K, HT_SIZE, N_HT)
+
+n_unique = 0
+for n, record in enumerate(fasta_iter(open(filename))):
+    sequence = record['sequence']
+    seq_len = len(sequence)
+    for n in range(0,seq_len+1-K):
+        kmer = sequence[n:n+K]
+        if (not ht.get(kmer)):
+            n_unique+=1
+        ht.count(kmer)
+        
+print n_unique
+print ht.n_occupied()
