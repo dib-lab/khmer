5df4f75c7f10c450137ee9c18520d8d9b6631b24
Adina <howead@msu.edu>
2010-10-19 22:59:09 -0400

    fixed some errors

--- scripts/graph-size-py.py
+++ scripts/graph-size-py.py
@@ -3,30 +3,29 @@ import sys
 import screed
 import os
 import subprocess
+import zlib
 import gzip
 
-K = 32
+K = 30
 HASHTABLE_SIZE=4**17+1
 THRESHOLD=100
 
-print '...', HASHTABLE_SIZE
+print 'ht size'
 ht = khmer.new_hashbits(K, HASHTABLE_SIZE, 1)
 
 read_count = 0
 
 fd = open(sys.argv[1], 'w')
 
-files = sys.argv[2:]
+for filename in sys.argv[2:]:
 
-for filename in files:
-
-    
     print 'processing file: ' + filename + ' reads processed: ' + str(read_count)
 
     for n, record in enumerate(screed.fastq.fastq_iter(gzip.open(filename))):
+        print n
         read_count += 1
         ht.consume(record['sequence'])
-
+    
         if read_count % 100000 == 0:
             fd.write(str(read_count) + " " + str(ht.n_occupied()) + " " + str(ht.n_occupied() / float(HASHTABLE_SIZE)) +'\n')
             fd.flush()
@@ -35,6 +34,7 @@ for filename in sys.argv[2:]:
     outfp = open(filename[:-3] + '.graphsize', 'w')
     
     n_kept = 0
+
     for n, record in enumerate(screed.fastq.fastq_iter(gzip.open(filename))):
         kmer = record['sequence'][:K]
         size = ht.calc_connected_graph_size(kmer, THRESHOLD)
