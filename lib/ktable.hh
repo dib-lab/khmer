3e17787ca9f1b90d9369516a61efea282aba33e3
C. Titus Brown <titus@idyll.org>
2010-11-12 18:04:49 -0500

    made _next_hash function inline in Hashtable

--- lib/ktable.hh
+++ lib/ktable.hh
@@ -35,7 +35,7 @@ namespace khmer {
   // two-way hash functions.
   HashIntoType _hash(const char * kmer, const WordLength k);
   HashIntoType _hash(const char * kmer, const WordLength k,
-			      HashIntoType& h, HashIntoType& r);
+		     HashIntoType& h, HashIntoType& r);
   HashIntoType _hash_forward(const char * kmer, WordLength k);
 
   std::string _revhash(HashIntoType hash, WordLength k);
