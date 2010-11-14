ae45fc29bf5e5c01f45737cabf2cd386e2eb214e
C. Titus Brown <titus@idyll.org>
2010-11-13 22:39:11 -0500

    added a test for 3 tables; cleaned up other tests

--- tests/test_counting_hash.py
+++ tests/test_counting_hash.py
@@ -16,39 +16,21 @@ PRIMES_8b = [8000000011, 8000000051]
 
 DNA = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
 
-class Test_HashtableIntersect(object):
+class Test_CountingHash(object):
     def setup(self):
         self.hi = khmer._new_counting_hash(12, PRIMES_1m)
-
-    def test_basic(self):
-        hi = self.hi
-        hi.consume(DNA)
-
-        x = hi.get_min_count(DNA)
-        assert x == 1, x
-
-        x = hi.get_max_count(DNA)
-        assert x == 1, x
-
-        hi.consume('ATTCTGACTGCA')
-
-        x = hi.get_min_count(DNA)
-        assert x == 1, x
-
-        x = hi.get_max_count(DNA)
-        assert x == 2, x
 
     def test_collision_1(self):
         kt = khmer.new_ktable(12)
         
         GG = 'G' * 12                   # forward_hash: 11184810
-        assert kt.forward_hash(GG) == 11184810
+        assert khmer.forward_hash(GG, 12) == 11184810
 
-        collision_1 = 'AAACGTATGACT'      # forward_hash: 184777L
-        assert kt.forward_hash(collision_1) == 184777L
+        collision_1 = 'AAACGTATGACT'
+        assert khmer.forward_hash(collision_1, 12) == 184777L
 
-        collision_2 = 'AAATACCGAGCG'      # forward_hash: 76603L
-        assert kt.forward_hash(collision_2) == 76603L
+        collision_2 = 'AAATACCGAGCG'
+        assert khmer.forward_hash(collision_2, 12) == 76603L
 
         # note, hash(GG) % 1000003 == hash(collision_1)
         # note, hash(GG) % 1009837 == hash(collision_2)
@@ -57,50 +39,80 @@ class Test_HashtableIntersect(object):
         hi.consume(GG)
         hi.consume(collision_1)
 
-        assert hi.get_min_count(GG) == 1
-        assert hi.get_max_count(GG) == 1
+        assert hi.get(GG) == 1
 
     def test_collision_2(self):
         kt = khmer.new_ktable(12)
         
         GG = 'G' * 12                   # forward_hash: 11184810
-        assert kt.forward_hash(GG) == 11184810
+        assert khmer.forward_hash(GG, 12) == 11184810
 
-        collision_1 = 'AAACGTATGACT'      # forward_hash: 184777L
-        assert kt.forward_hash(collision_1) == 184777L
+        collision_1 = 'AAACGTATGACT'
+        assert khmer.forward_hash(collision_1, 12) == 184777L
 
-        collision_2 = 'AAATACCGAGCG'      # forward_hash: 76603L
-        assert kt.forward_hash(collision_2) == 76603L
+        collision_2 = 'AAATACCGAGCG'
+        assert khmer.forward_hash(collision_2, 12) == 76603L
 
-        # note, hash(GG) % 1000003 == hash(collision_1)
-        # note, hash(GG) % 1009837 == hash(collision_2)
+        # hash(GG) % 1000003 == hash(collision_1)
+        # hash(GG) % 1009837 == hash(collision_2)
 
         hi = self.hi
         hi.consume(GG)
         hi.consume(collision_2)
 
-        assert hi.get_min_count(GG) == 1
-        assert hi.get_max_count(GG) == 1
+        assert hi.get(GG) == 1
 
     def test_collision_3(self):
         kt = khmer.new_ktable(12)
         
         GG = 'G' * 12                   # forward_hash: 11184810
-        assert kt.forward_hash(GG) == 11184810
+        assert khmer.forward_hash(GG, 12) == 11184810
 
-        collision_1 = 'AAACGTATGACT'      # forward_hash: 184777L
-        assert kt.forward_hash(collision_1) == 184777L
+        collision_1 = 'AAACGTATGACT'
+        assert khmer.forward_hash(collision_1, 12) == 184777L
 
-        collision_2 = 'AAATACCGAGCG'      # forward_hash: 76603L
-        assert kt.forward_hash(collision_2) == 76603L
+        collision_2 = 'AAATACCGAGCG'
+        assert khmer.forward_hash(collision_2, 12) == 76603L
 
-        # note, hash(GG) % 1000003 == hash(collision_1)
-        # note, hash(GG) % 1009837 == hash(collision_2)
+        # hash(GG) % 1000003 == hash(collision_1)
+        # hash(GG) % 1009837 == hash(collision_2)
 
         hi = self.hi
         hi.consume(GG)
         hi.consume(collision_1)
         hi.consume(collision_2)
 
-        assert hi.get_min_count(GG) == 2
-        assert hi.get_max_count(GG) == 2
+        assert hi.get(GG) == 2
+
+def test_3_tables():
+    x = list(PRIMES_1m)
+    x.append(1000005)
+    
+    hi = khmer._new_counting_hash(12, x)
+      
+    GG = 'G' * 12                   # forward_hash: 11184810
+    assert khmer.forward_hash(GG, 12) == 11184810
+
+    collision_1 = 'AAACGTATGACT'
+    assert khmer.forward_hash(collision_1, 12) == 184777L
+
+    collision_2 = 'AAATACCGAGCG'
+    assert khmer.forward_hash(collision_2, 12) == 76603L
+
+    collision_3 = 'AAACGTATCGAG'
+    assert khmer.forward_hash(collision_3, 12) == 184755L
+
+    # hash(GG) % 1000003 == hash(collision_1)
+    # hash(GG) % 1009837 == hash(collision_2)
+    # hash(GG) % 1000005 == hash(collision_3)
+    hi.consume(GG)
+    assert hi.get(GG) == 1
+    
+    hi.consume(collision_1)
+    assert hi.get(GG) == 1
+    
+    hi.consume(collision_2)
+    assert hi.get(GG) == 1
+    
+    hi.consume(collision_3)
+    assert hi.get(GG) == 2
