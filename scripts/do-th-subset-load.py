3c5f5f8dcf399cbe64b17677d5af72550dc120fd
C. Titus Brown <titus@idyll.org>
2010-11-22 04:57:37 +0000

    simplified merging for preferential atachment/big blog situation

--- scripts/do-th-subset-load.py
+++ scripts/do-th-subset-load.py
@@ -7,17 +7,13 @@ K = 32
 filename=sys.argv[1]
 subset_filenames=sys.argv[2:]
 
-def load(filename, ht):
-    print 'loading', filename
-    subset = ht.merge_subset_from_disk(filename)
-
 # create a fake-ish ht; K matters, but not hashtable size.
 ht = khmer.new_hashbits(32, 1, 1)
-
+ 
 # load & merge
 for subset_file in subset_filenames:
     print '<-', subset_file
-    load(subset_file, ht)
+    ht.merge_subset_from_disk(subset_file)
 
 # partition!
 n_partitions = ht.output_partitions(filename, filename + '.part')
