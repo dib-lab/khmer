a35811c6c5edd30c2cb4674030f276e075d8d6ad
C. Titus Brown <titus@idyll.org>
2010-11-30 10:04:49 -0500

    fixed '>' prefix issue

--- scripts/strip-and-split-for-assembly.py
+++ scripts/strip-and-split-for-assembly.py
@@ -16,8 +16,8 @@ for record in screed.fasta.fasta_iter(open(sys.argv[1]), parse_description=False
     fp = single_fp
     if name.endswith('2') and last_record and name[:-1] == last_name[:-1]:
         fp = paired_fp
-        print >>paired_fp, '%s\n%s' % (last_name, last_record['sequence'])
+        print >>paired_fp, '>%s\n%s' % (last_name, last_record['sequence'])
     
-    print >>fp, '%s\n%s' % (name, sequence,)
+    print >>fp, '>%s\n%s' % (name, sequence,)
     last_name = name
     last_record = record
