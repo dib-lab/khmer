113549a926be071cc5c788cd689b075ea417a485
C. Titus Brown <titus@idyll.org>
2011-01-06 15:11:08 -0500

    updated script to re-output sequences with annots

--- scripts/count-within-radius.py
+++ scripts/count-within-radius.py
@@ -1,15 +1,26 @@
-import sys, screed.fasta
+import sys, screed.fasta, os
+import khmer
 
 K = 32
 HASHTABLE_SIZE=int(8e9)
 N_HT = 4
 THRESHOLD=100
 
-import khmer
+infile = sys.argv[1]
+outfile = sys.argv[2]
+print 'saving to:', outfile
+
+print 'making hashtable'
 ht = khmer.new_hashbits(K, HASHTABLE_SIZE, N_HT)
 
-ht.consume_fasta(sys.argv[1])
+print 'eating', infile
+ht.consume_fasta(infile)
 
-for record in screed.fasta.fasta_iter(open(sys.argv[1])):
+outfp = open(outfile, 'w')
+for n, record in enumerate(screed.fasta.fasta_iter(open(infile))):
+    if n % 10000 == 0:
+        print '... saving', n
     seq = record['sequence']
-    print ht.count_kmers_within_radius(seq[:K], THRESHOLD, 20000)
+    
+    area = ht.count_kmers_within_radius(seq[:K], THRESHOLD, 2000)
+    print >>outfp, '>%s a=%d\n%s' % (record['name'], area, record['sequence'])
