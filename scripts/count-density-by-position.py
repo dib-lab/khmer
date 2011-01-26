686b311a6c5e05c7eda82712db8b04b28bb90fa0
C. Titus Brown <titus@idyll.org>
2011-01-26 01:17:46 -0500

    added new scripts for density and degree counting

---
+++ scripts/count-density-by-position.py
@@ -0,0 +1,43 @@
+import sys, screed.fasta, os
+import khmer
+
+K = 32
+HASHTABLE_SIZE=int(8e9)
+N_HT = 4
+
+###
+
+MAX_DENSITY=2000
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+outfp = open(outfile, 'w')
+    
+print 'saving to:', outfile
+
+print 'making hashtable'
+ht = khmer.new_hashbits(K, HASHTABLE_SIZE, N_HT)
+
+print 'eating', infile
+ht.consume_fasta(infile)
+
+RADIUS=10
+hist = [0.0] * 200
+histcount = [0] * 200
+
+for n, record in enumerate(screed.fasta.fasta_iter(open(infile))):
+    if n % 10000 == 0:
+        print '... saving', n
+    seq = record['sequence']
+
+    for pos in range(0, len(seq) - K + 1):
+        density = ht.count_kmers_within_radius(seq[pos:pos+K], RADIUS,
+                                               MAX_DENSITY)
+        density /= float(RADIUS)
+        hist[pos] += density
+        histcount[pos] += 1
+
+
+for i in range(len(hist)):
+    if histcount[i]:
+        print >>outfp, i, hist[i], histcount[i], hist[i]/histcount[i]
---
+++ scripts/count-distance-to-volume.py
@@ -0,0 +1,36 @@
+import sys, screed.fasta, os
+import khmer
+
+K = 32
+HASHTABLE_SIZE=int(8e9)
+N_HT = 4
+
+MAX_RADIUS=500
+VOLUME=500
+
+###
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+if len(sys.argv) > 3:
+    RADIUS=int(sys.argv[3])
+    
+print 'saving to:', outfile
+
+print 'making hashtable'
+ht = khmer.new_hashbits(K, HASHTABLE_SIZE, N_HT)
+
+print 'eating', infile
+ht.consume_fasta(infile)
+
+outfp = open(outfile, 'w')
+for n, record in enumerate(screed.fasta.fasta_iter(open(infile))):
+    if n % 10000 == 0:
+        print '... saving', n
+    seq = record['sequence']
+
+    middle = (len(seq) - K + 1) / 2
+
+    r = ht.find_radius_for_volume(seq[middle:middle+K], VOLUME, MAX_RADIUS)
+    
+    print >>outfp, '>%s r=%d\n%s' % (record['name'], r, record['sequence'])
