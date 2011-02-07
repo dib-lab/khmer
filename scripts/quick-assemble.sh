924e13f5dd99a9f8ae7ab92f5281b1abc497805f
C. Titus Brown <titus@idyll.org>
2011-02-07 01:43:32 -0500

    updated with K setting

--- scripts/quick-assemble.sh
+++ scripts/quick-assemble.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
-python ~/dev/khmer/scripts/strip-partition.py $1 | velveth $1.ass 33 -short -
-velvetg $1.ass
+K=33
+if [ "$2" ]; then
+   K=$2
+fi
+echo assembling $1 with K = $K
+
+python ~/dev/khmer/scripts/strip-partition.py $1 | velveth $1.ass.$K $K -short -
+velvetg $1.ass.$K
