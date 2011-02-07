2ccd6ba8c713dd5666eff94704cdff9bd976d170
C. Titus Brown <titus@idyll.org>
2011-02-07 00:12:41 -0500

    a bit of refactoring to fix truncation issue

--- scripts/split-fasta-on-circum.py
+++ scripts/split-fasta-on-circum.py
@@ -48,7 +48,7 @@ for n, record in enumerate(screed.fasta.fasta_iter(open(infile),
 
     is_high = False
     circums = []
-    for pos in range(end, start, -incr):
+    for pos in range(start, end):
         circum = ht.count_kmers_on_radius(seq[pos:pos+K], RADIUS, MAX_VOLUME)
 
         circums.append((circum, pos))
@@ -59,31 +59,32 @@ for n, record in enumerate(screed.fasta.fasta_iter(open(infile),
 
     # did we find a circumference >= our max?  If so, try to trim the reads.
     if is_high:
-        # trim from the back:
-        for (circum, pos) in circums:
-            if circum < MAX_CIRCUM:
-                break
-
-        chop_end = pos
-
         # trim from the front:
-        circums.reverse()
-        for (circum, pos) in circums:
+        for i in range(len(circums)):
+            circum, pos = circums[i]
             if circum < MAX_CIRCUM:
                 break
         chop_start = pos
 
+        # trim from the back:
+        for j in range(len(circums) - 1, 0, -1):
+            circum, pos = circums[j]
+            if circum < MAX_CIRCUM:
+                break
+        chop_end = pos
+
+        trimmed_circums = circums[i:j+1]
+
         # make sure we're not missing anything in the middle that wasn't
         # trimmed on either side:
-        if max([ circ for (circ,_) in circums ]) < MAX_CIRCUM and \
-               chop_end - chop_start >= 0:
-
-            # do the trimming & rewrite the name
+        if trimmed_circums and \
+               max([ circ for (circ,_) in trimmed_circums ]) < MAX_CIRCUM:
+            # do the trimming & note in the name
             sequence = record['sequence'][chop_start:chop_end + K]
             record['name'] = record['name'] + '\tTRUNC:%d-%d' % (chop_start,
                                                                  chop_end + K)
 
-            # clear the 'is_high' flag if there are no more high-circum
+            # clear the 'is_high' flag, since there are no more high-circum
             # k-mers left.
             is_high = False
 
@@ -94,3 +95,8 @@ for n, record in enumerate(screed.fasta.fasta_iter(open(infile),
         fp = lowfp
         
     print >>fp, '>%s\n%s' % (record['name'], record['sequence'])
+#    for circum, pos in circums:
+#        print >>fp, circum,
+#    print >>fp, ''
+#    if is_high:
+#        print >>fp, chop_start, chop_end
