e946a33ae0788276a2b411c35d6939d7ecd8e0ce
C. Titus Brown <titus@idyll.org>
2011-02-09 01:26:00 -0500

    do not regenerate .se and .pe files if they exist

--- scripts/velvet-assemble.sh
+++ scripts/velvet-assemble.sh
@@ -4,7 +4,10 @@ filename=$2
 K=$3
 
 BASE=`basename $filename`
-python $scriptpath/strip-and-split-for-assembly.py $filename `basename $filename`
+
+if [ \! -f $BASE.se -o \! -f $BASE.pe \]; then
+   python $scriptpath/strip-and-split-for-assembly.py $filename $BASE
+fi
 
 velveth $BASE.ass.$K $K -fasta -short ${BASE}.se -shortPaired ${BASE}.pe && \
 velvetg $BASE.ass.$K -read_trkg yes -exp_cov 3 -cov_cutoff 0
