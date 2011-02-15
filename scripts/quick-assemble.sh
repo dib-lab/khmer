da281f52cbdc3d8aefdbdf2c4ad8d8f95b062283
C. Titus Brown <titus@idyll.org>
2011-02-14 23:09:13 -0500

    remove PE foo

--- scripts/quick-assemble.sh
+++ scripts/quick-assemble.sh
@@ -6,4 +6,4 @@ fi
 echo assembling $1 with K = $K
 
 python ~/dev/khmer/scripts/strip-partition.py $1 | velveth $1.ass.$K $K -short -
-velvetg $1.ass.$K -exp_cov auto -read_trkg yes -cov_cutoff 0
+velvetg $1.ass.$K -read_trkg yes
