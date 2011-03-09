004dbc1feeb0512f28d51a7e23ff8a83fe81a619
C. Titus Brown <titus@idyll.org>
2011-03-09 16:53:25 -0500

    reformatted MSB2 file appropriately, + script

---
+++ scripts/reformat.py
@@ -0,0 +1,9 @@
+import sys
+import screed
+
+for record in screed.open(sys.argv[1], parse_description=False):
+    name, descr = record.name.split(" ", 1)
+    name = name.lstrip('>')
+    name += "\t".join(descr.split())
+    
+    print '>%s\n%s' % (name, record.sequence,)
