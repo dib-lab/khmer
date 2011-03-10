1b82c0568c6f4f547e498464e83af09ae30e103a
C. Titus Brown <titus@idyll.org>
2011-03-10 12:39:16 -0500

    fix for problem where all_tags is actually empty in do-partition.py output_partitions; use partition_map instead

--- lib/hashbits.cc
+++ lib/hashbits.cc
@@ -273,7 +273,6 @@ void Hashbits::consume_fasta_and_tag(const std::string &filename,
       HashIntoType kmer = _hash(first_kmer, _ksize, kmer_f, kmer_r);
 
       unsigned int since = _tag_density / 2 + 1;
-      // std::cout << "starting since at: " << since << "\n";
       for (unsigned int i = _ksize; i < seq.length(); i++) {
 
 	is_new_kmer = (bool) !get_count(kmer);
@@ -283,14 +282,12 @@ void Hashbits::consume_fasta_and_tag(const std::string &filename,
 	}
 
 	if (!is_new_kmer && all_tags.find(kmer) != all_tags.end()) {
-	  // std::cout << "RESETTING SINCE\n";
 	  since = 1;
 	} else {
 	  since++;
 	}
 
 	if (since >= _tag_density) {
-	  // std::cout << "SETTING SINCE\n";
 	  all_tags.insert(kmer);
 	  since = 1;
 	}
@@ -304,24 +301,15 @@ void Hashbits::consume_fasta_and_tag(const std::string &filename,
 	n_consumed++;
       }
 
-      if (since >= _tag_density/2 - 1 || 1) {
+      if (since >= _tag_density/2 - 1) {
 	all_tags.insert(kmer);	// insert the last k-mer, too.
-	// std::cout << "inserting last: " << kmer << "\n";
-      } else {
-	// std::cout << "since: " << since << " vs " << _tag_density/2 - 1 << "\n";
       }
 
-#if 0
-      std::cout << read.name << "; size " << seq.length() << " - ";
       for (unsigned int i = 0; i < seq.length() - _ksize + 1; i++) {
 	kmer = _hash(first_kmer + i, _ksize);
 	if (all_tags.find(kmer) != all_tags.end()) {
-	  std::cout << i << ", ";
 	}
       }
-      std::cout << "\n";
-      std::cout << "LAST KMER is: " << kmer << "\n";
-#endif // 1
       
     }
 	       
