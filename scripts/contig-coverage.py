362078159f9390393f88847a43811e2af4592d29
Adina <howead@msu.edu>
2011-05-09 14:59:10 -0400

    added testing to contig-coverage.py

--- scripts/contig-coverage.py
+++ scripts/contig-coverage.py
@@ -1,74 +1,81 @@
 import sys
 import khmer
 import screed
+import os
 from screed.fasta import fasta_iter
 
 K=32
-HASHTABLE_SIZE = 1e9
+HASHTABLE_SIZE = 1e6
 N_HT = 4
 
-'''Usage contig.coverage.py <output file> <reference> <reads:>'''
+DATA_DIR='/u/adina/test-reads/'
+
+thisdir = os.path.dirname(__file__)
+thisdir = os.path.abspath(thisdir)
+
+datadir = os.path.join(thisdir, DATA_DIR)
+datadir = os.path.abspath(datadir)
 
 def slidingWindow(sequence, K_size):
-    try:
-        check = iter(sequence)
-    except TypeError:
-        raise Exception('ERROR:  Sequence iteration fail.')
-
-    # Total slides needed to consume sequence
     total_windows = (len(sequence) - K_size) + 1
 
     for i in range(0, total_windows, 1):
         yield sequence[i:i+K_size]
 
 
-if '__name__==__main__':
-
-    fp = open(sys.argv[1], 'w')
-    contig_file = sys.argv[2]
-    reads_file = sys.argv[3:]
+def main(contig_filename, read_filenames_list, output_filename):
 
     ht = khmer.new_counting_hash(K, HASHTABLE_SIZE, N_HT)
 
     '''consumes contig into hashtable'''
-    print '...consuming contig reads from %s' % contig_file
-    for n, record in enumerate(fasta_iter(open(contig_file))):
+    for n, record in enumerate(fasta_iter(open(contig_filename))):
         sequence = record['sequence']
         contig_kmers = slidingWindow(sequence, K)
         for x in contig_kmers:
-            if x.find('N') > -1:
+            if x.find('N') > 0:
                 continue
             else:
                 ht.consume(x)
     
     '''counts reads into hashtable abundance'''
-    for each_file in reads_file:
+    for each_file in read_filenames_list:
         read_file = open(each_file, 'r')
-        print '...counting in read file from %s' % each_file
         for n1, record1 in enumerate(fasta_iter(read_file)):
             sequence = record1['sequence']
             read_kmers = slidingWindow(sequence, K)
             for kmer in read_kmers:
-                if ht.get(kmer) > 0:
+                if ht.get(kmer) > 0:                    
                     ht.count(kmer)
         read_file.close()
         
     '''retrieve abundances'''
-    d = {}    
-    print 'writing output of abundances to %s' % sys.argv[1]
-    for n2, record2 in enumerate(fasta_iter(open(contig_file))):
+    for n2, record2 in enumerate(fasta_iter(open(contig_filename))):
         contig_seq = record2['sequence']
-        id = record2['name']
-        count_list_forward = []
+        count_list = []
         contig_kmers = slidingWindow(contig_seq, K)
         for contig_kmer in contig_kmers:
-            if contig_kmer.find('N') > -1:
-                count_list_forward.append(0)
-                continue
-            else:
-                count_kmer = int(ht.get(contig_kmer)) - 1
-                count_list_forward.append(count_kmer)
-        for item in count_list_forward:
-            print >>fp, '%s' % item
+            count_kmer = int(ht.get(contig_kmer)) - 1
+            count_list.append(count_kmer)
 
+    fp = open(output_filename, 'w')
+    for item in count_list:
+        print >>fp, '%s' % item
+    
     print 'Hashtable occupancy =', ht.n_occupied()/float(HASHTABLE_SIZE)
+
+
+def test():
+    test_contig_file = os.path.join(datadir, 'sim-genome.fa')
+    test_read_file = [os.path.join(datadir, 'fifty-reads-step3-10x.fa')]
+    test_output_file = 'test.out'
+    main(test_contig_file, test_read_file, test_output_file)
+    
+
+if '__name__==__main__':
+
+    output_file = sys.argv[1]
+    contig_file = sys.argv[2]
+    reads_file = sys.argv[3:]
+    
+    #test()
+    main(contig_file, reads_file, output_file)
