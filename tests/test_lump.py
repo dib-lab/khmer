d9b741cb75c89f2bcbccf8a1a779bda6bd3ad919
C. Titus Brown <titus@idyll.org>
2011-05-28 23:15:28 -0400

    added a test to test stop tag generation in repartitioning

--- tests/test_lump.py
+++ tests/test_lump.py
@@ -51,3 +51,45 @@ def test_fakelump_stop2():
     (n_partitions, n_singletons) = ht.count_partitions()
     assert n_partitions == 3, n_partitions
 
+# try repartitioning
+def test_fakelump_repartitioning():
+    fakelump_fa = os.path.join(thisdir, 'test-data/fakelump.fa')
+    fakelump_fa_foo = os.path.join(thisdir, 'test-data/fakelump.fa.stopfoo')
+
+    ht = khmer.new_hashbits(32, 1e7, 4)
+    ht.consume_fasta_and_tag(fakelump_fa)
+
+    subset = ht.do_subset_partition(0, 0)
+    ht.merge_subset(subset)
+    
+    (n_partitions, n_singletons) = ht.count_partitions()
+    assert n_partitions == 1, n_partitions
+
+    # now, break partitions on any k-mer that you see more than once
+    # on big excursions, where big excursions are excursions 40 out
+    # that encounter more than 82 k-mers.  This should specifically
+    # identify our connected sequences in fakelump...
+
+    EXCURSION_DISTANCE=40
+    EXCURSION_KMER_THRESHOLD=82
+    EXCURSION_KMER_COUNT_THRESHOLD=1
+    counting = khmer.new_counting_hash(32, 1e7, 4)
+
+    ht.repartition_largest_partition(None, counting,
+                                     EXCURSION_DISTANCE,
+                                     EXCURSION_KMER_THRESHOLD,
+                                     EXCURSION_KMER_COUNT_THRESHOLD)
+
+    ht.save_stop_tags(fakelump_fa_foo)
+
+    # ok, now re-do everything with these stop tags, specifically.
+
+    ht = khmer.new_hashbits(32, 1e7, 4)
+    ht.consume_fasta_and_tag(fakelump_fa)
+    ht.load_stop_tags(fakelump_fa_foo)
+
+    subset = ht.do_subset_partition(0, 0, True)
+    ht.merge_subset(subset)
+    
+    (n_partitions, n_singletons) = ht.count_partitions()
+    assert n_partitions == 3, n_partitions
