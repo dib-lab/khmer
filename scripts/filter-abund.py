3475e5d1927366da90f6b150516693b0c1484f15
C. Titus Brown <titus@idyll.org>
2011-07-08 18:26:30 +0000

    new & cleansed scripts, incl filter-abund

---
+++ scripts/filter-abund.py
@@ -0,0 +1,48 @@
+import sys, screed.fasta, os
+import khmer
+from khmer.thread_utils import ThreadedSequenceProcessor, verbose_fasta_iter
+
+K = 32
+WORKER_THREADS=8
+GROUPSIZE=100
+
+###
+
+def main():
+    counting_ht = sys.argv[1]
+    infiles = sys.argv[2:]
+        
+    print 'file with ht: %s' % counting_ht
+    print '-- settings:'
+    print 'N THREADS', WORKER_THREADS
+    print '--'
+
+    print 'making hashtable'
+    ht = khmer.new_counting_hash(K, 1, 1)
+    ht.load(counting_ht)
+
+    for infile in infiles:
+       print 'filtering', infile
+       outfile = infile + '.abundfilt'
+
+       outfp = open(outfile, 'w')
+
+       def process_fn(record, ht=ht):
+          name = record['name']
+          seq = record['sequence']
+          if 'N' in seq:
+              return None, None
+
+          trim_seq, trim_at = ht.trim_on_abundance(seq, 2)
+
+          if trim_at >= K:
+              return name, trim_seq
+
+          return None, None
+
+       tsp = ThreadedSequenceProcessor(process_fn, WORKER_THREADS, GROUPSIZE)
+
+       tsp.start(verbose_fasta_iter(infile), outfp)
+
+if __name__ == '__main__':
+    main()
--- scripts/load-into-counting.py
+++ scripts/load-into-counting.py
@@ -2,7 +2,7 @@ import sys, screed, os
 import khmer
 
 K = 32
-HT_SIZE=4e9
+HT_SIZE=2e9
 N_HT=4
 
 ###
@@ -14,9 +14,13 @@ def main():
     print 'making hashtable'
     ht = khmer.new_counting_hash(K, HT_SIZE, N_HT)
 
-    for filename in filenames:
+    for n, filename in enumerate(filenames):
        print 'consuming input', filename
        ht.consume_fasta(filename)
+
+       if n > 0 and n % 10 == 0:
+          ht.save(base + '.ht')
+          open(base + '.ht.info', 'w').write('through %s' % filename)
 
     ht.save(base + '.ht')
 
