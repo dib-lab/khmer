f17a0d3bc49060f947b31e21292f05fb31349f80
C. Titus Brown <titus@idyll.org>
2011-08-05 05:11:40 +0000

    updated for load-into-counting; now outputs intermediate results

--- scripts/count-density-by-position.py
+++ scripts/count-density-by-position.py
@@ -11,7 +11,6 @@ MAX_DENSITY=2000
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
-outfp = open(outfile, 'w')
     
 print 'saving to:', outfile
 
@@ -19,14 +18,15 @@ print 'making hashtable'
 ht = khmer.new_hashbits(K, HASHTABLE_SIZE, N_HT)
 
 print 'eating', infile
-ht.consume_fasta(infile)
+#ht.consume_fasta(infile)
+ht.load(infile + '.ht')
 
 RADIUS=10
 hist = [0.0] * 200
 histcount = [0] * 200
 
 for n, record in enumerate(screed.fasta.fasta_iter(open(infile))):
-    if n % 10000 == 0:
+    if n % 1000 == 0:
         print '... saving', n
     seq = record['sequence']
 
@@ -38,6 +38,8 @@ for n, record in enumerate(screed.fasta.fasta_iter(open(infile))):
         histcount[pos] += 1
 
 
-for i in range(len(hist)):
-    if histcount[i]:
-        print >>outfp, i, hist[i], histcount[i], hist[i]/histcount[i]
+    if n % 1000 == 0:
+       outfp = open(outfile, 'w')
+       for i in range(len(hist)):
+           if histcount[i]:
+              print >>outfp, i, hist[i], histcount[i], hist[i]/histcount[i]
