bc5afdba9cd5a363bc0a9ee7c1fbe87fa6415605
C. Titus Brown <titus@idyll.org>
2011-08-05 05:14:39 +0000

    updated to deal with big sequences

--- scripts/count-within-radius.py
+++ scripts/count-within-radius.py
@@ -4,7 +4,7 @@ import khmer
 K = 32
 HASHTABLE_SIZE=int(8e9)
 N_HT = 4
-RADIUS=10
+RADIUS=100
 
 ###
 
@@ -23,16 +23,22 @@ ht = khmer.new_hashbits(K, HASHTABLE_SIZE, N_HT)
 print 'eating', infile
 ht.consume_fasta(infile)
 
+print 'loading'
+ht.save(outfile + '.ht')
+
 outfp = open(outfile, 'w')
 for n, record in enumerate(screed.open(infile)):
     if n % 10000 == 0:
         print '... saving', n
     seq = record['sequence']
 
-    middle = (len(seq) - K + 1) / 2
+    for pos in range(0, len(seq), 200):
+       subseq = seq[pos:pos+200]
+
+       middle = (len(subseq) - K + 1) / 2
     
-    density = ht.count_kmers_within_radius(seq[middle:middle+K], RADIUS,
-                                           MAX_DENSITY)
-    density /= float(RADIUS)
+       density = ht.count_kmers_within_radius(subseq[middle:middle+K], RADIUS,
+                                              MAX_DENSITY)
+       density /= float(RADIUS)
     
-    print >>outfp, '>%s d=%.3f\n%s' % (record['name'], density, record['sequence'])
+       print >>outfp, '>%s d=%.3f\n%s' % (record['name'], density, subseq) 
