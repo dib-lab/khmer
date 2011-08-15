861eeb7f7ce0e66e5bae8000c12f15c2d39a774f
C. Titus Brown <titus@idyll.org>
2011-08-15 01:41:41 -0400

    misc small changes, new scripts

---
+++ scripts/softmask-high-abund.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+import sys, khmer, screed, gzip
+
+THRESHOLD = 5
+
+hashfile = sys.argv[1]
+filename = sys.argv[2]
+output = sys.argv[3]
+
+print 'loading ht'
+ht = khmer.load_counting_hash(hashfile)
+print '...done!'
+
+K = ht.ksize()
+print 'loaded ht; K is %d, n_ht is %d, size ~ %g' % (K,
+                                                     len(ht.hashsizes()),
+                                                     ht.hashsizes()[0])
+
+outfp = gzip.open(output, 'w')
+
+total = 0
+total_masked = 0
+
+for n, record in enumerate(screed.open(filename)):
+    if n % 100000 == 0:
+        print '...', n
+
+    x = []
+    seq = record.sequence
+
+    total += len(seq) - K + 1
+    
+    pos = 0
+    while pos < len(seq) - K + 1:
+        kmer = seq[pos:pos + K]
+        if 'N' in kmer:
+            x.extend(kmer)
+            pos += K
+
+        count = ht.get(kmer)
+        
+        if count < THRESHOLD:
+            x.append(seq[pos])
+            pos += 1
+        else:
+            x.extend(['n'] * K)
+            pos += K
+            total_masked += K
+
+    outfp.write(">%s\n%s\n" % (record.name, "".join(x)))
+
+print 'total k-mers seen:', total
+print 'n softmasked:', total_masked
