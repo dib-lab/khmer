e670c0226c898b7824157549dd4a0ceee124b87a
C. Titus Brown <titus@idyll.org>
2011-09-14 19:26:39 +0000

    changed filter-sodd to use load-into-hashbits.py

--- scripts/filter-sodd.py
+++ scripts/filter-sodd.py
@@ -2,9 +2,6 @@ import sys, screed.fasta, os
 import khmer
 from khmer.thread_utils import ThreadedSequenceProcessor, verbose_fasta_iter
 
-K = 32
-HASHTABLE_SIZE=int(8e9)
-N_HT = 4
 MAX_SODD=3
 
 WORKER_THREADS=8
@@ -13,33 +10,11 @@ GROUPSIZE=100
 ###
 
 def main():
-    repfile = sys.argv[1]
-    infile = sys.argv[1]
-    if len(sys.argv) >= 3:
-        infile = sys.argv[2]
+    htfile = sys.argv[1]
+    outfiles = sys.argv[2:]
         
-    outfile = os.path.basename(infile) + '.low'
-    if len(sys.argv) >= 4:
-        outfile = sys.argv[3]
-
-    print 'file with representative artifacts: %s' % repfile
-    print 'input file to degree filter: %s' % infile
-    print 'filtering to output:', outfile
-    print '-- settings:'
-    print 'K', K
-    print 'HASHTABLE SIZE %g' % HASHTABLE_SIZE
-    print 'N HASHTABLES %d' % N_HT
-    print 'N THREADS', WORKER_THREADS
-    print 'MAX DEGREE', MAX_SODD
-    print '--'
-
-    print 'making hashtable'
-    ht = khmer.new_hashbits(K, HASHTABLE_SIZE, N_HT)
-
-    outfp = open(outfile, 'w')
-
-    print 'eating', repfile
-    ht.consume_fasta(repfile)
+    print 'loading hashbits'
+    ht = khmer.load_hashbits(htfile)
 
     def process_fn(record, ht=ht):
         name = record['name']
@@ -49,16 +24,17 @@ def main():
 
         trim_seq, trim_at = ht.trim_on_sodd(seq, MAX_SODD)
 
-        if trim_at >= K:
+        if trim_at >= ht.ksize():
             return name, trim_seq
 
         return None, None
 
-    tsp = ThreadedSequenceProcessor(process_fn, WORKER_THREADS, GROUPSIZE)
+    for filename in outfiles:
+       outpath = os.path.basename(filename) + '.sodd'
+       outfp = open(outpath, 'w')
 
-    ###
-
-    tsp.start(verbose_fasta_iter(infile), outfp)
+       tsp = ThreadedSequenceProcessor(process_fn, WORKER_THREADS, GROUPSIZE)
+       tsp.start(verbose_fasta_iter(filename), outfp)
 
 if __name__ == '__main__':
     main()
---
+++ scripts/load-into-hashbits.py
@@ -0,0 +1,97 @@
+# /usr/bin/env python
+"""
+Build a graph Bloom filter from the given sequences, save in <htname>.
+
+% python scripts/load-into-hashbits.py <htname> <data1> [ <data2> <...> ]
+
+Parameters to adjust: K, HT_SIZE.  HT_SIZE should be set to about 2x the
+available system memory.
+"""
+
+import sys, screed, os
+import khmer
+import argparse
+
+DEFAULT_K=32
+DEFAULT_N_HT=4
+DEFAULT_MIN_HASHSIZE=1e6
+
+def build_common_args():
+
+    parser = argparse.ArgumentParser(description=
+                                     'Build & load a counting Bloom filter.')
+
+    env_ksize = os.environ.get('KHMER_KSIZE', DEFAULT_K)
+    env_n_hashes = os.environ.get('KHMER_N_HASHES', DEFAULT_N_HT)
+    env_hashsize = os.environ.get('KHMER_MIN_HASHSIZE', DEFAULT_MIN_HASHSIZE)
+
+    parser.add_argument('-q', '--quiet', dest='quiet', default=False,
+                        action='store_true')
+    parser.add_argument('--ksize', '-k', type=int, dest='ksize',
+                        default=env_ksize,
+                        help='k-mer size to use')
+    parser.add_argument('--n_hashes', '-N', type=int, dest='n_hashes',
+                        default=env_n_hashes,
+                        help='number of hash tables to use')
+    parser.add_argument('--hashsize', '-x', type=float, dest='min_hashsize',
+                        default=env_hashsize,
+                        help='lower bound on hashsize to use')
+
+    return parser
+
+def parse_args(parser):
+    args = parser.parse_args()
+
+    if not args.quiet:
+        if args.min_hashsize == DEFAULT_MIN_HASHSIZE:
+            print>>sys.stderr, "** WARNING: hashsize is default!  You absodefly want to increase this!\n** Please read the docs!"
+
+        print>>sys.stderr, '\nPARAMETERS:'
+        print>>sys.stderr, ' - kmer size =    %d \t\t(-k)' % args.ksize
+        print>>sys.stderr, ' - n hashes =     %d \t\t(-N)' % args.n_hashes
+        print>>sys.stderr, ' - min hashsize = %-5.2g \t(-x)' % args.min_hashsize
+        print>>sys.stderr, ''
+        print>>sys.stderr, 'Estimated memory usage is %.2g bytes (n_hashes x min_hashsize / 8 bits/byte)' % (args.n_hashes * args.min_hashsize / 8.)
+        print>>sys.stderr, '-'*8
+
+    return args
+
+###
+
+def main():
+    parser = build_common_args()
+    parser.add_argument('output_filename')
+    parser.add_argument('input_filenames', nargs='+')
+
+    args = parse_args(parser)
+
+    K=args.ksize
+    HT_SIZE=args.min_hashsize
+    N_HT=args.n_hashes
+
+    base = args.output_filename
+    filenames = args.input_filenames
+
+    print 'Saving hashtable to %s' % base
+    print 'Loading kmers from sequences in %s' % repr(filenames)
+
+    ###
+    
+    print 'making hashtable'
+    ht = khmer.new_hashbits(K, HT_SIZE, N_HT)
+
+    for n, filename in enumerate(filenames):
+       print 'consuming input', filename
+       ht.consume_fasta(filename)
+
+       if n > 0 and n % 10 == 0:
+           print 'mid-save', base
+           ht.save(base)
+           open(base + '.info', 'w').write('through %s' % filename)
+
+    print 'saving', base
+    ht.save(base)
+    open(base + '.info', 'w').write('through end: %s' % filename)
+
+if __name__ == '__main__':
+    main()
