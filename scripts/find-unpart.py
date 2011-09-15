d0af83c80d78fa8ac004d0afa7abd12534dd2482
C. Titus Brown <titus@idyll.org>
2011-09-15 00:47:06 -0400

    split the ht/tagset filename from the file-to-partition file

--- scripts/find-unpart.py
+++ scripts/find-unpart.py
@@ -2,24 +2,25 @@ import khmer, sys, os
 import gc
 import glob
 
-filename=sys.argv[1]
-basename = os.path.basename(filename)
-pmap_filename = sys.argv[2]
+already_part = sys.argv[1]
+new_to_part = sys.argv[2]
+basename = os.path.basename(new_to_part)
+pmap_filename = sys.argv[3]
 
-if not os.path.exists(filename):
-    print '%s doesn\'t exist! dying.' % filename
+if not os.path.exists(already_part):
+    print '%s doesn\'t exist! dying.' % already_part
     sys.exit(0)
 
 # create a fake-ish ht; K matters, but not hashtable size.
-ht = khmer.load_hashbits(basename + '.ht')
-ht.load_tagset(basename + '.tagset')
+ht = khmer.load_hashbits(already_part + '.ht')
+ht.load_tagset(already_part + '.tagset')
 ht.merge_subset_from_disk(pmap_filename)
 
 # find singletons
-n_singletons = ht.find_unpart(filename)
+n_singletons = ht.find_unpart(new_to_part)
 print 'found:', n_singletons
 
-n_partitions = ht.output_partitions(filename, basename + '.unpart')
+n_partitions = ht.output_partitions(new_to_part, basename + '.unpart')
 
 ###
 
