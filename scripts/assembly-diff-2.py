2d3ecdb9c54b2a9b5088fce0c426ab49d9dc3c37
C. Titus Brown <titus@idyll.org>
2011-10-08 11:57:52 -0400

    commented extract_unique_paths; N->G in assembly-diff-2.py

--- scripts/assembly-diff-2.py
+++ scripts/assembly-diff-2.py
@@ -13,14 +13,20 @@ filename2 = sys.argv[2]
 uniq2 = open(os.path.basename(sys.argv[2]) + '.uniq', 'w')
 
 kh = khmer.new_hashbits(K, HASHTABLE_SIZE, N_HT)
-kh.consume_fasta(filename1)
+for n, record in enumerate(screed.open(filename1)):
+    if n % 10000 == 0:
+        print '...', filename1, n
+    seq = record.sequence.upper().replace('N', 'G')
+    kh.consume(seq)
 
-n = 0
-for record in screed.open(filename2):
+path_n = 0
+for n, record in enumerate(screed.open(filename2)):
+    if n % 10000 == 0:
+        print '...', filename2, n
     seq = record.sequence
     paths = kh.extract_unique_paths(seq, UNIQUE_LEN, UNIQUE_F)
     kh.consume(seq)
 
     for path in paths:
-        n += 1
-        print >>uniq2, '>%s\n%s' % (n, path)
+        path_n += 1
+        print >>uniq2, '>%s\n%s' % (path_n, path)
