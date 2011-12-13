d99614fe748c031b399445eb091a2f3bb775d6a9
C. Titus Brown <titus@idyll.org>
2011-12-12 23:53:43 -0500

    fixed strip-partition to use screed.open

--- sandbox/strip-partition.py
+++ sandbox/strip-partition.py
@@ -2,7 +2,7 @@
 import screed
 import sys
 
-for record in screed.fasta.fasta_iter(open(sys.argv[1]), parse_description=False):
+for record in screed.open(sys.argv[1]):
     name = record['name']
     sequence = record['sequence']
 
