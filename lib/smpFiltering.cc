f85ae0036b3966751ef58b93d3ccaf7dd4dd4f51
Eric McDonald <em@msu.edu>
2012-02-01 14:02:57 -0500

    Import Tim Brom's SMP FAST{A,Q} parsers from his 'smp_filtering' branch.

---
+++ lib/smpFiltering.cc
@@ -0,0 +1,108 @@
+/* This file is a simple serial implementation of the folloing algorithm:
+
+for read in file:
+    if median(read) > C:
+        discard();
+    else:
+        count(read);
+        save_to_file(read);
+*/
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "threadedParsers.hh"
+#include "counting.hh"
+#include "primes.hh"
+#include <math.h>
+
+using namespace std;
+using namespace khmer;
+
+#define MAX_MEDIAN_COUNT 10 /* Only count if the median is < this */
+#define TABLE_SIZE 2000000000
+
+/* Forward Declarations */
+
+int main(int argc, char **argv)
+{
+    /* Check commandline arguments */
+    if (argc != 4)
+    {
+        cerr << "You must specify an input and output filename and the k-size" << endl;
+        cerr << "\tUsage: " << argv[0] << " input filename output filename k-size" << endl;
+        exit(-1);
+    }
+
+    unsigned int K;
+    if (sscanf(argv[3], "%d", &K) != 1)
+    {
+        cerr << "Could not interpret K size of " << argv[3] << endl;
+        exit(-1);
+    }
+    
+
+    /* Initialize the counting hash */
+    vector<HashIntoType> tableSizes;
+    Primes pri(TABLE_SIZE);
+    tableSizes.push_back(pri.get_next_prime());
+    tableSizes.push_back(pri.get_next_prime());
+    tableSizes.push_back(pri.get_next_prime());
+    CountingHash h(K, tableSizes);
+
+    /* Start parsing the fasta file */
+    ThreadedIParserFactory* pf = ThreadedIParserFactory::get_parser(argv[1], 104857600);
+    long long unsigned int totalCount = 0, keptCount = 0;
+
+    #pragma omp parallel reduction(+:totalCount,keptCount)
+    while (!pf->is_complete())
+    {
+        ThreadedIParser* p = pf->get_next_parser();
+
+        /* Open the output file */
+        ofstream outFile;
+        string outputFileName(argv[2]);
+        char numstr[21]; // enough to hold all numbers up to 64-bits
+        sprintf(numstr, "%020lu", p->getEndPos());
+        outputFileName += "_";
+        outputFileName += numstr;
+        outFile.open(outputFileName.c_str());
+        if (! outFile.is_open())
+        {
+            cerr << "Failed to open file " << outputFileName;
+            perror("");
+            cerr << endl;
+            exit(-1);
+        }
+
+        while (!p->is_complete())
+        {
+            Read r;
+            BoundedCounterType medCount;
+            float meanCount;
+            float stdDev;
+            r = p->get_next_read();
+            totalCount++;
+            h.get_median_count(r.seq, medCount, meanCount, stdDev);
+            if (medCount < MAX_MEDIAN_COUNT)
+            {
+                keptCount++;
+                /* Count it */
+                KMerIterator kmers(r.seq.c_str(), K);
+                while(!kmers.done())
+                {
+                    h.count(kmers.next());
+                }
+
+                outFile << ">" << r.name << endl;
+                outFile << r.seq << endl;
+            }
+        }
+        outFile.close();
+    }
+
+
+    printf("Total Count: %llu, Kept Count: %llu\n", totalCount, keptCount);
+    printf("Hashtable Occupancy: %lf\n", (double)h.n_occupied() / TABLE_SIZE);
+}
---
+++ lib/threadedParsers.hh
@@ -0,0 +1,99 @@
+#ifndef THREADED_PARSERS_H
+#define THREADED_PARSERS_H
+
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <fstream>
+#include <assert.h>
+
+struct Read
+{
+   std::string name;
+   std::string seq;
+   //std::string quality;
+};
+
+/* Base class for parsers, defines a set of methods that a parser must
+ * implement and a static function that returns the proper parser
+ * for different file types
+ */
+class ThreadedIParser
+{
+public:
+   virtual Read get_next_read() = 0;
+   virtual bool is_complete() = 0;
+   virtual ~ThreadedIParser() { }
+   virtual long int getEndPos() = 0;
+};
+
+class ThreadedFastaParser : public ThreadedIParser
+{
+private:
+   std::ifstream infile;
+   long int endPos;
+
+public:
+   ThreadedFastaParser(const std::string &inputfile, long int startPos, long int end);
+   ~ThreadedFastaParser() { infile.close();  }
+   Read get_next_read();
+   bool is_complete() { return (infile.tellg() >= endPos || infile.eof()); } 
+   long int getEndPos() { return endPos; }
+};
+
+class ThreadedFastqParser : public ThreadedIParser
+{
+private:
+   std::ifstream infile;
+   long int endPos;
+
+public:
+   ThreadedFastqParser(const std::string &inputfile, long int startPos, long int end);
+   ~ThreadedFastqParser() { infile.close(); }
+   Read get_next_read();
+   bool is_complete() { return infile.tellg() >= endPos || infile.eof(); }
+   long int getEndPos() { return endPos; }
+};
+
+/* Base class for parser factories. Each factory will return a parser
+ * object that will return all the reads within a chunk of the file
+ */
+class ThreadedIParserFactory
+{
+public:
+    virtual ThreadedIParser* get_next_parser() = 0;
+    virtual bool is_complete() = 0;
+    static ThreadedIParserFactory* get_parser(const std::string &inputfile, long int chunkSize);
+};
+
+class ThreadedFastaParserFactory : public ThreadedIParserFactory
+{
+private:
+    std::string filename;
+    long int curPos;
+    long int fileSize;
+    long int chunkSize;
+
+public:
+    ThreadedFastaParserFactory(const std::string &inputfile, 
+        long int size);
+    ThreadedIParser* get_next_parser();
+    bool is_complete() { return curPos >= fileSize; }
+};
+
+class ThreadedFastqParserFactory : public ThreadedIParserFactory
+{
+private:
+    std::string filename;
+    long int curPos;
+    long int fileSize;
+    long int chunkSize;
+
+public:
+    ThreadedFastqParserFactory(const std::string &inputfile, 
+        long int chunkSize);
+    ThreadedIParser* get_next_parser();
+    bool is_complete() { return curPos >= fileSize; }
+};
+
+#endif
