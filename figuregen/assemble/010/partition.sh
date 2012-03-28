f2ce54a938b8ff3af1bd2fe1e73486c72321fd03
root <root@ip-10-87-85-207.ec2.internal>
2012-03-28 16:09:38 +0000

    changed assembly pipeline

--- figuregen/assemble/010/partition.sh
+++ figuregen/assemble/010/partition.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 python ../graph-size.py 31 2156638134 3 1 200 ../../data/msb2.fq msb2.fa
+python ../graph-size.py 31 2156638134 3 1 200 msb2.fa msb2a.fa
+rm msb2.fa
+mv msb2a.fa msb2.fa
+python ../graph-size.py 31 2156638134 3 1 200 msb2.fa msb2a.fa
+rm msb2.fa
+mv msb2a.fa msb2.fa
 python ../load-graph.py -k 31 --n_hashes 3 --hashsize 2156638134 msb2 msb2.fa
 python ../partition-graph.py --threads 1 msb2
 python ../merge-partitions.py -k 31 msb2
