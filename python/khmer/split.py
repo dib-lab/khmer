700f6440949c65b1c3eac387a40a074b1e24c9b9
Eric McDonald <em@msu.edu>
2012-03-28 14:50:08 -0400

    Handle differences between CPython API and Cython interfaces with a crude try-except hack during imports.

--- python/khmer/split.py
+++ python/khmer/split.py
@@ -1,7 +1,9 @@
 import _khmer
 from _khmer import new_ktable
-from _khmer import new_hashtable
-#from _khmer import _new_counting_hash as new_hashtable
+try: # CPython API
+    from _khmer import new_hashtable
+except ImportError: # Cython
+    from _khmer import _new_counting_hash as new_hashtable
 #from _khmer import consume_genome
 #from _khmer import forward_hash, forward_hash_no_rc, reverse_hash
 
