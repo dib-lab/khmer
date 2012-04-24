985f5475a83fc304a91d07d06f0f5943b3791906
Jason Pell <jason.pell@gmail.com>
2012-04-24 12:48:42 -0400

    added errorbars to S1 figure

--- figuregen/perc/S1/runThis.sh
+++ figuregen/perc/S1/runThis.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -login
-#PBS -l nodes=1:ppn=1,walltime=2:00:00,mem=7gb
+#PBS -l nodes=1:ppn=1,walltime=4:00:00,mem=4gb
 #PBS -j oe
 
-cd /mnt/scratch/pelljaso/perc/S1
+cd /mnt/scratch/pelljaso/pdbg/figuregen/perc/S1
 
 for Z in 4 5 6 7 8 9 10 11 12; do
-  ./ppt 0."$localN" 1 100 R_0."$localN"_K"$Z".txt $Z 1.0
+  ./ppt 0."$localN" 1 100 R_0."$localN"_"$Z".txt $Z 1.0
 done
