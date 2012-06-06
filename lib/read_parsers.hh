7edbd58657b6c00dbb535cd3f216e417d905cc0f
Eric McDonald <em@msu.edu>
2012-06-05 21:38:21 -0400

    Refactor TraceLogger class into a separate set of files.

--- lib/read_parsers.hh
+++ lib/read_parsers.hh
@@ -30,6 +30,7 @@ extern "C"
 #include "bzip2/bzlib.h"
 
 #include "khmer_config.hh"
+#include "trace_logger.hh"
 
 
 #define MIN( a, b )	(((a) > (b)) ? (b) : (a))
@@ -40,9 +41,6 @@ namespace khmer
 
 
 struct InvalidStreamHandle : public std:: exception
-{ };
-
-struct InvalidStreamBuffer : public std:: exception
 { };
 
 struct StreamReadError : public std:: exception
@@ -56,53 +54,6 @@ struct InvalidNumberOfThreadsRequested : public std:: exception
 
 struct TooManyThreads : public std:: exception
 { };
-
-
-struct TraceLogger
-{
-    
-    enum
-    {
-	TLVL_ALL	= 0,
-	TLVL_DEBUG9, TLVL_DEBUG8, TLVL_DEBUG7, TLVL_DEBUG6, TLVL_DEBUG5,
-	TLVL_DEBUG4, TLVL_DEBUG3, TLVL_DEBUG2, TLVL_DEBUG1, TLVL_DEBUG0,
-	TLVL_INFO9, TLVL_INFO8, TLVL_INFO7, TLVL_INFO6, TLVL_INFO5,
-	TLVL_INFO4, TLVL_INFO3, TLVL_INFO2, TLVL_INFO1, TLVL_INFO0,
-	TLVL_WARNING	= 30,
-	TLVL_ERROR	= 40,
-	TLVL_CRITICAL	= 50,
-	TLVL_NONE	= 255
-    };
-    
-    TraceLogger( uint8_t const level, FILE * stream_handle );
-    TraceLogger(
-	uint8_t const level, char const * const file_name_format, ...
-    );
-    ~TraceLogger( );
-
-    inline void	    operator( )(
-	uint8_t const level, char const * const format, ...
-    ) const
-    {
-	va_list varargs;
-	
-	if (_level <= level)
-	{
-	    va_start( varargs, format );
-	    vfprintf( _stream_handle, format, varargs );
-	    va_end( varargs );
-	    fflush( _stream_handle );
-	}
-
-    }
-
-private:
-    
-    uint8_t	    _level;
-    bool	    _shared_stream;
-    FILE *	    _stream_handle;
-
-};
 
 
 struct IPerformanceMetrics
