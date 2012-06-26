d0d69759c67d275a3e9c157ee40050dc4265b291
C. Titus Brown <titus@idyll.org>
2012-06-25 22:23:46 -0400

    half-arsed streaming error trimming

---
+++ sandbox/trim-low-abund.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python
+import sys, screed, os
+import khmer
+from khmer.thread_utils import ThreadedSequenceProcessor, verbose_loader
+import argparse
+
+NORMALIZE_LIMIT=20
+DEFAULT_CUTOFF=2
+
+DEFAULT_K=32
+DEFAULT_N_HT=4
+DEFAULT_MIN_HASHSIZE=1e6
+
+def main():
+    parser = argparse.ArgumentParser(description='Foo.')
+
+    env_ksize = os.environ.get('KHMER_KSIZE', DEFAULT_K)
+    env_n_hashes = os.environ.get('KHMER_N_HASHES', DEFAULT_N_HT)
+    env_hashsize = os.environ.get('KHMER_MIN_HASHSIZE', DEFAULT_MIN_HASHSIZE)
+
+    parser.add_argument('--ksize', '-k', type=int, dest='ksize',
+                        default=env_ksize,
+                        help='k-mer size to use')
+    parser.add_argument('--n_hashes', '-N', type=int, dest='n_hashes',
+                        default=env_n_hashes,
+                        help='number of hash tables to use')
+    parser.add_argument('--hashsize', '-x', type=float, dest='min_hashsize',
+                        default=env_hashsize,
+                        help='lower bound on hashsize to use')
+
+    parser.add_argument('input_filenames', nargs='+')
+    args = parser.parse_args()
+
+    K=args.ksize
+    HT_SIZE=args.min_hashsize
+    N_HT=args.n_hashes
+
+    print 'making hashtable'
+    ht = khmer.new_counting_hash(K, HT_SIZE, N_HT)
+
+    save_pass2 = 0
+
+    pass2list = []
+    for filename in args.input_filenames:
+        pass2filename = os.path.basename(filename) + '.pass2'
+        trimfilename = os.path.basename(filename) + '.abundtrim'
+
+        pass2list.append((pass2filename, trimfilename))
+
+        pass2fp = open(pass2filename, 'w')
+        trimfp = open(trimfilename, 'w')
+
+        for n, read in enumerate(screed.open(filename)):
+            if n % 10000 == 0:
+                print '...', n, filename, save_pass2
+            seq = read.sequence.replace('N', 'A')
+            med, _, _ = ht.get_median_count(seq)
+
+            if med < NORMALIZE_LIMIT:
+                ht.consume(seq)
+                pass2fp.write('>%s\n%s\n' % (read.name, read.sequence))
+                save_pass2 += 1
+            else:
+                trim_seq, trim_at = ht.trim_on_abundance(seq, DEFAULT_CUTOFF)
+                if trim_at >= K:
+                    trimfp.write('>%s\n%s\n' % (read.name, trim_seq))
+
+        pass2fp.close()
+        trimfp.close()
+
+        print 'saved %d of %d to pass2fp' % (save_pass2, n,)
+
+    for pass2filename, trimfilename in pass2list:
+        for n, read in enumerate(screed.open(pass2filename)):
+            if n % 10000 == 0:
+                print '... x 2', n, filename
+
+            trimfp = open(trimfilename, 'a')
+        
+            seq = read.sequence.replace('N', 'A')
+            med, _, _ = ht.get_median_count(seq)
+
+            if med >= NORMALIZE_LIMIT or 1:
+                trim_seq, trim_at = ht.trim_on_abundance(seq, DEFAULT_CUTOFF)
+                if trim_at >= K:
+                    trimfp.write('>%s\n%s\n' % (read.name, trim_seq))
+            else:
+                trimfp.write('>%s\n%s\n' % (read.name, read.sequence))
+
+if __name__ == '__main__':
+    main()
+
