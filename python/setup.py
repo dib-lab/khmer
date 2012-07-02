773f60adf21281af2ecf545d19864c9f491e1528
Jason Pell <jason.pell@gmail.com>
2012-07-02 10:44:50 -0400

    this should be identical to the ged-lab/khmer master branch

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,11 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from distutils.core import setup, Extension
 
 # the c++ extension module (needs to be linked in with ktable.o ...)
-extension_mod = Extension("khmer._khmer",
-                          sources=["_khmermodule.pyx"],
+extension_mod = Extension("khmer._khmermodule",
+                          ["_khmermodule.cc"],
                           extra_compile_args=['-g'],
-                          language="c++",
                           include_dirs=['../lib',],
                           library_dirs=['../lib',],
-                          libraries=["stdc++"],
                           extra_objects=['../lib/ktable.o',
                                          '../lib/hashtable.o',
                                          '../lib/parsers.o',
@@ -36,7 +32,6 @@ extension_mod = Extension("khmer._khmer",
                                    '../lib/hashtable.o',
                                    '../lib/ktable.o',
                                    '../lib/parsers.o',
-                                   '../lib/intertable.o',
                                    '../lib/hashbits.o',
                                    '../lib/counting.o',
                                    '../lib/subset.o',
@@ -67,6 +62,4 @@ setup(name = "khmer", version = "0.4",
       url = 'http://ged.msu.edu/',
       license='New BSD License',
       packages = [py_mod,],
-      ext_modules = [extension_mod,],
-      cmdclass = {'build_ext': build_ext},
-)
+      ext_modules = [extension_mod,])
