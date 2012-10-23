6a17d1fb67567d5867afb5e8a842412625fca5f9
Eric McDonald <em@msu.edu>
2012-10-23 19:54:25 -0400

    Refactor IPerformanceMetrics to separate files.

--- lib/read_parsers.cc
+++ lib/read_parsers.cc
@@ -12,33 +12,11 @@
 #   include <linux/fs.h>
 #endif
 
-#include <ctime>
-
 #include "read_parsers.hh"
 
 
 namespace khmer
 {
-
-
-IPerformanceMetrics::
-IPerformanceMetrics( )
-{ }
-
-
-IPerformanceMetrics::
-~IPerformanceMetrics( )
-{ }
-
-
-uint64_t const
-IPerformanceMetrics::
-_timespec_diff_in_nsecs( timespec const &start, timespec const &stop )
-{
-    return
-	    ((stop.tv_sec * 1000000000U) + (uint64_t)stop.tv_nsec)
-	-   ((start.tv_sec * 1000000000U) + (uint64_t)start.tv_nsec);
-}
 
 
 ThreadIDMap::
--- lib/read_parsers.hh
+++ lib/read_parsers.hh
@@ -31,6 +31,7 @@ extern "C"
 
 #include "khmer_config.hh"
 #include "trace_logger.hh"
+#include "perf_metrics.hh"
 
 
 namespace khmer
@@ -43,48 +44,11 @@ struct InvalidStreamHandle : public std:: exception
 struct StreamReadError : public std:: exception
 { };
 
-struct InvalidPerformanceMetricsKey : public std:: exception
-{ };
-
 struct InvalidNumberOfThreadsRequested : public std:: exception
 { };
 
 struct TooManyThreads : public std:: exception
 { };
-
-
-struct IPerformanceMetrics
-{
-
-	    IPerformanceMetrics( );
-    virtual ~IPerformanceMetrics( );
-    
-    inline void	    start_timers( )
-    {
-	clock_gettime( CLOCK_REALTIME, &_temp_clock_start );
-	clock_gettime( CLOCK_THREAD_CPUTIME_ID, &_temp_cpu_start );
-    }
-    inline void	    stop_timers( )
-    {
-	clock_gettime( CLOCK_THREAD_CPUTIME_ID, &_temp_cpu_stop );
-	clock_gettime( CLOCK_REALTIME, &_temp_clock_stop );
-    }
-    virtual void    accumulate_timer_deltas( uint32_t metrics_key )	= 0;
-    
-    // TODO: Add a printing or log file feature.
-
-protected:
-    
-    timespec	_temp_cpu_start;
-    timespec	_temp_cpu_stop;
-    timespec	_temp_clock_start;
-    timespec	_temp_clock_stop;
-
-    uint64_t const  _timespec_diff_in_nsecs(
-	timespec const &start, timespec const &stop
-    );
-
-};
 
 
 struct ThreadIDMap
