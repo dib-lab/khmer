e0e54f947d3a6dc07ec44330b7896475406fd996
C. Titus Brown <titus@idyll.org>
2012-11-04 15:09:28 -0500

    added disqus comments and github editing

--- doc/conf.py
+++ doc/conf.py
@@ -121,6 +121,7 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_style = 'labibi.css'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
