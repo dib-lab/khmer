eeebb7478ae598ca266a0f5a6ed4c8f18fb9841a
Eric McDonald <em@msu.edu>
2013-02-22 17:07:10 -0500

    Create test of using ReadParser object with multiple threads.

--- tests/test_read_parsers.py
+++ tests/test_read_parsers.py
@@ -6,7 +6,7 @@ from khmer import ReadParser
 import khmer_tst_utils as utils
 
 
-def test_DEFAULT_ARGUMENTS( ):
+def test_with_default_arguments( ):
     
     read_names = [ ]
     # Note: Using a data file where read names are just integers on [0,99).
@@ -21,6 +21,40 @@ def test_DEFAULT_ARGUMENTS( ):
     # Each read number should match the corresponding name.
     for m, n in enumerate( read_names ):
         assert m == n
+
+
+def test_with_multiple_threads( ):
+    
+    import operator
+    import threading
+
+    reads_count_1thr = 0
+    rparser = ReadParser( utils.get_test_data( "test-reads.fa" ) )
+    for read in rparser: reads_count_1thr += 1
+
+    def count_reads( rparser, counters, tnum ):
+        counters[ tnum ] = reduce( operator.add, ( 1 for read in rparser ) )
+
+    N_THREADS = 4
+    config = khmer.get_config( )
+    bufsz = config.get_reads_input_buffer_size( )
+    config.set_reads_input_buffer_size( N_THREADS * 64 * 1024 )
+    threads = [ ]
+    reads_counts_per_thread = [ 0 ] * N_THREADS
+    rparser = ReadParser( utils.get_test_data( "test-reads.fa" ), N_THREADS )
+    for tnum in xrange( N_THREADS ):
+        t = \
+        threading.Thread(
+            target = count_reads,
+            args = [ rparser, reads_counts_per_thread, tnum ]
+        )
+        threads.append( t )
+        t.start( )
+    for t in threads:
+        t.join( )
+    config.set_reads_input_buffer_size( bufsz )
+
+    assert reads_count_1thr == sum( reads_counts_per_thread )
 
 
 # TODO: Write more tests.
