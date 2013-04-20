3cbfc5c4c0810c97aa890ae0aebd932bb1c0f85d
C. Titus Brown <titus@idyll.org>
2013-04-20 11:20:56 -0400

    commented the tests a leetle bit

--- tests/test_align.py
+++ tests/test_align.py
@@ -9,21 +9,28 @@ def test_alignnocov():
    ch.consume("ACCTAGGTTCGACATGTACC")
    graphAlign, readAlign = aligner.align(read)
 
-   assert readAlign == 'ACCTAGGTTCGACATGTACC'
+   # should be the same
+   assert readAlign ==   'ACCTAGGTTCGACATGTACC'
    assert graphAlign  == 'ACCTAGGTTCGACATGTACC'
 
 
 def test_readalign():
    ch = khmer.new_counting_hash(10, 1048576, 1)
-   read = "ACCTAGGTTCGACATGTACC"
    aligner = khmer.new_readaligner(ch, 1, 20)
    for i in range(20):
       ch.consume("AGAGGGAAAGCTAGGTTCGACAAGTCCTTGACAGAT")
+   read =                "ACCTAGGTTCGACATGTACC"
+   #                      ^^            ^  ^
+      
    ch.consume("GCTTTTAAAAAGGTTCGACAAAGGCCCGGG")
+
+
+
    graphAlign, readAlign = aligner.align(read)
 
-   assert readAlign == 'ACCTAGGTTCGACATGTACC'
-   assert graphAlign  == 'AGCTAGGTTCGACAAGT-CC'
+   assert readAlign ==  'ACCTAGGTTCGACATGTACC'
+   assert graphAlign == 'AGCTAGGTTCGACAAGT-CC'
+   #                                   ^  ^
 
 def test_alignerrorregion():
    ch = khmer.new_counting_hash(10, 1048576, 1)
