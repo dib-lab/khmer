835fe3fb81411d9ff5718ef0aed0874f03d02691
C. Titus Brown <titus@idyll.org>
2013-09-01 21:07:44 -0400

    added copyright license and brief description to sandbox/bin-reads-by-abundance

--- sandbox/bin-reads-by-abundance.py
+++ sandbox/bin-reads-by-abundance.py
@@ -1,4 +1,15 @@
 #! /usr/bin/env python
+#
+# This file is part of khmer, http://github.com/ged-lab/khmer/, and is
+# Copyright (C) Michigan State University, 2009-2013. It is licensed under
+# the three-clause BSD license; see doc/LICENSE.txt. Contact: ctb@msu.edu
+#
+"""
+Bin reads by abundance, with some heuristics thrown in to reduce memory
+usage.
+
+CTB.
+"""
 import os.path
 import khmer
 import argparse
