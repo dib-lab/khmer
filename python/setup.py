abd0f2510ab03603bfc4630448d6c515f8b865d4
Michael Crusoe <mcrusoe@msu.edu>
2013-09-23 17:55:02 -0400

    initial switch to pure setup.py building

--- python/setup.py
+++ python/setup.py
@@ -3,34 +3,39 @@
 # Copyright (C) Michigan State University, 2009-2013. It is licensed under
 # the three-clause BSD license; see doc/LICENSE.txt. Contact: ctb@msu.edu
 #
-from distribute_setup import use_setuptools
-use_setuptools()
+import ez_setup
+ez_setup.use_setuptools()
 
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools import Extension
 
 from os.path import (
+    abspath         as path_abspath,
+    dirname         as path_dirname,
     pardir	    as path_pardir,
     join	    as path_join,
 )
 
+from os import (
+    chdir           as os_chdir,
+    listdir	    as os_listdir
+)
+
+from subprocess import call
+
+thisdir = path_abspath(path_dirname(__file__))
+rootdir = path_abspath(path_join(thisdir, '..'))
+zlibdir = path_join(rootdir, 'lib/zlib')
+bzip2dir = path_join(rootdir, 'lib/bzip2')
+
 # Execute dynamically-generated portion of setup script in current namespace.
 # Please see the dynamically-generated file to learn which variables are added
 # to the current namespace.
-execfile( "setup-dynamic.py" )
+#execfile(path_join(thisdir, "setup-dynamic.py" ))
 
 extra_objs = [ ]
 extra_objs.extend( map(
-    lambda bn: path_join( path_pardir, "lib", bn + ".o" ),
-    [ 
-	"khmer_config", "thread_id_map", "trace_logger", "perf_metrics", 
-	"read_parsers", 
-	"ktable", "hashtable", "hashbits", "counting", "subset",
-        "aligner", "scoringmatrix", "node", "kmer",
-    ]
-) )
-extra_objs.extend( map(
-    lambda bn: path_join( path_pardir, "lib", "zlib", bn + ".o" ),
+    lambda bn: path_join( rootdir, "lib", "zlib", bn + ".o" ),
     [ 
 	"adler32", "compress", "crc32", "deflate", "gzio", 
 	"infback", "inffast", "inflate", "inftrees", "trees", "uncompr", 
@@ -38,7 +43,7 @@ extra_objs.extend( map(
     ]
 ) )
 extra_objs.extend( map(
-    lambda bn: path_join( path_pardir, "lib", "bzip2", bn + ".o" ),
+    lambda bn: path_join( rootdir, "lib", "bzip2", bn + ".o" ),
     [
 	"blocksort", "huffman", "crctable", "randtable", "compress",
 	"decompress", "bzlib",
@@ -47,19 +52,29 @@ extra_objs.extend( map(
 
 build_depends = list(extra_objs)
 build_depends.extend( map(
-    lambda bn: path_join( path_pardir, "lib", bn + ".hh" ),
+    lambda bn: path_join( rootdir, "lib", bn + ".hh" ),
     [
 	"storage", "khmer", "khmer_config", "ktable", "hashtable", "counting",
     ]
 ) )
 
+sources = [ path_join( thisdir, "_khmermodule.cc") ]
+sources.extend( map(
+	lambda bn: path_join( rootdir, "lib", bn + ".cc"),
+	[
+		"khmer_config", "thread_id_map", "trace_logger", "perf_metrics", 
+		"read_parsers", "ktable", "hashtable", "hashbits", "counting", "subset",
+	        "aligner", "scoringmatrix", "node", "kmer",
+	]
+) )
+
 extension_mod_DICT = \
     {
-	"sources": [ "_khmermodule.cc" ],
-	"extra_compile_args": extra_compile_args,
+	"sources": sources,
+#	"extra_compile_args": extra_compile_args,
 	"extra_link_args": filter( None, [ ] ),
-	"include_dirs": [ path_join( path_pardir, "lib" ), ],
-	"library_dirs": [ path_join( path_pardir, "lib" ), ],
+	"include_dirs": [ path_join( rootdir, "lib" ), ],
+	"library_dirs": [ path_join( rootdir, "lib" ), ],
 	"extra_objects": extra_objs,
 	"depends": build_depends,
 	"language": "c++",
@@ -69,17 +84,39 @@ extension_mod_DICT = \
 extension_mod = Extension( "khmer._khmermodule", **extension_mod_DICT )
 # python modules: only 'khmer'
 py_mod = 'khmer'
+
+scripts = []
+scripts.extend( [path_join(rootdir, "scripts", script)
+	for script in os_listdir(path_join(rootdir, "scripts"))
+	if script.endswith(".py")])
+
 setup_metadata = \
     {
-	"name": "khmer", "version": "0.4",
+	"name": "khmer",
+	"version": "0.6",
 	"description": 'khmer k-mer counting library',
-	"author": 'C. Titus Brown, Jason Pell, and Adina Howe',
+	#"long_description": 'TODO',
+	"author": 'Michael Crusoe and Greg Edvenson and Jordan Fish and Adina Howe and Eric McDonald and Joshua Nahum and Kaben Nanlohy and Jason Pell and Jared Simpson and C. S. Welcher and Qingpeng Zhang and C. Titus Brown',
 	"author_email": 'ctb@msu.edu',
+        "maintainer_email": 'mcrusoe@msu.edu',
 	"url": 'http://ged.msu.edu/',
 	"license": 'New BSD License',
 	"packages": [ py_mod, ],
+	"install_requires": "screed>=0.7",
+	"scripts": scripts,
 	"ext_modules": [ extension_mod, ],
+	#"platforms": 'TODO', #??
+	"package_dir": { 'khmer': path_join(thisdir, 'khmer')}
     }
+
+os_chdir(zlibdir)
+zlib_status = call('./configure --shared', shell=True)
+zlib_make_status = call('make libz.a', shell=True)
+
+os_chdir(bzip2dir)
+bzip2_status = call('make -f Makefile-libbz2_so all', shell=True)
+
+os_chdir(thisdir)
 
 setup( **setup_metadata )
 
