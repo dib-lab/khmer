2ea0554e91fe246573b57c2ea87babb3a6b3532f
Michael Crusoe <mcrusoe@msu.edu>
2013-10-10 18:53:08 -0400

    only run cppcheck and doxygen on Linux

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -16,26 +16,30 @@ python setup.py clean --all
 
 if [[ "${NODE_LABELS}" == *linux* ]]
 then
-	CFLAGS="-pg -fprofile-arcs -ftest-coverage" python setup.py build_ext --debug --inplace --libraries gcov
+	CFLAGS="-pg -fprofile-arcs -ftest-coverage" python setup.py build_ext \
+		--debug --inplace --libraries gcov
 else
 	python setup.py build_ext
 fi
 
 pip install --quiet nosexcover
-python setup.py nosetests --with-xcoverage --with-xunit --cover-package=khmer --cover-erase --attr=\!known_failing
+python setup.py nosetests --with-xcoverage --with-xunit --cover-package=khmer \
+	--cover-erase --attr=\!known_failing
 
 make doc
 
 pip install --quiet pylint
-pylint -f parseable doc/*.py figuregen/*.py novelty/*.py khmer/*.py sandbox/*.py scripts/*.py tests khmer | tee ../pylint.out
-
-cppcheck --std=posix --platform=unix64 -j8 --enable=all -I lib/ -i lib/zlib/ -i lib/bzip2/ -DVALIDATE_PARTITIONS --xml lib 2> cppcheck-result.xml
-
-mkdir -p doc/doxygen
-doxygen
+pylint -f parseable doc/*.py figuregen/*.py novelty/*.py khmer/*.py sandbox/*.py \
+       	scripts/*.py tests khmer | tee ../pylint.out
 
 if [[ "${NODE_LABELS}" == *linux* ]]
 then
 	pip install -U gcovr
 	gcovr -r $PWD --xml > coverage-gcovr.xml
+
+	cppcheck --std=posix --platform=unix64 -j8 --enable=all -I lib/ -i lib/zlib/ \
+		-i lib/bzip2/ -DVALIDATE_PARTITIONS --xml lib 2> cppcheck-result.xml
+
+	mkdir -p doc/doxygen
+	doxygen
 fi
