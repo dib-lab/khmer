12490f7e876c42039567d38cb396fc8f9a7808b2
Michael Crusoe <mcrusoe@msu.edu>
2013-10-10 18:47:59 -0400

    typo

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -24,8 +24,7 @@ fi
 pip install --quiet nosexcover
 python setup.py nosetests --with-xcoverage --with-xunit --cover-package=khmer --cover-erase --attr=\!known_failing
 
-
-makedoc
+make doc
 
 pip install --quiet pylint
 pylint -f parseable doc/*.py figuregen/*.py novelty/*.py khmer/*.py sandbox/*.py scripts/*.py tests khmer | tee ../pylint.out
