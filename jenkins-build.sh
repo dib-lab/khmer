ad5ad57db3d2712609db31d6faf4bc42e9ae4bee
Michael Crusoe <mcrusoe@msu.edu>
2013-10-10 18:43:06 -0400

    don't estimate coverage on OS X

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -14,10 +14,18 @@ python setup.py clean --all
 #tar czf khmer-cov.tgz cov-int
 #curl --form project=Khmer --form token=${COVERITY_TOKEN} --form email=mcrusoe@msu.edu --form file=@khmer-cov.tgz --form version=0.4.0.0.BUILD_TAG http://scan5.coverity.com/cgi-bin/upload.py
 
-CFLAGS="-pg -fprofile-arcs -ftest-coverage" python setup.py build_ext --debug --inplace --libraries gcov
+if [[ "${NODE_LABELS}" == *linux* ]]
+then
+	CFLAGS="-pg -fprofile-arcs -ftest-coverage" python setup.py build_ext --debug --inplace --libraries gcov
+else
+	python setup.py build_ext
+fi
+
 pip install --quiet nosexcover
 python setup.py nosetests --with-xcoverage --with-xunit --cover-package=khmer --cover-erase --attr=\!known_failing
-make doc
+
+
+makedoc
 
 pip install --quiet pylint
 pylint -f parseable doc/*.py figuregen/*.py novelty/*.py khmer/*.py sandbox/*.py scripts/*.py tests khmer | tee ../pylint.out
@@ -27,5 +35,8 @@ cppcheck --std=posix --platform=unix64 -j8 --enable=all -I lib/ -i lib/zlib/ -i 
 mkdir -p doc/doxygen
 doxygen
 
-pip install -U gcovr
-gcovr -r $PWD --xml > coverage-gcovr.xml
+if [[ "${NODE_LABELS}" == *linux* ]]
+then
+	pip install -U gcovr
+	gcovr -r $PWD --xml > coverage-gcovr.xml
+fi
