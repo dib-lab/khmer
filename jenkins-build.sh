2869b91de401abf7c3cdf16c39f281712b325f40
Michael Crusoe <mcrusoe@msu.edu>
2013-10-15 00:52:47 -0400

    add build changes for 0.6.1

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,26 +1,62 @@
 #!/bin/bash
 
-set -x #print commands as they are run
+cov_analysis_dir=/usr/local/cov-analysis-linux64-6.5.1/bin
+cov_analysis_bin=cov-build
 
 rm -Rf .env build dist khmer/_khmermodule.so
+
 virtualenv .env
 
 . .env/bin/activate
 
 python setup.py clean --all
 
-#PATH=$PATH:/usr/local/cov-analysis-linux64-6.5.1/bin
-#CFLAGS="-pg -fprofile-arcs -ftest-coverage" cov-build --dir cov-int python setup.py build_ext --debug 
-#tar czf khmer-cov.tgz cov-int
-#curl --form project=Khmer --form token=${COVERITY_TOKEN} --form email=mcrusoe@msu.edu --form file=@khmer-cov.tgz --form version=0.4.0.0.BUILD_TAG http://scan5.coverity.com/cgi-bin/upload.py
+unset coverage_pre coverage_post coverity
 
 if [[ "${NODE_LABELS}" == *linux* ]]
 then
-	CFLAGS="-pg -fprofile-arcs -ftest-coverage" python setup.py build_ext \
-		--debug --inplace --libraries gcov
+	if type gcov >/dev/null 2>&1
+	then
+		coverage_pre='CFLAGS="-pg -fprofile-arcs -ftest-coverage"'
+		coverage_post='--debug --inplace --libraries gcov'
+	else
+		echo "gcov was not found, skipping coverage check"
+	fi
 else
-	python setup.py build_ext
+	echo "Not on a Linux node, skipping coverage check"
+		
 fi
+
+if [[ "${JOB_NAME}" == "khmer-multi" ]]
+then
+	if [[ -x ${cov_analysis_dir}/${cov_analysis_bin} ]]
+	then
+		if [[ -v COVERITY_TOKEN ]]
+		then
+			PATH=${PATH}:${cov_analysis_dir}
+			coverity="${cov_analysis_bin} --dir cov-int"
+		else
+			echo "Missing coverity credentials, skipping scan"
+		fi
+	else
+		echo "${cov_analysis_bin} does not exist in \
+			${cov_analysis_dir}. Skipping coverity scan."
+	fi
+else
+	echo "Not the main build so skipping the coverity scan"
+fi
+
+${coverage_pre} ${coverity} python setup.py build_ext ${coverage_post}
+
+if [[ -v coverity ]]
+then
+	tar czf khmer-cov.tgz cov-int
+	curl --form project=Khmer --form token=${COVERITY_TOKEN} --form \
+		email=mcrusoe@msu.edu --form file=@khmer-cov.tgz --form \
+		version=0.0.0.${BUILD_TAG} \
+		http://scan5.coverity.com/cgi-bin/upload.py
+fi
+
 
 pip install --quiet nosexcover
 python setup.py nosetests --with-xcoverage --with-xunit --cover-package=khmer \
@@ -32,7 +68,7 @@ pip install --quiet pylint
 pylint -f parseable doc/*.py figuregen/*.py novelty/*.py khmer/*.py sandbox/*.py \
        	scripts/*.py tests khmer | tee ../pylint.out
 
-if [[ "${NODE_LABELS}" == *linux* ]]
+if [[ -v coverage_pre ]]
 then
 	pip install -U gcovr
 	gcovr -r $PWD --xml > coverage-gcovr.xml
