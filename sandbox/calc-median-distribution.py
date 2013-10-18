402c8d8ab704abb1559ce815a35032f2cdbd5d0c
C. Titus Brown <titus@idyll.org>
2013-10-18 02:12:57 -0400

    updates to calc-median-distribution in sandbox/

--- sandbox/calc-median-distribution.py
+++ sandbox/calc-median-distribution.py
@@ -31,6 +31,10 @@ def main():
 
     hist = {}
 
+
+    for i in range(65536):
+       hist[i] = 0
+
     for n, record in enumerate(screed.open(seqfile)):
         if n > 0 and n % 100000 == 0:
             print '...', n
@@ -38,12 +42,19 @@ def main():
         seq = record.sequence.replace('N', 'A')
         med, _, _ = ht.get_median_count(seq)
 
-        hist[med] = hist.get(med, 0) + 1
+        hist[med] = hist[med] + 1
+
+    histlist = list(hist.items())
+    histlist.sort()
 
     maxk = max(hist.keys())
+    sumk = sum(hist.values())
 
-    for i in range(maxk + 1):
-        outfp.write('%d %d\n' % (i, hist.get(i, 0)))
+    sofar = 0
+    for n, m in histlist:
+        sofar += m
+        percent = float(sofar) / sumk
+        outfp.write('%d %d %d %.3f\n' % (n, m, sofar, percent))
     outfp.close()
 
 if __name__ == '__main__':
