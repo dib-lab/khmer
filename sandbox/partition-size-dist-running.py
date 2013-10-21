67b1586da1026dd98d4f8fd62b06fded237af1b5
C. Titus Brown <titus@idyll.org>
2013-10-21 11:21:31 -0400

    misc update to inf assembly script

--- sandbox/partition-size-dist-running.py
+++ sandbox/partition-size-dist-running.py
@@ -11,12 +11,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("readfiles", nargs="+")
     args = parser.parse_args()
+    outfp = open('out', 'w')
 
     kh = khmer.new_counting_hash(K, X, N)
     
     for filename in args.readfiles:
         for n, record in enumerate(screed.open(filename)):
-            if n % 10000 == 0:
+            if n % 1000 == 0:
                 print '...', n
                 p = kh.do_subset_partition_with_abundance(10, 255)
 
@@ -24,8 +25,16 @@ def main():
                     p_sizes = dict(p.partition_sizes()[0])
                     p_cov = dict(p.partition_average_coverages(kh))
 
-                    for k in p_sizes:
-                        print '\tp %d: size %d, avg cov %d' % (k, p_sizes[k], p_cov[k])
+                    p_sizes_items = p_sizes.items()
+                    p_sizes_items.sort(key=lambda x:x[1], reverse=True)
+
+                    for k, size in p_sizes_items[:5]:
+                        print '\tp %d: size %d, avg cov %d' % (k, size, p_cov[k])
+
+                    largest_p = p_sizes_items[0][0]
+                    if p_cov[largest_p] > 100:
+                        pass
+#break
 
             seq = record.sequence.upper()
 
@@ -37,6 +46,8 @@ def main():
                 pass
             else:                   # medium coverage? keep, sort of :)
                 kh.consume_high_abund_kmers(seq, 20)
+
+            outfp.write('>%s\n%s\n' % (record.name, record.sequence))
 
 if __name__ == '__main__':
     main()
