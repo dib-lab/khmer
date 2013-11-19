bc794aeca4416955df16e5c4c5ac7f846eb5f2cc
Chris Welcher <cs.welcher@gmail.com>
2013-11-19 18:10:57 -0500

    done for the day

--- sandbox/build-sparse-graph.py
+++ sandbox/build-sparse-graph.py
@@ -1,6 +1,7 @@
 import khmer
 import sys
 import screed
+import graph_tool.all as gt
 
 
 input_fasta = sys.argv[3]
@@ -10,4 +11,7 @@ x = sys.argv[2]
 
 ht = khmer.new_hashbits(K, x, 4)
 
+sparse_graph = gt.Graph()
 
+for n, record in enumerate(screed.open(input_fasta)):
+    
