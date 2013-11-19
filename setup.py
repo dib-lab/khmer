aae2b9e0faac5ed0ed256180b28f2e18bb253346
Michael R. Crusoe <mcrusoe@msu.edu>
2013-11-19 14:53:48 -0500

    add FORCE directives to Makefile. Try out MANIFEST.in plus find_packages.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 import ez_setup
 ez_setup.use_setuptools()
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools import Extension
 
 import versioneer
@@ -102,7 +102,7 @@ setup_metadata = \
         "description": 'khmer k-mer counting library',
         "long_description": open("README.rst").read(),
         "author": 'Michael R. Crusoe, Greg Edvenson, Jordan Fish,'
-        ' Adina Howe, Eric McDonald, Joshua Nahum, Kaben Nanlohy'
+        ' Adina Howe, Eric McDonald, Joshua Nahum, Kaben Nanlohy,'
         ' Jason Pell, Jared Simpson, C. S. Welcher,'
         ' Qingpeng Zhang, and C. Titus Brown',
         "author_email": 'khmer-project@idyll.org',
@@ -111,9 +111,9 @@ setup_metadata = \
         #http://docs.python.org/2/distutils/setupscript.html
         # #additiona-meta-data note #3
         "url": 'http://ged.msu.edu/',
-        "packages": ['khmer'],
+        "packages": find_packages(),
         "install_requires": [ 'screed >= 0.7', 'argparse >= 1.2.1', ],
-        "setup_requires": [ 'nose >= 1.0', 'setuptools-git >= 0.3', 'sphinx', ],
+        "setup_requires": [ 'nose >= 1.0', 'setuptools-git >= 1.0', 'sphinx', ],
         "scripts": scripts,
         "ext_modules": [extension_mod, ],
         #"platforms": '', # empty as is conveyed by the classifiers below
