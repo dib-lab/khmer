f7fbc6d7b7a9a687e74d1252c9751a2ecf0d7b72
Jordan Fish <jrdn.fish@gmail.com>
2013-12-22 20:21:43 -0500

    Fixed comments in read_aligner.[cc/hh]

--- lib/read_aligner.cc
+++ lib/read_aligner.cc
@@ -1,8 +1,8 @@
-/*
-  file: read_aligner.cc
-  author: Jordan Fish
-  purpose: align...reads?
-*/
+//
+// This file is part of khmer, http://github.com/ged-lab/khmer/, and is
+// Copyright (C) Michigan State University, 2009-2013. It is licensed under
+// the three-clause BSD license; see doc/LICENSE.txt. Contact: ctb@msu.edu
+//
 #include "read_aligner.hh"
 
 namespace khmer {
