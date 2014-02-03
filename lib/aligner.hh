a46ec79f045c16e8980048d84d33672e25599299
Michael Crusoe <mcrusoe@msu.edu>
2014-02-03 17:42:51 -0500

    FORMATTING ONLY; astyle -A10 for previous commit

--- lib/aligner.hh
+++ lib/aligner.hh
@@ -14,101 +14,103 @@
 #include <set>
 #include <vector>
 
-namespace khmer {
+namespace khmer
+{
 
-class CandidateAlignment {
+class CandidateAlignment
+{
 public:
-   std::map<int,int> readDeletions;
-   std::string alignment;
+    std::map<int,int> readDeletions;
+    std::string alignment;
 
-   CandidateAlignment(std::map<int,int> _readDel, std::string _aln)
-   {
-      readDeletions = _readDel;
-      alignment = _aln;
-   }
+    CandidateAlignment(std::map<int,int> _readDel, std::string _aln) {
+        readDeletions = _readDel;
+        alignment = _aln;
+    }
 
-   CandidateAlignment() {
-      alignment = "";
-   }
+    CandidateAlignment() {
+        alignment = "";
+    }
 
-   bool operator<(const CandidateAlignment& param) const {
-      return alignment < param.alignment;
-   }
+    bool operator<(const CandidateAlignment& param) const {
+        return alignment < param.alignment;
+    }
 
-   std::string getReadAlignment(std::string seq) {
-      int tmpDels = 0;
-      int readIndex = 0;
-      std::string readAlign;
+    std::string getReadAlignment(std::string seq) {
+        int tmpDels = 0;
+        int readIndex = 0;
+        std::string readAlign;
 
-      for (int i = 0; i < (int)alignment.length(); i++) {
-         if (tmpDels > 0) {
-            readAlign += '-';
-         } else {
-            readAlign += seq[readIndex];
-         }
-   
-         if (tmpDels == 0 && readDeletions.count(readIndex+1) == 0) {
-            readIndex++;
-         } else if (tmpDels > 0) {
-            tmpDels--;
-
-            if (tmpDels == 0) {
-               readIndex++;
+        for (int i = 0; i < (int)alignment.length(); i++) {
+            if (tmpDels > 0) {
+                readAlign += '-';
+            } else {
+                readAlign += seq[readIndex];
             }
-         } else {
-            tmpDels = readDeletions[readIndex+1];
-         }
-      }
 
-      return readAlign;
-   }
+            if (tmpDels == 0 && readDeletions.count(readIndex+1) == 0) {
+                readIndex++;
+            } else if (tmpDels > 0) {
+                tmpDels--;
 
-   void outputReadDels() {      
-      std::map<int, int>::iterator it;
+                if (tmpDels == 0) {
+                    readIndex++;
+                }
+            } else {
+                tmpDels = readDeletions[readIndex+1];
+            }
+        }
 
-      for (it = readDeletions.begin(); it != readDeletions.end(); it++) {
-         std::cout << "Key: " << (*it).first << " Value: " <<
+        return readAlign;
+    }
+
+    void outputReadDels() {
+        std::map<int, int>::iterator it;
+
+        for (it = readDeletions.begin(); it != readDeletions.end(); it++) {
+            std::cout << "Key: " << (*it).first << " Value: " <<
                       (*it).second << std::endl;
-      }
-   }    
+        }
+    }
 };
 
-class Aligner {
-   khmer::CountingHash * ch;
-   ScoringMatrix * sm;
-   int k;
-   double lambdaOne; // error distribution parameter
-   double lambdaTwo; // non-error distribution parameter
-   unsigned int maxErrorRegion;
+class Aligner
+{
+    khmer::CountingHash * ch;
+    ScoringMatrix * sm;
+    int k;
+    double lambdaOne; // error distribution parameter
+    double lambdaTwo; // non-error distribution parameter
+    unsigned int maxErrorRegion;
 
 public:
-   Node * subalign(Node *, unsigned int, unsigned char, std::set<Node*>&, 
-                  std::vector<Node*>&, const std::string&);
-   std::string extractString(Node*, unsigned char, std::map<int,int>*);
-   CandidateAlignment align(khmer::CountingHash*, const std::string&, 
-                            const std::string&, int);
+    Node * subalign(Node *, unsigned int, unsigned char, std::set<Node*>&,
+                    std::vector<Node*>&, const std::string&);
+    std::string extractString(Node*, unsigned char, std::map<int,int>*);
+    CandidateAlignment align(khmer::CountingHash*, const std::string&,
+                             const std::string&, int);
 
-   Aligner(khmer::CountingHash* _ch, 
-           double lOne=0.0, double lTwo=0.0,
-	   unsigned int maxErrorReg=UINT_MAX) {
-      ch = _ch;
-      sm = new ScoringMatrix();
-      k = ch->ksize();
-      lambdaOne=lOne;
-      lambdaTwo=lTwo;
-      maxErrorRegion = maxErrorReg;
-   }
+    Aligner(khmer::CountingHash* _ch,
+            double lOne=0.0, double lTwo=0.0,
+            unsigned int maxErrorReg=UINT_MAX) {
+        ch = _ch;
+        sm = new ScoringMatrix();
+        k = ch->ksize();
+        lambdaOne=lOne;
+        lambdaTwo=lTwo;
+        maxErrorRegion = maxErrorReg;
+    }
 
-   int ksize() {
-      return k;
-   }
+    int ksize() {
+        return k;
+    }
 
-   ~Aligner() {
-      delete sm;
-   }
+    ~Aligner() {
+        delete sm;
+    }
 
-   void printErrorFootprint(const std::string& read);
-   CandidateAlignment alignRead(const std::string&);
+    void printErrorFootprint(const std::string& read);
+    CandidateAlignment alignRead(const std::string&);
 };
 
 };
--- lib/node.cc
+++ lib/node.cc
@@ -5,230 +5,239 @@
 //
 
 #include "node.hh"
-namespace khmer {
+namespace khmer
+{
 enum { A, C, G, T };
 
-unsigned int factorial(unsigned int n) {
-   int ret = 1;
+unsigned int factorial(unsigned int n)
+{
+    int ret = 1;
 
-   if (n == 0 || n == 1)
-      return 1;
+    if (n == 0 || n == 1) {
+        return 1;
+    }
 
-   for (unsigned int i = 2; i <= n; i++) {
-      ret = ret * i;
-   }
+    for (unsigned int i = 2; i <= n; i++) {
+        ret = ret * i;
+    }
 
-   return ret;
+    return ret;
 }
 
-double pois(double l, unsigned int k) {
-   return (pow(l,k)/factorial(k))*exp(0-l);
+double pois(double l, unsigned int k)
+{
+    return (pow(l,k)/factorial(k))*exp(0-l);
 }
 
-double weight_nonerror(unsigned int kCov, double lambOne, double lambTwo) {
-   return 0-log((pois(lambTwo, kCov))/(pois(lambOne, kCov) + pois(lambTwo, kCov)));
+double weight_nonerror(unsigned int kCov, double lambOne, double lambTwo)
+{
+    return 0-log((pois(lambTwo, kCov))/(pois(lambOne, kCov) + pois(lambTwo, kCov)));
 }
 
-bool isCorrectKmer(unsigned int kCov, double lambOne, double lambTwo) {
-   if (lambOne == 0.0 && lambTwo == 0.0) {
-      return (kCov > 0);
-   }
+bool isCorrectKmer(unsigned int kCov, double lambOne, double lambTwo)
+{
+    if (lambOne == 0.0 && lambTwo == 0.0) {
+        return (kCov > 0);
+    }
 
-   if (pois(lambTwo, kCov) >= pois(lambOne, kCov)) {
-      return true;
-   } else {
-      return false;
-   }
+    if (pois(lambTwo, kCov) >= pois(lambOne, kCov)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-char getNextNucl(int base) {
-   if (base == A) {
-      return 'A';
-   } else if (base == C) {
-      return 'C';
-   } else if (base == G) {
-      return 'G';
-   } else if (base == T) {
-      return 'T';
-   }
-   return '-';
-} 
-
-bool Node::operator== (const Node &param) const {
-   if (!(kmer == param.kmer)) {
-      return 0;
-   }
-
-   if (state != param.state) {
-      return 0;
-   }
-
-   if (stateNo != param.stateNo) {
-      return 0;
-   }
-
-   return 1;
+char getNextNucl(int base)
+{
+    if (base == A) {
+        return 'A';
+    } else if (base == C) {
+        return 'C';
+    } else if (base == G) {
+        return 'G';
+    } else if (base == T) {
+        return 'T';
+    }
+    return '-';
 }
 
-bool Node::operator< (const Node &param) const {
-   if (fval < param.fval) {
-      return 1;
-   }
-   return 0;
+bool Node::operator== (const Node &param) const
+{
+    if (!(kmer == param.kmer)) {
+        return 0;
+    }
+
+    if (state != param.state) {
+        return 0;
+    }
+
+    if (stateNo != param.stateNo) {
+        return 0;
+    }
+
+    return 1;
 }
 
-Kmer Node::makeNextKmer(unsigned char forward, char b) {
-   HashIntoType ret_h;
-   HashIntoType ret_r;
-
-   const unsigned int rc_left_shift = kmer.getK()*2 - 2;
-
-   if (forward) {
-      ret_h = next_f(kmer.getH(), b);
-      ret_r = next_r(kmer.getR(), b);
-   } else {
-      ret_h = prev_f(kmer.getH(), b);
-      ret_r = prev_r(kmer.getR(), b);
-   }
-
-   return Kmer(ret_h, ret_r, kmer.getDir(), kmer.getK());
+bool Node::operator< (const Node &param) const
+{
+    if (fval < param.fval) {
+        return 1;
+    }
+    return 0;
 }
 
-Node::Node(Node* _parent, 
-           char _emission, 
-           unsigned int _stateNo, 
+Kmer Node::makeNextKmer(unsigned char forward, char b)
+{
+    HashIntoType ret_h;
+    HashIntoType ret_r;
+
+    const unsigned int rc_left_shift = kmer.getK()*2 - 2;
+
+    if (forward) {
+        ret_h = next_f(kmer.getH(), b);
+        ret_r = next_r(kmer.getR(), b);
+    } else {
+        ret_h = prev_f(kmer.getH(), b);
+        ret_r = prev_r(kmer.getR(), b);
+    }
+
+    return Kmer(ret_h, ret_r, kmer.getDir(), kmer.getK());
+}
+
+Node::Node(Node* _parent,
+           char _emission,
+           unsigned int _stateNo,
            char _state,
-           Kmer _kmer) {
-   parent = _parent;
-   kmer = _kmer;
-   emission = _emission;
-   stateNo = _stateNo;
-   state = _state;
+           Kmer _kmer)
+{
+    parent = _parent;
+    kmer = _kmer;
+    emission = _emission;
+    stateNo = _stateNo;
+    state = _state;
 
-   fval = 0;
-   gval = 0;
-   hval = 0;
+    fval = 0;
+    gval = 0;
+    hval = 0;
 
-   diff = 0;
+    diff = 0;
 
-   bitmask = 0;
-   for (unsigned int i = 0; i < kmer.getK(); i++) {
-      bitmask = (bitmask << 2) | 3;
-   }
+    bitmask = 0;
+    for (unsigned int i = 0; i < kmer.getK(); i++) {
+        bitmask = (bitmask << 2) | 3;
+    }
 }
 
 std::queue<Node*> Node::enumerate(CountingHash* ch,
-                         ScoringMatrix* sm,
-                         unsigned char forward,
-                         const std::string& seq,
-                         double lambdaOne,
-                         double lambdaTwo) {
-   std::queue<Node*> ret;
+                                  ScoringMatrix* sm,
+                                  unsigned char forward,
+                                  const std::string& seq,
+                                  double lambdaOne,
+                                  double lambdaTwo)
+{
+    std::queue<Node*> ret;
 
-   int index;
-   int remaining;
-   double bestMatch = sm->score('A','A');
-   double errorOffset = 20.0;
+    int index;
+    int remaining;
+    double bestMatch = sm->score('A','A');
+    double errorOffset = 20.0;
 
-   if (forward) {
-      index = stateNo + 1;
-      //remaining = kmer.getK(); 
-      remaining = seq.size() - index;     
-   } else {
-      index = stateNo - 1;
-      remaining = index;
-   }
+    if (forward) {
+        index = stateNo + 1;
+        //remaining = kmer.getK();
+        remaining = seq.size() - index;
+    } else {
+        index = stateNo - 1;
+        remaining = index;
+    }
 
-   // loop for matches and insertions
-   for (int i = A; i <= T; i++) {
-      char nextNucl = getNextNucl(i);
-      Kmer nextKmer = makeNextKmer(forward, nextNucl);
+    // loop for matches and insertions
+    for (int i = A; i <= T; i++) {
+        char nextNucl = getNextNucl(i);
+        Kmer nextKmer = makeNextKmer(forward, nextNucl);
 
-      if (!ch->get_count(nextKmer.getUniqueHash())) {
-         continue;
-      }
+        if (!ch->get_count(nextKmer.getUniqueHash())) {
+            continue;
+        }
 
-      int nextKmerCov = ch->get_count(nextKmer.getUniqueHash());
-      // match
-      Node * nextMatch = new Node(this,
-                                nextNucl,
-                                index,
-                                'm',
-                                nextKmer);
-      nextMatch->gval = gval + sm->score(nextNucl, seq[index]);
-      int isCorrect = isCorrectKmer(nextKmerCov, lambdaOne, lambdaTwo);
-      if (!isCorrect)
-      {
-         nextMatch->gval += errorOffset;
-      }
-      nextMatch->hval = bestMatch * remaining;
-      nextMatch->fval = nextMatch->gval + nextMatch->hval;
-      
-      if (nextNucl == seq[index]) {
-         nextMatch->diff = diff;
-      } else {
-         nextMatch->diff = diff + 1;
-      }
+        int nextKmerCov = ch->get_count(nextKmer.getUniqueHash());
+        // match
+        Node * nextMatch = new Node(this,
+                                    nextNucl,
+                                    index,
+                                    'm',
+                                    nextKmer);
+        nextMatch->gval = gval + sm->score(nextNucl, seq[index]);
+        int isCorrect = isCorrectKmer(nextKmerCov, lambdaOne, lambdaTwo);
+        if (!isCorrect) {
+            nextMatch->gval += errorOffset;
+        }
+        nextMatch->hval = bestMatch * remaining;
+        nextMatch->fval = nextMatch->gval + nextMatch->hval;
 
-      if (isCorrect) {
-         ret.push(nextMatch);
-      }  else {
-         delete nextMatch;
-      }
+        if (nextNucl == seq[index]) {
+            nextMatch->diff = diff;
+        } else {
+            nextMatch->diff = diff + 1;
+        }
 
-      // insertion
-      if (state != 'd') {
-         Node * nextIns = new Node(this,
-                                 nextNucl,
-                                 stateNo,
-                                 'i',
-                                 nextKmer);
-         nextIns->gval = gval + sm->score(nextNucl, '-');
-         if (!isCorrect)
-         {
-            nextIns->gval += errorOffset;
-         }
+        if (isCorrect) {
+            ret.push(nextMatch);
+        }  else {
+            delete nextMatch;
+        }
 
-         nextIns->hval = bestMatch * (remaining + 1);
-         nextIns->fval = nextIns->gval + nextIns->hval;
+        // insertion
+        if (state != 'd') {
+            Node * nextIns = new Node(this,
+                                      nextNucl,
+                                      stateNo,
+                                      'i',
+                                      nextKmer);
+            nextIns->gval = gval + sm->score(nextNucl, '-');
+            if (!isCorrect) {
+                nextIns->gval += errorOffset;
+            }
 
-         nextIns->diff = diff + 1;
+            nextIns->hval = bestMatch * (remaining + 1);
+            nextIns->fval = nextIns->gval + nextIns->hval;
 
-         if (isCorrect) {
-            ret.push(nextIns);
-         } else {
-            delete nextIns;
-         }
-      }
-   }
+            nextIns->diff = diff + 1;
 
-   // deletion
-   if (state != 'i') {
-      int kmerCov = ch->get_count(kmer.getUniqueHash());
-      int isCorrect = isCorrectKmer(kmerCov, lambdaOne, lambdaTwo);
-      Node * nextDel = new Node(this,
-                          '-',
-                          index,
-                          'd',
-                          kmer);
-      nextDel->gval = gval + sm->score('-', seq[index]);
-      if (!isCorrect)
-      {
-         nextDel->gval += errorOffset;
-      }
+            if (isCorrect) {
+                ret.push(nextIns);
+            } else {
+                delete nextIns;
+            }
+        }
+    }
 
-      nextDel->hval = bestMatch * remaining;
-      nextDel->fval = nextDel->gval + nextDel->hval;
+    // deletion
+    if (state != 'i') {
+        int kmerCov = ch->get_count(kmer.getUniqueHash());
+        int isCorrect = isCorrectKmer(kmerCov, lambdaOne, lambdaTwo);
+        Node * nextDel = new Node(this,
+                                  '-',
+                                  index,
+                                  'd',
+                                  kmer);
+        nextDel->gval = gval + sm->score('-', seq[index]);
+        if (!isCorrect) {
+            nextDel->gval += errorOffset;
+        }
 
-      nextDel->diff = diff + 1;
+        nextDel->hval = bestMatch * remaining;
+        nextDel->fval = nextDel->gval + nextDel->hval;
 
-      if (isCorrect) {
-         ret.push(nextDel);
-      } else {
-         delete nextDel;
-      }
-   }
+        nextDel->diff = diff + 1;
 
-   return ret;
+        if (isCorrect) {
+            ret.push(nextDel);
+        } else {
+            delete nextDel;
+        }
+    }
+
+    return ret;
 }
 };
