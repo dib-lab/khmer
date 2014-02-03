367e29d343b79d14f2aa41d12c1fd8adc0379c73
Michael Crusoe <mcrusoe@msu.edu>
2014-02-03 17:42:51 -0500

    FORMATTING ONLY; astyle -A10 for previous commit

--- lib/kmer.cc
+++ lib/kmer.cc
@@ -9,72 +9,85 @@
 #include <iostream>
 using namespace std;
 
-namespace khmer {
+namespace khmer
+{
 
-Kmer::Kmer(std::string kmer) {
-   _hash(kmer.c_str(), kmer.size(), h, r);   
-   k = kmer.size();
+Kmer::Kmer(std::string kmer)
+{
+    _hash(kmer.c_str(), kmer.size(), h, r);
+    k = kmer.size();
 
-   if (kmer == _revhash(h, k)) {
-      direction = 1;
-   } else {
-      direction = 0;
-   }
+    if (kmer == _revhash(h, k)) {
+        direction = 1;
+    } else {
+        direction = 0;
+    }
 }
 
-Kmer::Kmer(HashIntoType _h, HashIntoType _r, unsigned char _direction, 
-           unsigned int _k) {
-   h = _h;
-   r = _r;
-   direction = _direction;
-   k = _k;
+Kmer::Kmer(HashIntoType _h, HashIntoType _r, unsigned char _direction,
+           unsigned int _k)
+{
+    h = _h;
+    r = _r;
+    direction = _direction;
+    k = _k;
 }
 
-unsigned char Kmer::getDir() {
-   return direction;
+unsigned char Kmer::getDir()
+{
+    return direction;
 }
 
-HashIntoType Kmer::getUniqueHash() const {
-   return uniqify_rc(h, r);
+HashIntoType Kmer::getUniqueHash() const
+{
+    return uniqify_rc(h, r);
 }
 
-unsigned int Kmer::getK() {
-   return k;
+unsigned int Kmer::getK()
+{
+    return k;
 }
 
-HashIntoType Kmer::getH() {
-   return h;
+HashIntoType Kmer::getH()
+{
+    return h;
 }
 
-HashIntoType Kmer::getR() {
-   return r;
+HashIntoType Kmer::getR()
+{
+    return r;
 }
 
-std::string Kmer::toString() {
-   return _revhash(uniqify_rc(h, r), k); 
+std::string Kmer::toString()
+{
+    return _revhash(uniqify_rc(h, r), k);
 }
 
-std::string Kmer::toStringH() {
-   return _revhash(h, k);
+std::string Kmer::toStringH()
+{
+    return _revhash(h, k);
 }
 
-std::string Kmer::toStringR() {
-   return _revhash(r, k);
+std::string Kmer::toStringR()
+{
+    return _revhash(r, k);
 }
 
-bool Kmer::operator== (const Kmer &b) const {
-   if (getUniqueHash() == b.getUniqueHash()) {
-      return 1;
-   } else {
-      return 0;
-   }
+bool Kmer::operator== (const Kmer &b) const
+{
+    if (getUniqueHash() == b.getUniqueHash()) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
-bool Kmer::operator< (const Kmer &b) const {
-   if (getUniqueHash() < b.getUniqueHash()) {
-      return 1;
-   } else {
-      return 0;
-   }
+bool Kmer::operator< (const Kmer &b) const
+{
+    if (getUniqueHash() < b.getUniqueHash()) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 };
