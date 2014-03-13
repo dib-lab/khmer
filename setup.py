c337f8111bd64b2cd08b8526db3678e5f2097121
Ram RS <ramrs@nyu.edu>
2014-03-13 19:01:39 -0400

    pep8 conformity

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # This file is part of khmer, http://github.com/ged-lab/khmer/, and is
 # Copyright (C) Michigan State University, 2009-2013. It is licensed under
-# the three-clause BSD license; see doc/LICENSE.txt. 
+# the three-clause BSD license; see doc/LICENSE.txt.
 #
 """ Setup for khmer project. """
 
