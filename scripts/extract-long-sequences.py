01cf5e7d5c30f98a9e9717f930cde4612cb66aa5
Michael Wright <wrigh517@gmail.com>
2014-06-05 18:50:17 -0400

    FASTQ and FASTA acceptable. TODO, find acceptable FASTQ output

--- scripts/extract-long-sequences.py
+++ scripts/extract-long-sequences.py
@@ -21,8 +21,8 @@ import sys
 
 def get_parser():
     parser = argparse.ArgumentParser(
-        description='Extracts FASTQ or FASTA sequences longer than argument 
-        specified length.',
+        description='Extracts FASTQ or FASTA sequences longer than argument' 
+        ' specified length.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('input_filenames', help='Input FAST[AQ]'
@@ -38,24 +38,38 @@ def main():
     args = get_parser().parse_args()
     if args.output:
         write_out = open(args.output, 'w')
-
-    for file in args.input_filenames
+    for file in args.input_filenames:
         for record in screed.open(file):
             if len(record['sequence']) >= args.length:
-                if ('+' in file):
-                    print >> sys.stderr, ( '>%s\n%s' % (record['name'], 
-                        record['sequence'],) )
-                else:
-                    if hasattr(record, 'accuracy'):
-                        write_out.write(
-                            '@{name}\n{seq}\n'
-                            '+\n{acc}\n'.format(name=record.name,
-                                                seq=record.sequence,
-                                                acc=record.accuracy))
-                    else:
+                # Emit records if any passed --- From NBM
+                if hasattr(record, 'accuracy'):
+                    outfp.write(
+                        '@{name}\n{seq}\n'
+                        '+\n{acc}\n'.format(name=record.name,
+                                            seq=record.sequence,
+                                            acc=record.accuracy))
+
+
+                    if args.output:
                         write_out.write(
                             '>{name}\n{seq}\n'.format(name=record.name,
                                                         seq=record.sequence))
+                    #for output to stdout
+                    else:
+                        print >> sys.stdout, ( '>%s\n%s' % (record['name'], 
+                            record['sequence'],) )
+                #if FASTQ
+                else:
+                    if args.output:
+                        outfp.write(
+                            '>{name}\n{seq}\n'.format(name=record.name,
+                                                    seq=record.sequence))
+                                                
+                    else:
+                        print >> sys.stdout, (
+                            '>{name}\n{seq}\n'.format(name=record.name,
+                                                    seq=record.sequence))
+
 
 if __name__ == '__main__':
     main()
