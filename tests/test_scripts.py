37a09784656c47d45f9a31a07de868024fde772f
C. Titus Brown <titus@idyll.org>
2014-06-15 07:06:26 -0400

    moved sweep-files and sweep-reads to sandbox/

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1359,171 +1359,6 @@ def test_sample_reads_randomly():
                         '895:1:1:1335:19932', '895:1:1:1340:19387'])
 
 
-def test_sweep_reads():
-    readfile = utils.get_temp_filename('reads.fa')
-    contigfile = utils.get_temp_filename('contigs.fp')
-    in_dir = os.path.dirname(contigfile)
-
-    shutil.copyfile(utils.get_test_data('test-sweep-reads.fa'), readfile)
-    shutil.copyfile(utils.get_test_data('test-sweep-contigs.fp'), contigfile)
-
-    script = scriptpath('sweep-reads.py')
-    args = ['-k', '25', '--prefix', 'test', '--label-by-pid',
-            contigfile, readfile, 'junkfile.fa']
-
-    status, out, err = runscript(script, args, in_dir, fail_ok=True)
-
-    # check if the bad file was skipped without issue
-    assert 'ERROR' in err, err
-    assert 'skipping' in err, err
-
-    out1 = os.path.join(in_dir, 'test_0.fa')
-    out2 = os.path.join(in_dir, 'test_1.fa')
-    mout = os.path.join(in_dir, 'test_multi.fa')
-    oout = os.path.join(in_dir, 'test_orphaned.fa')
-
-    print os.listdir(in_dir)
-
-    seqs1 = set([r.name for r in screed.open(out1)])
-    seqs2 = set([r.name for r in screed.open(out2)])
-    seqsm = set([r.name for r in screed.open(mout)])
-    seqso = set([r.name for r in screed.open(oout)])
-
-    print seqs1
-    print seqs2
-    print seqsm
-    print seqso
-    assert seqs1 == set(['read1_p0\t0', 'read2_p0\t0'])
-    assert seqs2 == set(['read3_p1\t1'])
-    assert (seqsm == set(['read4_multi\t0\t1']) or
-            seqsm == set(['read4_multi\t1\t0']))
-    assert seqso == set(['read5_orphan'])
-
-
-def test_sweep_reads_fq():
-    readfile = utils.get_temp_filename('reads.fa')
-    contigfile = utils.get_temp_filename('contigs.fp')
-    in_dir = os.path.dirname(contigfile)
-
-    shutil.copyfile(utils.get_test_data('test-sweep-reads.fq'), readfile)
-    shutil.copyfile(utils.get_test_data('test-sweep-contigs.fp'), contigfile)
-
-    script = scriptpath('sweep-reads.py')
-    args = ['-k', '25', '--prefix', 'test', '--label-by-pid',
-            contigfile, readfile, 'junkfile.fa']
-
-    status, out, err = runscript(script, args, in_dir, fail_ok=True)
-
-    # check if the bad file was skipped without issue
-    assert 'ERROR' in err, err
-    assert 'skipping' in err, err
-
-    out1 = os.path.join(in_dir, 'test_0.fq')
-    out2 = os.path.join(in_dir, 'test_1.fq')
-    mout = os.path.join(in_dir, 'test_multi.fq')
-    oout = os.path.join(in_dir, 'test_orphaned.fq')
-
-    print open(out1).read()
-
-    print os.listdir(in_dir)
-
-    seqs1 = set([r.name for r in screed.open(out1)])
-    seqs2 = set([r.name for r in screed.open(out2)])
-    seqsm = set([r.name for r in screed.open(mout)])
-    seqso = set([r.name for r in screed.open(oout)])
-
-    print seqs1
-    print seqs2
-    print seqsm
-    print seqso
-    assert seqs1 == set(['read1_p0\t0', 'read2_p0\t0'])
-    assert seqs2 == set(['read3_p1\t1'])
-    assert (seqsm == set(['read4_multi\t0\t1']) or
-            seqsm == set(['read4_multi\t1\t0']))
-    assert seqso == set(['read5_orphan'])
-
-    seqs1 = set([r.accuracy for r in screed.open(out1)])
-    seqs2 = set([r.accuracy for r in screed.open(out2)])
-    seqsm = set([r.accuracy for r in screed.open(mout)])
-    seqso = set([r.accuracy for r in screed.open(oout)])
-
-
-def test_sweep_reads_2():
-
-    infile = utils.get_temp_filename('seqs.fa')
-    inref = utils.get_temp_filename('ref.fa')
-    shutil.copyfile(utils.get_test_data('random-20-X2.fa'), infile)
-    shutil.copyfile(utils.get_test_data('random-20-a.fa'), inref)
-    wdir = os.path.dirname(inref)
-    script = scriptpath('sweep-reads.py')
-    args = ['-m', '50', '-k', '20', '-l', '9', '-b', '60', '--prefix',
-            'test', '--label-by-seq', inref, infile]
-    status, out, err = runscript(script, args, wdir)
-
-    for i in xrange(99):
-        p = os.path.join(wdir, 'test_{i}.fa'.format(i=i))
-        print p, err, out
-        assert os.path.exists(p)
-        os.remove(p)
-    assert os.path.exists(os.path.join(wdir, 'test.counts.csv'))
-    assert os.path.exists(os.path.join(wdir, 'test.dist.txt'))
-    assert not os.path.exists(os.path.join(wdir, 'test_multi.fa'))
-
-
-def test_sweep_reads_3():
-
-    infile = utils.get_temp_filename('seqs.fa')
-    shutil.copyfile(utils.get_test_data('random-20-a.fa'), infile)
-    wdir = os.path.dirname(infile)
-    script = scriptpath('sweep-reads.py')
-    args = ['-m', '75', '-k', '20', '-l', '1', '--prefix',
-            'test', '--label-by-group', '10', infile, infile]
-    status, out, err = runscript(script, args, wdir)
-
-    for i in xrange(10):
-        p = os.path.join(wdir, 'test_{i}.fa'.format(i=i))
-        print p, err, out
-        assert os.path.exists(p)
-        os.remove(p)
-
-    counts_fn = os.path.join(wdir, 'test.counts.csv')
-    with open(counts_fn) as cfp:
-        for line in cfp:
-            _, _, c = line.partition(',')
-            assert int(c) in [9, 10]
-
-    assert os.path.exists(counts_fn)
-    assert os.path.exists(os.path.join(wdir, 'test.dist.txt'))
-    assert not os.path.exists(os.path.join(wdir, 'test_multi.fa'))
-
-
-def test_count_overlap():
-    seqfile1 = utils.get_temp_filename('test-overlap1.fa')
-    in_dir = os.path.dirname(seqfile1)
-    seqfile2 = utils.get_temp_filename('test-overlap2.fa', in_dir)
-    outfile = utils.get_temp_filename('overlap.out', in_dir)
-    curvefile = utils.get_temp_filename('overlap.out.curve', in_dir)
-    shutil.copy(utils.get_test_data('test-overlap1.fa'), seqfile1)
-    shutil.copy(utils.get_test_data('test-overlap2.fa'), seqfile2)
-    htfile = _make_graph(seqfile1, ksize=20)
-    script = scriptpath('count-overlap.py')
-    args = ['--ksize', '20', '--n_tables', '2', '--min-tablesize', '10000000',
-            htfile + '.pt', seqfile2, outfile]
-    (status, out, err) = runscript(script, args, in_dir)
-    assert status == 0
-    assert os.path.exists(outfile), outfile
-    data = [x.strip() for x in open(outfile)]
-    data = set(data)
-    assert '# of unique k-mers in dataset2: 759047' in data
-    assert '# of overlap unique k-mers: 245621' in data
-    assert os.path.exists(curvefile), curvefile
-    data = [x.strip() for x in open(curvefile)]
-    data = set(data)
-    assert '178633 1155' in data
-    assert '496285 2970' in data
-    assert '752053 238627' in data
-
-
 def test_fastq_to_fasta():
 
     script = scriptpath('fastq-to-fasta.py')
@@ -1650,3 +1485,32 @@ def test_sample_reads_randomly_S():
                         '895:1:1:1340:19387', '895:1:1:1252:19493',
                         '895:1:1:1381:7062', '895:1:1:1383:3089',
                         '895:1:1:1342:20695', '895:1:1:1303:6251'])
+
+
+
+
+def test_count_overlap():
+    seqfile1 = utils.get_temp_filename('test-overlap1.fa')
+    in_dir = os.path.dirname(seqfile1)
+    seqfile2 = utils.get_temp_filename('test-overlap2.fa', in_dir)
+    outfile = utils.get_temp_filename('overlap.out', in_dir)
+    curvefile = utils.get_temp_filename('overlap.out.curve', in_dir)
+    shutil.copy(utils.get_test_data('test-overlap1.fa'), seqfile1)
+    shutil.copy(utils.get_test_data('test-overlap2.fa'), seqfile2)
+    htfile = _make_graph(seqfile1, ksize=20)
+    script = scriptpath('count-overlap.py')
+    args = ['--ksize', '20', '--n_tables', '2', '--min-tablesize', '10000000',
+            htfile + '.pt', seqfile2, outfile]
+    (status, out, err) = runscript(script, args, in_dir)
+    assert status == 0
+    assert os.path.exists(outfile), outfile
+    data = [x.strip() for x in open(outfile)]
+    data = set(data)
+    assert '# of unique k-mers in dataset2: 759047' in data
+    assert '# of overlap unique k-mers: 245621' in data
+    assert os.path.exists(curvefile), curvefile
+    data = [x.strip() for x in open(curvefile)]
+    data = set(data)
+    assert '178633 1155' in data
+    assert '496285 2970' in data
+    assert '752053 238627' in data
