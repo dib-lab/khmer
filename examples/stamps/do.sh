ff60f896da7bcdf8f5e42ef2bcf330efad1d7536
Will Trimble <trimble@anl.gov>
2014-07-23 13:52:24 -0500

    parameter has been renamed

--- examples/stamps/do.sh
+++ examples/stamps/do.sh
@@ -6,7 +6,7 @@
 #
 python ../../scripts/load-into-counting.py -x 1e8 -k 20 stamps-reads.kh ../../data/stamps-reads.fa.gz 
 python ../../scripts/abundance-dist.py stamps-reads.kh ../../data/stamps-reads.fa.gz stamps-reads.hist
-python ../../scripts/normalize-by-median.py -k 20 -C 10 -x 1e8 ../../data/stamps-reads.fa.gz --savehash stamps-dn.kh
+python ../../scripts/normalize-by-median.py -k 20 -C 10 -x 1e8 ../../data/stamps-reads.fa.gz --savetable stamps-dn.kh
 python ../../scripts/abundance-dist.py stamps-dn.kh stamps-reads.fa.gz.keep stamps-dn.hist
 python ../../scripts/do-partition.py -k 32 -x 1e8 -s 1e4 -T 8 stamps-part ../../data/stamps-reads.fa.gz 
 python ../../sandbox/error-correct-pass2.py -C 10 stamps-dn.kh ../../data/stamps-reads.fa.gz 
@@ -20,6 +20,6 @@ python ../../scripts/abundance-dist.py stamps-part.g0.kh stamps-part.group0000.f
 python ../../scripts/abundance-dist.py stamps-part.g1.kh stamps-part.group0001.fa stamps-part.g1.hist
 
 python ../../scripts/filter-abund.py stamps-dn.kh stamps-reads.fa.gz.keep
-python ../../scripts/normalize-by-median.py -x 1e8 -k 20 -C 10 stamps-reads.fa.gz.keep.abundfilt --savehash stamps-dn3.kh
+python ../../scripts/normalize-by-median.py -x 1e8 -k 20 -C 10 stamps-reads.fa.gz.keep.abundfilt --savetable stamps-dn3.kh
 
 python ../../scripts/abundance-dist.py stamps-dn3.kh stamps-reads.fa.gz.keep.abundfilt.keep stamps-dn3.hist
