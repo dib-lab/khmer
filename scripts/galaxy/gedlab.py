41b87701dc0815931a91bf7c9cc7b95a5f599cc5
Michael R. Crusoe <mcrusoe@msu.edu>
2014-08-05 16:37:41 -0400

    PEP8

--- scripts/galaxy/gedlab.py
+++ scripts/galaxy/gedlab.py
@@ -9,16 +9,17 @@ import logging
 
 log = logging.getLogger(__name__)
 
-class Count( Binary ):
 
-    def __init__( self, **kwd ):
-        Binary.__init__( self, **kwd )
+class Count(Binary):
+
+    def __init__(self, **kwd):
+        Binary.__init__(self, **kwd)
 
 
-class Presence( Binary ):
+class Presence(Binary):
 
-    def __init__( self, **kwd ):
-        Binary.__init__( self, **kwd )
+    def __init__(self, **kwd):
+        Binary.__init__(self, **kwd)
 
 Binary.register_unsniffable_binary_ext("ct")
 Binary.register_unsniffable_binary_ext("pt")
