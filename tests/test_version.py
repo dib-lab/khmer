85fbb122fec3f9674ebc6c097553ba3a0ff8203a
Justin Lippi <jlippi@gmail.com>
2014-08-05 22:06:49 -0400

    naming changes

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,6 @@
 #
 # This file is part of khmer, http://github.com/ged-lab/khmer/, and is
-# Copyright (C) Michigan State University, 2009-2013. It is licensed under
+# Copyright (C) Michigan State University, 2014. It is licensed under
 # the three-clause BSD license; see doc/LICENSE.txt.
 # Contact: khmer-project@idyll.org
 #
@@ -10,10 +10,10 @@ import versioneer
 import os
 
 
-@attr ('jenkins')
+@attr('jenkins')
 def test_python_and_c_match():
-    #checks c++ compiler option version against versioneer version
+    # checks c++ compiler option version against versioneer version
     # (respectively)
-    print 'c++ version {}:'.format(khmer.get_version_cpp())
+    print 'c++ version {}:'.format(khmer.__version_cpp__())
     print 'versioneer (python) version: {}'.format(khmer.__version__)
-    assert khmer.get_version_cpp() == khmer.__version__;
+    assert khmer.__version_cpp__() == khmer.__version__
