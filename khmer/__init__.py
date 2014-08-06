a7efa834eb0944ee6d569c6c27bc76e19e1b7547
Justin Lippi <jlippi@gmail.com>
2014-08-05 22:06:49 -0400

    naming changes

--- khmer/__init__.py
+++ khmer/__init__.py
@@ -27,7 +27,8 @@ from khmer._khmer import forward_hash_no_rc  # tests/test_functions.py
 from khmer._khmer import reverse_hash  # tests/test_functions.py
 # tests/counting_single.py
 
-from khmer._khmer import get_version_cpp  # tests/test_version.py
+from khmer._khmer import get_version_cpp as __version_cpp__
+# tests/test_version.py
 
 from khmer._khmer import get_config  # tests/test_read_parsers.py
 # tests/test_khmer_config.py
@@ -108,6 +109,7 @@ def reset_reporting_callback():
     set_reporting_callback(_default_reporting_callback)
 
 reset_reporting_callback()
+
 
 def extract_hashbits_info(filename):
     """Open the given hashbits file and return a tuple of information.
