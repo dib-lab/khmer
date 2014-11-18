12b196dd157a70ab26031410b2941fc821311bd0
Jessica Mizzi <mizzijes@msu.edu>
2014-11-18 16:51:12 -0500

    Fixed typo

--- scripts/do-partition.py
+++ scripts/do-partition.py
@@ -97,7 +97,7 @@ def get_parser():
     parser.add_argument('graphbase', help="base name for output files")
     parser.add_argument('input_filenames', metavar='input_sequence_filename',
                         nargs='+', help='input FAST[AQ] sequence filenames')
-    parser.add_argument('-f', '-force', default=False, action='store_true',
+    parser.add_argument('-f', '--force', default=False, action='store_true',
                         help='Overwrite output file if it exists')
     return parser
 
