239d3ca27cc78e8b64aecde224ba01f93c70b948
Michael R. Crusoe <mcrusoe@msu.edu>
2014-12-05 13:11:03 -0500

    PEP8

--- khmer/khmer_args.py
+++ khmer/khmer_args.py
@@ -169,12 +169,12 @@ def add_threading_args(parser):
                         help='Number of simultaneous threads to execute')
 
 _algorithms = {
-    'software': 'MR Crusoe et al., ' \
-        '2014. http://dx.doi.org/10.6084/m9.figshare.979190',
+    'software': 'MR Crusoe et al., '
+    '2014. http://dx.doi.org/10.6084/m9.figshare.979190',
     'diginorm': 'CT Brown et al., arXiv:1203.4802 [q-bio.GN]',
     'graph': 'J Pell et al., http://dx.doi.org/10.1073/pnas.1121464109',
-    'counting': 'Q Zhang et al., ' \
-        'http://dx.doi.org/10.1371/journal.pone.0101271',
+    'counting': 'Q Zhang et al., '
+    'http://dx.doi.org/10.1371/journal.pone.0101271',
     'sweep': 'C Scott, MR Crusoe, and CT Brown, unpublished'
 }
 
