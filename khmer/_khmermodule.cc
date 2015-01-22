db6a629eafd0bbb7f3284c912fa35d37486950db
Luiz Irber <luiz.irber@gmail.com>
2015-01-22 18:09:36 -0500

    Fix docstrings

--- khmer/_khmermodule.cc
+++ khmer/_khmermodule.cc
@@ -4351,12 +4351,13 @@ static PyMethodDef khmer_hllcounter_methods[] = {
     {
         "consume_string", (PyCFunction)hllcounter_consume_string,
         METH_VARARGS,
-        "Break a sequence into k-mer and add each k-mer to the counter."
+        "Break a sequence into k-mers and add each k-mer to the counter."
     },
     {
         "consume_fasta", (PyCFunction)hllcounter_consume_fasta,
         METH_VARARGS,
-        "Read sequences from file and add each k-mer to the counter."
+        "Read sequences from file, break into k-mers, "
+        "and add each k-mer to the counter."
     },
     {NULL} /* Sentinel */
 };
@@ -4383,7 +4384,7 @@ static PyTypeObject khmer_KHLLCounter_Type = {
     0,                                         /* tp_setattro */
     0,                                         /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,  /* tp_flags */
-    "HyperLogLog counter object",              /* tp_doc */
+    "HyperLogLog counter",                     /* tp_doc */
     0,                                         /* tp_traverse */
     0,                                         /* tp_clear */
     0,                                         /* tp_richcompare */
