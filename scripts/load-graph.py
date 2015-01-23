cd2ca6b9ad32cbd7d13133654dd7fd62a5fd1ab9
Jake Fenton <bocajnotnef@gmail.com>
2015-01-23 10:29:42 -0500

    Updated load-graph to use oxli/build-graph

--- scripts/load-graph.py
+++ scripts/load-graph.py
@@ -18,108 +18,39 @@ import sys
 import threading
 
 import khmer
+import argparse
 from khmer.khmer_args import build_hashbits_args
 from khmer.khmer_args import (report_on_config, info, add_threading_args)
 from khmer.kfile import check_file_status, check_space
 from khmer.kfile import check_space_for_hashtable
+from oxli import build_graph, common
 
 
 def get_parser():
-    parser = build_hashbits_args(descr="Load sequences into the compressible "
-                                 "graph format plus optional tagset.")
-    add_threading_args(parser)
-    parser.add_argument('--no-build-tagset', '-n', default=False,
-                        action='store_true', dest='no_build_tagset',
-                        help='Do NOT construct tagset while loading sequences')
-    parser.add_argument('output_filename',
-                        metavar='output_presence_table_filename', help='output'
-                        ' k-mer presence table filename.')
-    parser.add_argument('input_filenames', metavar='input_sequence_filename',
-                        nargs='+', help='input FAST[AQ] sequence filename')
-    parser.add_argument('--report-total-kmers', '-t', action='store_true',
-                        help="Prints the total number of k-mers to stderr")
-    parser.add_argument('--write-fp-rate', '-w', action='store_true',
-                        help="Write false positive rate into .info file")
-    parser.add_argument('-f', '--force', default=False, action='store_true',
-                        help='Overwrite output file if it exists')
+    parser = argparse.ArgumentParser(description="Load the sequences into the "
+                                     "compressible graph format plus "
+                                     "optional tagset.")
+
+    build_graph.add_args(parser)
+    common.add_hashbits_args(parser)
     return parser
 
 
 def main():
     info('load-graph.py', ['graph', 'SeqAn'])
     args = get_parser().parse_args()
-    report_on_config(args, hashtype='hashbits')
+    report_on_config(args)
 
-    base = args.output_filename
-    filenames = args.input_filenames
+    # handle argparse file things in a way that functions can handle
+    ifiles = []
+    for element in args.input_filenames:
+        ifiles.append(element)
 
-    for _ in args.input_filenames:
-        check_file_status(_, args.force)
-
-    check_space(args.input_filenames, args.force)
-    check_space_for_hashtable(
-        (float(args.n_tables * args.min_tablesize) / 8.), args.force)
-
-    print >>sys.stderr, 'Saving k-mer presence table to %s' % base
-    print >>sys.stderr, 'Loading kmers from sequences in %s' % repr(filenames)
-    if args.no_build_tagset:
-        print >>sys.stderr, 'We WILL NOT build the tagset.'
-    else:
-        print >>sys.stderr, 'We WILL build the tagset', \
-                            ' (for partitioning/traversal).'
-
-    print >>sys.stderr, 'making k-mer presence table'
-    htable = khmer.new_hashbits(args.ksize, args.min_tablesize, args.n_tables)
-
-    if args.no_build_tagset:
-        target_method = htable.consume_fasta_with_reads_parser
-    else:
-        target_method = htable.consume_fasta_and_tag_with_reads_parser
-
-    for _, filename in enumerate(filenames):
-        rparser = khmer.ReadParser(filename)
-        threads = []
-        print >>sys.stderr, 'consuming input', filename
-        for num in xrange(args.threads):
-            cur_thread = threading.Thread(
-                target=target_method, args=(rparser,))
-            threads.append(cur_thread)
-            cur_thread.start()
-
-        for thread in threads:
-            thread.join()
-
-    if args.report_total_kmers:
-        print >> sys.stderr, 'Total number of unique k-mers: {0}'.format(
-            htable.n_unique_kmers())
-
-    print >>sys.stderr, 'saving k-mer presence table in', base + '.pt'
-    htable.save(base + '.pt')
-
-    if not args.no_build_tagset:
-        print >>sys.stderr, 'saving tagset in', base + '.tagset'
-        htable.save_tagset(base + '.tagset')
-
-    info_fp = open(base + '.info', 'w')
-    info_fp.write('%d unique k-mers' % htable.n_unique_kmers())
-
-    fp_rate = khmer.calc_expected_collisions(htable)
-    print >>sys.stderr, 'fp rate estimated to be %1.3f' % fp_rate
-    if args.write_fp_rate:
-        print >> info_fp, \
-            '\nfalse positive rate estimated to be %1.3f' % fp_rate
-
-    if fp_rate > 0.15:          # 0.18 is ACTUAL MAX. Do not change.
-        print >> sys.stderr, "**"
-        print >> sys.stderr, ("** ERROR: the graph structure is too small for "
-                              "this data set. Increase table size/# tables.")
-        print >> sys.stderr, "**"
-        if not args.force:
-            sys.exit(1)
-
-    print >> sys.stderr, 'wrote to', base + '.info and', base + '.pt'
-    if not args.no_build_tagset:
-        print >> sys.stderr, 'and ' + base + '.tagset'
+    build_graph.do_build_graph(args.output_filename, ifiles, args.force,
+                               args.no_build_tagset, args.report_total_kmers,
+                               args.write_fp_rate, args.quiet,
+                               args.ksize, args.n_tables,
+                               args.min_tablesize, args.threads)
 
 if __name__ == '__main__':
     main()
