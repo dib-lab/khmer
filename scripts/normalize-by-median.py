067841e6f082ce1dbdf614bbddf7e0f2f980724a
Tamer Mansour <drtamermansour@gmail.com>
2015-02-24 12:59:51 -0500

    Update normalize-by-median.py

--- scripts/normalize-by-median.py
+++ scripts/normalize-by-median.py
@@ -198,6 +198,8 @@ def get_parser():
                         dest='single_output_filename',
                         default='', help='only output a single'
                         ' file with the specified filename')
+    parser.add_argument('--append', default=False, action='store_true',
+                        help='append reads to the outputfile. Only with -o specified')
     parser.add_argument('input_filenames', metavar='input_sequence_filename',
                         help='Input FAST[AQ] sequence filename.', nargs='+')
     parser.add_argument('--report-total-kmers', '-t', action='store_true',
@@ -241,7 +243,10 @@ def main():  # pylint: disable=too-many-branches,too-many-statements
 
     if args.single_output_filename != '':
         output_name = args.single_output_filename
-        outfp = open(args.single_output_filename, 'a')
+            if append:
+                outfp = open(args.single_output_filename, 'a')
+            else:
+                outfp = open(args.single_output_filename, 'w+')
     else:
         output_name = os.path.basename(input_filename) + '.keep'
         outfp = open(output_name, 'w+')
