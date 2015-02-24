f8499b64492d79db2f37d77b1a89c7187cb8d0d2
Michael R. Crusoe <mcrusoe@msu.edu>
2015-02-24 17:41:23 -0500

    upgrade write-trimmomatic

--- sandbox/write-trimmomatic.py
+++ sandbox/write-trimmomatic.py
@@ -1,27 +1,28 @@
 #! /usr/bin/env python2
 #
 # This file is part of khmer, http://github.com/ged-lab/khmer/, and is
-# Copyright (C) Michigan State University, 2009-2013. It is licensed under
+# Copyright (C) Michigan State University, 2009-2015. It is licensed under
 # the three-clause BSD license; see doc/LICENSE.txt.
 # Contact: khmer-project@idyll.org
 #
 import glob
+import sys
 
-filelist = glob.glob('*R1*.fastq.gz')
+filelist = glob.glob(*R1*.fastq.gz)
 
 for r1 in filelist:
-    r2 = r1.replace('R1', 'R2')
-    final_pe = r1[:-9] + '.pe.fq.gz'
-    final_se = r1[:-9] + '.se.fq.gz'
+    r2 = r1.replace(R1, R2)
+    final_pe = r1[:-9] + .pe.fq.gz
+    final_se = r1[:-9] + .se.fq.gz
     print """\
 mkdir trim
 cd trim
-java -jar /usr/local/bin/trimmomatic-0.30.jar PE ../%s ../%s s1_pe s1_se s2_pe s2_se ILLUMINACLIP:/usr/local/share/adapters/TruSeq3-PE.fa:2:30:10
-/usr/local/share/khmer/scripts/interleave-reads.py s1_pe s2_pe | gzip -9c > ../%s
+TrimmomaticPE ../%s ../%s s1_pe s1_se s2_pe s2_se ILLUMINACLIP:%s:2:30:10
+interleave-reads.py s1_pe s2_pe | gzip -9c > ../%s
 
-cat s1_se s2_se | gzip -9c > ../%s
+cat s1_se s2_se | gzip -9c > ../../trimmed/%s
 cd ..
 rm -r ./trim/
 
 chmod u-w %s %s
-""" % (r1, r2, final_pe, final_se, final_pe, final_se)
+""" % (r1, r2, sys.argv[1], final_pe, final_se, final_pe, final_se)
