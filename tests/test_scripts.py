ac5380899cd15d86cf4159c12a197cf94670341d
Sherine Awad <sherine.awad@gmail.com>
2015-03-05 20:26:08 -0500

    remove extra line

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,5 +1,4 @@
 #
-
 # This file is part of khmer, http://github.com/ged-lab/khmer/, and is
 # Copyright (C) Michigan State University, 2009-2015. It is licensed under
 # the three-clause BSD license; see doc/LICENSE.txt.
