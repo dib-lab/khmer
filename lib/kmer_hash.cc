5c86e2a924e0e81b8fc73061de8662ff72ef46f7
Michael R. Crusoe <mcrusoe@msu.edu>
2015-03-09 20:35:41 -0400

    cleanups to make the diff smaller

--- lib/kmer_hash.cc
+++ lib/kmer_hash.cc
@@ -23,128 +23,117 @@ using namespace std;
 namespace khmer
 {
 
-
 HashIntoType _hash(const char * kmer, const WordLength k,
                    HashIntoType& _h, HashIntoType& _r)
 {
-  // constants FTW
-  
-  // sizeof(HashIntoType) * 8 bits / 2 bits/base
-  if (!(k <= sizeof(HashIntoType)*4) || !(strlen(kmer) >= k)) {
-    throw khmer_exception("Supplied kmer string doesn't match the underlying k-size.");
-  }
+    // sizeof(HashIntoType) * 8 bits / 2 bits/base
+    if (!(k <= sizeof(HashIntoType)*4) || !(strlen(kmer) >= k)) {
+        throw khmer_exception("Supplied kmer string doesn't match the underlying k-size.");
+    }
 
-  HashIntoType h = 0, r = 0;
+    _h = _cyclichash(kmer, k);
+    _r = _revcyclichash(kmer, k);
 
-  h = _cyclichash(kmer, k);
-  r = _revcyclichash(kmer, k);
-
-  _h = h;
-  _r = r;
-
-  return uniqify_rc(h, r);
+    return uniqify_rc(_h, _r);
 }
 
-// Bitwise Shifting 
+// Bitwise Shifting
 HashIntoType _cyclichash(std::string kmer_string, const WordLength k)
 {
-  // Our list of random numbers for each base value
-  // Hard coding in random vals from array
-  // Random values generated from www.random.org
-  HashIntoType vals[] = {303837760, 392233993, 127908739, 491989698}; 
-  //std::vector<HashIntoType> random_base_vals (vals, vals + sizeof(vals) / sizeof(vals));
+    // Our list of random numbers for each base value
+    // Hard coding in random vals from array
+    // Random values generated from www.random.org
+    HashIntoType vals[] = {303837760, 392233993, 127908739, 491989698};
+    //std::vector<HashIntoType> random_base_vals (vals, vals + sizeof(vals) / sizeof(vals));
 
-  HashIntoType hashvalue = 0;
+    HashIntoType hashvalue = 0;
 
-  // Iterate through the kmer, hashing each 
-  for (int i = 0; i < kmer_string.length(); i++) 
-  {
-    // A circular bitshift (http://en.wikipedia.org/wiki/Circular_shift)
-    hashvalue = (hashvalue << 1 | hashvalue >> (k - 1));
-    
-    // Hash each letter of the current kmer
-    switch (kmer_string[i]) 
-    {
-      case 'A':
-      case 'a':
-        hashvalue ^= vals[0];
-      case 'C':
-      case 'c':
-        hashvalue ^= vals[1];
-      case 'G':
-      case 'g':
-        hashvalue ^= vals[2];
-      case 'T':
-      case 't':
-      case 'U':
-      case 'u':
-        hashvalue ^= vals[3];
-      default:
-        throw khmer_exception(); 
-    }// of Switch
-  }
-  return hashvalue;
+    // Iterate through the kmer, hashing each
+    for (int i = 0; i < kmer_string.length(); i++) {
+        // A circular bitshift (http://en.wikipedia.org/wiki/Circular_shift)
+        hashvalue = (hashvalue << 1 | hashvalue >> (k - 1));
+
+        // Hash each letter of the current kmer
+        switch (kmer_string[i]) {
+        case 'A':
+        case 'a':
+            hashvalue ^= vals[0];
+        case 'C':
+        case 'c':
+            hashvalue ^= vals[1];
+        case 'G':
+        case 'g':
+            hashvalue ^= vals[2];
+        case 'T':
+        case 't':
+        case 'U':
+        case 'u':
+            hashvalue ^= vals[3];
+        default:
+            throw khmer_exception();
+        }// of Switch
+    }
+    return hashvalue;
 }
 
 HashIntoType _revcyclichash(std::string kmer_string, const WordLength k)
 {
-  // Our list of random numbers for each base value
-  // Hard coding in random vals from array
-  // Random values generated from www.random.org
-  HashIntoType compliment_vals[] = {491989698, 127908739, 392233993, 303837760}; 
-  //std::vector<HashIntoType> compliment_random_base_vals (vals, vals + sizeof(vals) / sizeof(vals));
+    // Our list of random numbers for each base value
+    // Hard coding in random vals from array
+    // Random values generated from www.random.org
+    HashIntoType compliment_vals[] = {491989698, 127908739, 392233993, 303837760};
+    //std::vector<HashIntoType> compliment_random_base_vals (vals, vals + sizeof(vals) / sizeof(vals));
 
-  HashIntoType hashvalue = 0;
+    HashIntoType hashvalue = 0;
 
-  // Iterate through the kmer backwards, hashing each base 
-  for (int i = kmer_string.length(); i > -1; i--) 
-  {
-    // A circular bitshift (http://en.wikipedia.org/wiki/Circular_shift)
-    hashvalue = (hashvalue << 1 | hashvalue >> (k - 1));
-    
-    // Hash each letter of the current kmer
-    switch (kmer_string[i]) 
-    {
-      case 'A':
-      case 'a':
-        hashvalue ^= compliment_vals[0];
-      case 'C':
-      case 'c':
-        hashvalue ^= compliment_vals[1];
-      case 'G':
-      case 'g':
-        hashvalue ^= compliment_vals[2];
-      case 'T':
-      case 't':
-      case 'U':
-      case 'u':
-        hashvalue ^= compliment_vals[3];
-      default:
-        throw khmer_exception(); 
-    }// of Switch
-  }
-  return hashvalue;
+    // Iterate through the kmer backwards, hashing each base
+    for (int i = kmer_string.length(); i > -1; i--) {
+        // A circular bitshift (http://en.wikipedia.org/wiki/Circular_shift)
+        hashvalue = (hashvalue << 1 | hashvalue >> (k - 1));
+
+        // Hash each letter of the current kmer
+        switch (kmer_string[i]) {
+        case 'A':
+        case 'a':
+            hashvalue ^= compliment_vals[0];
+        case 'C':
+        case 'c':
+            hashvalue ^= compliment_vals[1];
+        case 'G':
+        case 'g':
+            hashvalue ^= compliment_vals[2];
+        case 'T':
+        case 't':
+        case 'U':
+        case 'u':
+            hashvalue ^= compliment_vals[3];
+        default:
+            throw khmer_exception();
+        }// of Switch
+    }
+    return hashvalue;
 }
+
 // _hash: return the maximum of the forward and reverse hash.
 
 HashIntoType _hash(const char * kmer, const WordLength k)
 {
-  HashIntoType h = 0;
-  HashIntoType r = 0;
+    HashIntoType h = 0;
+    HashIntoType r = 0;
 
-  return khmer::_hash(kmer, k, h, r);
+    return khmer::_hash(kmer, k, h, r);
 }
 
 // _hash_forward: return the hash from the forward direction only.
 
 HashIntoType _hash_forward(const char * kmer, WordLength k)
 {
-  HashIntoType h = 0;
-  HashIntoType r = 0;
+    HashIntoType h = 0;
+    HashIntoType r = 0;
 
 
-  khmer::_hash(kmer, k, h, r);
-  return h;			// return forward only
+    khmer::_hash(kmer, k, h, r);
+    return h;			// return forward only
 }
 
 //
@@ -153,20 +142,20 @@ HashIntoType _hash_forward(const char * kmer, WordLength k)
 
 std::string _revhash(HashIntoType hash, WordLength k)
 {
-  std::string s = "";
+    std::string s = "";
 
-  unsigned int val = hash & 3;
-  s += revtwobit_repr(val);
-
-  for (WordLength i = 1; i < k; i++) {
-    hash = hash >> 2;
-    val = hash & 3;
+    unsigned int val = hash & 3;
     s += revtwobit_repr(val);
-  }
 
-  reverse(s.begin(), s.end());
+    for (WordLength i = 1; i < k; i++) {
+        hash = hash >> 2;
+        val = hash & 3;
+        s += revtwobit_repr(val);
+    }
 
-  return s;
+    reverse(s.begin(), s.end());
+
+    return s;
 }
 
 std::string _revcomp(const std::string& kmer)
