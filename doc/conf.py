b2e412bdc51b2dd57f1ced2609caffaddb7d441b
Michael R. Crusoe <mcrusoe@msu.edu>
2015-04-05 07:09:28 -0400

    doc/*.txt -> *.rst

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,8 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.txt'
+source_suffix = '.rst'
+
 
 # The encoding of source files.
 # source_encoding = 'utf-8'
