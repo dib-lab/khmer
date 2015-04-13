411cca54efe7da02ec6b1c401261dc1683c266ad
MagentaShades <sarah@magentashades.com>
2015-04-13 18:36:42 -0400

    updated copyrights in comments fixes issue #482

--- tests/test_read_aligner.py
+++ tests/test_read_aligner.py
@@ -1,6 +1,6 @@
 #
 # This file is part of khmer, http://github.com/ged-lab/khmer/, and is
-# Copyright (C) Michigan State University, 2009-2013. It is licensed under
+# Copyright (C) Michigan State University, 2009-2015. It is licensed under
 # the three-clause BSD license; see doc/LICENSE.txt. Contact: ctb@msu.edu
 #
 import khmer
