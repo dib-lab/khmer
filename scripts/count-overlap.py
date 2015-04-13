33d4b8b24a65e13635a16a1c5e1f97eb9c03b46b
scottsievert <sieve121@umn.edu>
2015-04-13 11:28:07 -0400

    doc/LICENSE.txt => LICENSE
    in tests/, scripts/, sandbox/, lib/

--- scripts/count-overlap.py
+++ scripts/count-overlap.py
@@ -2,7 +2,7 @@
 #
 # This file is part of khmer, http://github.com/ged-lab/khmer/, and is
 # Copyright (C) Michigan State University, 2012-2015. It is licensed under
-# the three-clause BSD license; see doc/LICENSE.txt.
+# the three-clause BSD license; see LICENSE.
 # Contact: khmer-project@idyll.org
 #
 # pylint: disable=missing-docstring,invalid-name
