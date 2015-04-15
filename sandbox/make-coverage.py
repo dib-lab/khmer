d721a5aa8e15d9da001a29262f14336def4bc25b
Michael R. Crusoe <mcrusoe@msu.edu>
2015-04-15 19:12:28 -0400

    put make-coverage prog inside a main

--- sandbox/make-coverage.py
+++ sandbox/make-coverage.py
@@ -9,35 +9,39 @@ import screed
 
 import sys
 
-dbfile = sys.argv[1]
-mapfile = sys.argv[2]
+def main():
+    dbfile = sys.argv[1]
+    mapfile = sys.argv[2]
 
-lengths = {}
-for n, record in enumerate(screed.open(dbfile)):
-    if n % 100000 == 0:
-        print '...', n
-    lengths[record.name] = len(record.sequence)
+    lengths = {}
+    for n, record in enumerate(screed.open(dbfile)):
+        if n % 100000 == 0:
+            print '...', n
+        lengths[record.name] = len(record.sequence)
 
-sums = {}
-for n, line in enumerate(open(mapfile)):
-    if n % 100000 == 0:
-        print '... 2x', n
-    x = line.split('\t')
-    name = x[2]
-    readlen = len(x[4])
-    sums[name] = sums.get(name, 0) + 1
+    sums = {}
+    for n, line in enumerate(open(mapfile)):
+        if n % 100000 == 0:
+            print '... 2x', n
+        x = line.split('\t')
+        name = x[2]
+        readlen = len(x[4])
+        sums[name] = sums.get(name, 0) + 1
 
-mapped_reads = n
+    mapped_reads = n
 
-rpkms = {}
-for k in sums:
-    rpkms[k] = sums[k] * (1000. / float(lengths[k])) * \
-        float(mapped_reads) / 1e6
+    rpkms = {}
+    for k in sums:
+        rpkms[k] = sums[k] * (1000. / float(lengths[k])) * \
+            float(mapped_reads) / 1e6
 
-outfp = open(dbfile + '.cov', 'w')
-for n, record in enumerate(screed.open(dbfile)):
-    if n % 100000 == 0:
-        print '...', n
+    outfp = open(dbfile + '.cov', 'w')
+    for n, record in enumerate(screed.open(dbfile)):
+        if n % 100000 == 0:
+            print '...', n
 
-    print >>outfp, ">%s[cov=%d]\n%s" % (
-        record.name, rpkms.get(record.name, 0), record.sequence)
+        print >>outfp, ">%s[cov=%d]\n%s" % (
+            record.name, rpkms.get(record.name, 0), record.sequence)
+
+if __name__ == '__main__':
+        main()
