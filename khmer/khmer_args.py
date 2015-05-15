2d85c4414f5e4f07f6ec1a6d902d410f88b23a55
Michael R. Crusoe <mcrusoe@msu.edu>
2015-05-15 16:05:40 -0400

    update copyright years for all files touched

--- khmer/khmer_args.py
+++ khmer/khmer_args.py
@@ -1,7 +1,7 @@
 #
 # vim: set encoding=utf-8
 # This file is part of khmer, https://github.com/dib-lab/khmer/, and is
-# Copyright (C) Michigan State University, 2014. It is licensed under
+# Copyright (C) Michigan State University, 2014-2015. It is licensed under
 # the three-clause BSD license; see LICENSE.
 # Contact: khmer-project@idyll.org
 #
--- lib/subset.hh
+++ lib/subset.hh
@@ -1,6 +1,6 @@
 //
 // This file is part of khmer, https://github.com/dib-lab/khmer/, and is
-// Copyright (C) Michigan State University, 2009-2014. It is licensed under
+// Copyright (C) Michigan State University, 2009-2015. It is licensed under
 // the three-clause BSD license; see LICENSE.
 // Contact: khmer-project@idyll.org
 //
