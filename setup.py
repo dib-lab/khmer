684823a88f5bdf4a1253c9a0ace055f9d3be1b27
Camille Scott <camille.scott.w@gmail.com>
2015-06-14 15:32:15 -0700

    Add reminder to update lib/Makefile with flags to setup.py

--- setup.py
+++ setup.py
@@ -114,6 +114,7 @@ SOURCES.extend(path_join("lib", bn + ".cc") for bn in [
 SOURCES.extend(path_join("third-party", "smhasher", bn + ".cc") for bn in [
     "MurmurHash3"])
 
+# Don't forget to update lib/Makefile with these flags!
 EXTRA_COMPILE_ARGS = ['-O3', '-std=c++11']
 EXTRA_LINK_ARGS = []
 
