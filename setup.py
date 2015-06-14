91cacde1b30c409b22685a6d39c28996b3f5c210
Camille Scott <camille.scott.w@gmail.com>
2015-06-14 12:08:58 -0700

    Move -stdlib=libc++ into osx-specific args

--- setup.py
+++ setup.py
@@ -114,12 +114,13 @@ SOURCES.extend(path_join("lib", bn + ".cc") for bn in [
 SOURCES.extend(path_join("third-party", "smhasher", bn + ".cc") for bn in [
     "MurmurHash3"])
 
-EXTRA_COMPILE_ARGS = ['-O3', '-std=c++11', '-stdlib=libc++']
+EXTRA_COMPILE_ARGS = ['-O3', '-std=c++11']
 EXTRA_LINK_ARGS = []
 
 if sys.platform == 'darwin':
     # force 64bit only builds
-    EXTRA_COMPILE_ARGS.extend(['-arch', 'x86_64', '-mmacosx-version-min=10.7'])
+    EXTRA_COMPILE_ARGS.extend(['-arch', 'x86_64', '-mmacosx-version-min=10.7',
+                               '-stdlib=libc++'])
 
 if check_for_openmp():
     EXTRA_COMPILE_ARGS.extend(['-fopenmp'])
