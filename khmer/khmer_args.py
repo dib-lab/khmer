9daf9b197bc8a1776d6f70cd169a9531cc5548b7
C. Titus Brown <titus@idyll.org>
2015-07-19 10:21:35 -0400

    updated citation for semi-streaming paper

--- khmer/khmer_args.py
+++ khmer/khmer_args.py
@@ -226,7 +226,7 @@ _algorithms = {
     'software': 'MR Crusoe et al., '
     '2014. http://dx.doi.org/10.6084/m9.figshare.979190',
     'diginorm': 'CT Brown et al., arXiv:1203.4802 [q-bio.GN]',
-    'streaming': 'Q Zhang, S Awad, CT Brown, unpublished',
+    'streaming': 'Q Zhang, S Awad, CT Brown, https://peerj.com/preprints/890/',
     'graph': 'J Pell et al., http://dx.doi.org/10.1073/pnas.1121464109',
     'counting': 'Q Zhang et al., '
     'http://dx.doi.org/10.1371/journal.pone.0101271',
