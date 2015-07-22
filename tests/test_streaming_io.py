943a550091b1c0755dc4c7bdc186b85425f7f91a
C. Titus Brown <titus@idyll.org>
2015-07-21 20:19:33 -0400

    switched extract-long-sequences.py -o to use argparse.FileType

--- tests/test_streaming_io.py
+++ tests/test_streaming_io.py
@@ -35,6 +35,8 @@ def run_shell_cmd(cmd, fail_ok=False):
     (out, err) = p.communicate()
 
     if p.returncode != 0 and not fail_ok:
+        print('out:', out)
+        print('err:', err)
         raise AssertionError("exit code is non zero: %d" % p.returncode)
 
     return (p.returncode, out, err)
@@ -43,13 +45,13 @@ def run_shell_cmd(cmd, fail_ok=False):
 def test_interleave_split_1():
     in1 = utils.get_test_data('paired.fq.1')
     in2 = utils.get_test_data('paired.fq.2')
-    
+
     out1 = utils.get_temp_filename('a.fa')
     out2 = utils.get_temp_filename('b.fa')
 
     cmd = """
        {scripts}/interleave-reads.py {in1} {in2} -o -             |
-       {scripts}/split-paired-reads.py -1 {out1} -2 {out2}
+       {scripts}/split-paired-reads.py -1 {out1} -2 {out2} -
     """
 
     cmd = cmd.format(scripts=scriptpath(),
@@ -62,6 +64,30 @@ def test_interleave_split_1():
     assert files_are_equal(in2, out2), diff_files(in2, out2)
 
 
+def test_interleave_split_2_fail():
+    in1 = utils.get_test_data('paired.fq.1')
+    in2 = utils.get_test_data('paired.fq.2')
+
+    out1 = utils.get_temp_filename('a.fa')
+    out2 = utils.get_temp_filename('b.fa')
+
+    cmd = """
+       {scripts}/interleave-reads.py {in1} {in2} -o -             |
+       {scripts}/split-paired-reads.py -
+    """
+
+    cmd = cmd.format(scripts=scriptpath(),
+                     in1=in1, in2=in2,
+                     out1=out1, out2=out2)
+
+    (status, out, err) = run_shell_cmd(cmd, fail_ok=True)
+    assert status != 0
+    print(out)
+    print(err)
+    assert "Accepting input from stdin; output filenames must be provided." \
+           in err
+
+
 def test_extract_paired_pe():
     in1 = utils.get_test_data('paired-mixed.fq')
     out_test = utils.get_test_data('paired-mixed.fq.pe')
@@ -69,7 +95,7 @@ def test_extract_paired_pe():
 
     cmd = """
        cat {in1} |
-       {scripts}/extract-paired-reads.py -p - -s /dev/null > {out1}
+       {scripts}/extract-paired-reads.py - -p - -s /dev/null > {out1}
     """
 
     cmd = cmd.format(scripts=scriptpath(), in1=in1, out1=out1)
@@ -86,7 +112,7 @@ def test_extract_paired_se():
 
     cmd = """
        cat {in1} |
-       {scripts}/extract-paired-reads.py -p /dev/null -s - > {out1}
+       {scripts}/extract-paired-reads.py - -p /dev/null -s - > {out1}
     """
 
     cmd = cmd.format(scripts=scriptpath(), in1=in1, out1=out1)
@@ -94,6 +120,26 @@ def test_extract_paired_se():
     run_shell_cmd(cmd)
 
     assert files_are_equal(out1, out_test), diff_files(out1, out_test)
+
+
+def test_extract_paired_se_fail():
+    in1 = utils.get_test_data('paired-mixed.fq')
+    out_test = utils.get_test_data('paired-mixed.fq.se')
+    out1 = utils.get_temp_filename('a.fq')
+
+    cmd = """
+       cat {in1} |
+       {scripts}/extract-paired-reads.py -p /dev/null - > {out1}
+    """
+
+    cmd = cmd.format(scripts=scriptpath(), in1=in1, out1=out1)
+
+    (status, out, err) = run_shell_cmd(cmd, fail_ok=True)
+    assert status != 0
+    print(out)
+    print(err)
+    assert "Accepting input from stdin; output filenames must be provided." \
+           in err
 
 
 def test_norm_by_median_1():
@@ -103,8 +149,8 @@ def test_norm_by_median_1():
 
     cmd = """
        cat {in1} |
-       {scripts}/extract-paired-reads.py -p - -s /dev/null |
-       {scripts}/normalize-by-median.py -p - -o - > {out1}
+       {scripts}/extract-paired-reads.py - -p - -s /dev/null |
+       {scripts}/normalize-by-median.py - -o - > {out1}
     """
 
     cmd = cmd.format(scripts=scriptpath(), in1=in1, out1=out1)
@@ -112,3 +158,76 @@ def test_norm_by_median_1():
     run_shell_cmd(cmd)
 
     assert files_are_equal(out1, out_test), diff_files(out1, out_test)
+
+
+def test_norm_by_median_2_fail():
+    in1 = utils.get_test_data('paired-mixed.fq')
+    out_test = utils.get_test_data('paired-mixed.fq.pe')
+    out1 = utils.get_temp_filename('a.fq')
+
+    cmd = """
+       cat {in1} |
+       {scripts}/extract-paired-reads.py - -p - -s /dev/null |
+       {scripts}/normalize-by-median.py -p - > {out1}
+    """
+
+    cmd = cmd.format(scripts=scriptpath(), in1=in1, out1=out1)
+
+    (status, out, err) = run_shell_cmd(cmd, fail_ok=True)
+    assert status != 0
+    print(out)
+    print(err)
+    assert "Accepting input from stdin; output filename must be provided with"\
+           in err
+
+
+def test_sample_reads_randomly_1():
+    in1 = utils.get_test_data('paired-mixed.fq')
+    out1 = utils.get_temp_filename('a.fq')
+
+    cmd = """
+       cat {in1} |
+       {scripts}/sample-reads-randomly.py - -o - > {out1}
+    """
+
+    cmd = cmd.format(scripts=scriptpath(), in1=in1, out1=out1)
+
+    run_shell_cmd(cmd)
+
+    assert files_are_equal(in1, out1), diff_files(in1, out1)
+
+
+def test_sample_reads_randomly_2_fail():
+    in1 = utils.get_test_data('paired-mixed.fq')
+    out1 = utils.get_temp_filename('a.fq')
+
+    cmd = """
+       cat {in1} |
+       {scripts}/sample-reads-randomly.py - > {out1}
+    """
+
+    cmd = cmd.format(scripts=scriptpath(), in1=in1, out1=out1)
+
+    (status, out, err) = run_shell_cmd(cmd, fail_ok=True)
+    assert status != 0
+    print(out)
+    print(err)
+    assert "Accepting input from stdin; output filename must be provided with"\
+           in err
+
+
+def test_extract_long_sequences_1():
+    in1 = utils.get_test_data('paired-mixed.fa')
+    out1 = utils.get_temp_filename('a.fa')
+
+    cmd = """
+       cat {in1} |
+       {scripts}/extract-long-sequences.py - -l 10 > {out1}
+    """
+
+    cmd = cmd.format(scripts=scriptpath(), in1=in1, out1=out1)
+
+    run_shell_cmd(cmd)
+
+    countlines = sum(1 for line in open(out1))
+    assert countlines == 22, countlines
