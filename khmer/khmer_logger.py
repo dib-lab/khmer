aa573ef15f3620290d4309d89f826428bf27e326
Jake Fenton <bocajnotnef@gmail.com>
2015-07-27 14:58:40 -0400

    pep257, moar funcs

--- khmer/khmer_logger.py
+++ khmer/khmer_logger.py
@@ -13,26 +13,39 @@ import sys
 class Logger(object):
 
     """
-    Object to handle error/warning/info output in scripts
+    Object to handle error/warning/info output in scripts.
 
-    Four levels: info, warn, err, debug
+    Four levels: info, warn, err, debug.
 
-    Info is for status output, written to stderr, is surpressable
-
-    Warn is for warning output, written to stderr, is surpressable
+    Info is for status output, written to stderr, is surpressable.
 
     Err is for critical failures, will not be surpressed when quiet is set; Err
-    output is written to stderr
+    output is written to stderr.
 
-    Debug output is written to stdout and is never surpressed
+    Debug output is written to stdout and is surpressable.
+
+    Warning output is written to stdout and is not surpressable.
     """
 
     def __init__(self, quiet):
         self._quiet = quiet
 
     def log_inf(self, message, **kwargs):
-        """For non-critical informative/status output"""
+        """For non-critical informative/status output to stderr."""
         if not self._quiet:
             print(message.format(**kwargs), file=sys.stderr)
+
+    def log_err(self, message, **kwargs):
+        """For critical error output to stderr."""
+        print(message.format(**kwargs), file=sys.stderr)
+
+    def log_dbg(self, message, **kwagrs):
+        """For non-critical debug output to stdout."""
+        if not self._quiet:
+            print(message.format(**kwargs), file=sys.stdout)
+
+    def log_wrn(self, message, **kwargs):
+        """For critical warning output to stdout."""
+        print(message.format(**kwargs), file=sys.stdout)
 
 # vim: set ft=python ts=4 sts=4 sw=4 et tw=79:
