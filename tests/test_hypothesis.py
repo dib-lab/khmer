ca2718410714ad77af70b83c39e0705296e07b62
Luiz Irber <luiz.irber@gmail.com>
2015-11-06 21:42:57 -0800

    Define a kmer generation strategy

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -1,31 +1,37 @@
-from hypothesis import given, assume
-from hypothesis.strategies import text
+#! /usr/bin/env python
+
+from __future__ import division, unicode_literals
+
+from hypothesis import given, assume, strategies as st
 from nose.plugins.attrib import attr
 
+import khmer
 from khmer import reverse_hash, forward_hash, forward_hash_no_rc
 
-#from hypothesis import Settings, Verbosity
-#Settings.default.verbosity = Verbosity.verbose
+
 TRANSLATE = {'A': 'T', 'C': 'G', 'T': 'A', 'G': 'C'}
+
+KSIZE = 13
+st_kmer = st.text("ACGT", min_size=KSIZE, max_size=KSIZE)
+
+
+def lex_rc(kmer):
+    return reverse_hash(forward_hash(kmer, len(kmer)), len(kmer))
 
 
 @attr('hypothesis')
-@given(text("ACGT"))
+@given(st_kmer)
 def test_forward_hash(kmer):
     ksize = len(kmer)
-    assume(ksize > 0)
-    assume(ksize < 32)
 
     rh = reverse_hash(forward_hash(kmer, ksize), ksize)
     assert rh == kmer or rh == "".join(TRANSLATE[c] for c in kmer[::-1])
 
 
 @attr('hypothesis')
-@given(text("ACGT"))
+@given(st_kmer)
 def test_forward_hash_no_rc(kmer):
     ksize = len(kmer)
-    assume(ksize > 0)
-    assume(ksize < 32)
 
     rh = reverse_hash(forward_hash_no_rc(kmer, ksize), ksize)
     assert rh == kmer
