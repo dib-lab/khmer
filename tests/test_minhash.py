e6ec4ff3fefb806a8f22ab76bfb663af19817a7c
C. Titus Brown <titus@idyll.org>
2016-04-27 19:54:04 -0700

    tests

---
+++ tests/test_minhash.py
@@ -0,0 +1,74 @@
+# This file is part of khmer, https://github.com/dib-lab/khmer/, and is
+# Copyright (C) 2010-2015, Michigan State University.
+# Copyright (C) 2015-2016, The Regents of the University of California.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+#     * Neither the name of the Michigan State University nor the names
+#       of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written
+#       permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Contact: khmer-project@idyll.org
+# pylint: disable=missing-docstring,protected-access
+
+from __future__ import print_function
+from __future__ import absolute_import, unicode_literals
+
+import khmer
+from . import khmer_tst_utils as utils
+
+# add:
+# * get default params from Python
+# * keyword args for minhash constructor
+# * trap error from handing protein/non-DNA to a DNA MH
+
+def test_default_params():
+    mh = khmer.MinHash(100, 32)
+    mh2 = khmer.MinHash(100, 32, 9999999967, False)
+
+    mh.add_hash(9999999968)
+    mh2.add_hash(9999999968)
+    assert mh.get_mins() == mh2.get_mins()
+    assert mh.get_mins()[0] == 1
+
+def test_basic_dna():
+    mh = khmer.MinHash(1, 4)
+    mh.add_sequence('ATGC')
+    a = mh.get_mins()
+
+    mh.add_sequence('GCAT')             # this will not get added; hash > ATGC
+    b = mh.get_mins()
+
+    print(a, b)
+    assert a == b
+    assert len(b) == 1
+
+
+def test_protein():
+    mh = khmer.MinHash(1, 4, 9999999967, True)
+    mh.add_protein('AGYYG')
+
+    assert len(mh.get_mins()) == 1
