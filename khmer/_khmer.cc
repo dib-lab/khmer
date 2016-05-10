c604f38d28791c4e24c480abfbc99927343aff44
C. Titus Brown <titus@idyll.org>
2016-05-10 05:49:26 -0700

    added HashSet type <-> SeenSet

--- khmer/_khmer.cc
+++ khmer/_khmer.cc
@@ -624,9 +624,89 @@ static PyTypeObject khmer_PrePartitionInfo_Type = {
     0,                                    /* tp_setattro */
     0,                                    /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT,                   /* tp_flags */
-    "Stores a k-kmer and a set of tagged seen k-mers.", /* tp_doc */
+    "Stores a k-mer and a set of tagged seen k-mers.", /* tp_doc */
 };
 
+
+/***********************************************************************/
+
+typedef struct {
+    PyObject_HEAD
+    SeenSet * hashes;
+} khmer_HashSet_Object;
+
+static
+void
+khmer_HashSet_dealloc(khmer_HashSet_Object * obj)
+{
+    delete obj->hashes;
+    obj->hashes = NULL;
+    Py_TYPE(obj)->tp_free((PyObject*)obj);
+}
+
+static PyObject* khmer_HashSet_new(PyTypeObject * type, PyObject * args,
+                                   PyObject * kwds)
+{
+    khmer_HashSet_Object * self;
+
+    self = (khmer_HashSet_Object *)type->tp_alloc(type, 0);
+
+    if (self != NULL) {
+        if (!PyArg_ParseTuple(args, "")) {
+            Py_DECREF(self);
+            return NULL;
+        }
+
+        try {
+            self->hashes = new SeenSet;
+        } catch (std::bad_alloc &e) {
+            Py_DECREF(self);
+            return PyErr_NoMemory();
+        }
+    }
+    return (PyObject *) self;
+}
+
+static PyTypeObject khmer_HashSet_Type = {
+    PyVarObject_HEAD_INIT(NULL, 0)        /* init & ob_size */
+    "_khmer.HashSet",                     /* tp_name */
+    sizeof(khmer_HashSet_Object),         /* tp_basicsize */
+    0,                                    /* tp_itemsize */
+    (destructor)khmer_HashSet_dealloc,    /* tp_dealloc */
+    0,                                    /* tp_print */
+    0,                                    /* tp_getattr */
+    0,                                    /* tp_setattr */
+    0,                                    /* tp_compare */
+    0,                                    /* tp_repr */
+    0,                                    /* tp_as_number */
+    0,                                    /* tp_as_sequence */
+    0,                                    /* tp_as_mapping */
+    0,                                    /* tp_hash */
+    0,                                    /* tp_call */
+    0,                                    /* tp_str */
+    0,                                    /* tp_getattro */
+    0,                                    /* tp_setattro */
+    0,                                    /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,                   /* tp_flags */
+    "Stores a set of hashed k-mers.",     /* tp_doc */
+    0,                                    /* tp_traverse */
+    0,                                    /* tp_clear */
+    0,                                    /* tp_richcompare */
+    0,                                    /* tp_weaklistoffset */
+    0,                                    /* tp_iter */
+    0,                                    /* tp_iternext */
+    0,                                    /* tp_methods */
+    0,                                    /* tp_members */
+    0,                                    /* tp_getset */
+    0,                                    /* tp_base */
+    0,                                    /* tp_dict */
+    0,                                    /* tp_descr_get */
+    0,                                    /* tp_descr_set */
+    0,                                    /* tp_dictoffset */
+    0,                                    /* tp_init */
+    0,                                    /* tp_alloc */
+    khmer_HashSet_new,                    /* tp_new */
+};
 
 /***********************************************************************/
 
@@ -4827,6 +4907,12 @@ MOD_INIT(_khmer)
         return MOD_ERROR_VAL;
     }
 
+    // khmer_HashSet_Type.tp_methods = khmer_hashset_methods;
+    khmer_HashSet_Type.tp_new = khmer_HashSet_new;
+    if (PyType_Ready(&khmer_HashSet_Type) < 0) {
+        return MOD_ERROR_VAL;
+    }
+
     if (PyType_Ready(&khmer_KHLLCounter_Type) < 0) {
         return MOD_ERROR_VAL;
     }
@@ -4889,6 +4975,12 @@ MOD_INIT(_khmer)
     Py_INCREF(&khmer_ReadAlignerType);
     if (PyModule_AddObject(m, "ReadAligner",
                            (PyObject *)&khmer_ReadAlignerType) < 0) {
+        return MOD_ERROR_VAL;
+    }
+
+    Py_INCREF(&khmer_HashSet_Type);
+    if (PyModule_AddObject(m, "HashSet",
+                           (PyObject *)&khmer_HashSet_Type) < 0) {
         return MOD_ERROR_VAL;
     }
 
