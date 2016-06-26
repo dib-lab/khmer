7a5a162fb478fe35d5d68d4c07a5c8836888da90
C. Titus Brown <titus@idyll.org>
2016-06-26 14:30:28 -0700

    update copyright year

--- scripts/filter-abund-single.py
+++ scripts/filter-abund-single.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # This file is part of khmer, https://github.com/dib-lab/khmer/, and is
 # Copyright (C) 2013-2015, Michigan State University.
-# Copyright (C) 2015, The Regents of the University of California.
+# Copyright (C) 2015-2016, The Regents of the University of California.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
