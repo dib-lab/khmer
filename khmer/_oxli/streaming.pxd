60f3597c71dcfa6c72d92a11900b218d2bd4bc5e
Camille Scott <camille.scott.w@gmail.com>
2016-11-16 17:09:23 -0800

    Add cythonized BrokenPairedReader

--- khmer/_oxli/streaming.pxd
+++ khmer/_oxli/streaming.pxd
@@ -1,11 +1,19 @@
 from libcpp.memory cimport unique_ptr
 from libcpp cimport bool
+from libcpp.string cimport string
 
 from _oxli cimport CpSequence, CpFastxParser
+
+cdef inline void move_cpsequence(CpSequence& move_from, CpSequence& move_to)
+cdef inline void move_sequence(Sequence move_from, Sequence move_to)
 
 cdef class Sequence:
     cdef unique_ptr[CpSequence] _this
 
+    cdef void take(self, Sequence other)
+    @staticmethod
+    cdef Sequence _create(str name, str sequence, 
+                          str annotations=*, str quality=*)
 
 cdef class SequencePair:
     cdef Sequence first
@@ -18,18 +26,31 @@ cdef class ReadBundle:
 
 cdef class FastxParser:
     cdef unique_ptr[CpFastxParser] _this
+
+    cdef bool _imprint_next(self, Sequence seq)
+    cdef bool _cp_imprint_next(self, CpSequence& seq)
     cdef Sequence _next(self)
 
 
-cpdef tuple broken_paired_reader(FastxParser fastx_iter, int min_length=*,
-                                 bool force_single=*, bool require_paired=*)
+cdef class BrokenPairedReader:
+
+    cdef FastxParser parser
+    cdef readonly int min_length
+    cdef readonly bool force_single
+    cdef readonly bool require_paired
+    cdef readonly Sequence record
+
+    cdef tuple _next(self)
 
 cdef tuple _split_left_right(str name)
 
-cdef bool check_is_pair(Sequence first, Sequence second)
+cdef bool _check_is_pair(Sequence first, Sequence second)
 
 cdef bool check_is_left(str name)
 
 cdef bool check_is_right(str name)
 
+cdef inline bool is_valid_dna(const char base)
+
+cdef inline bool sanitize_sequence(string& sequence)
 
---
+++ tests/test_cython_streaming.py
@@ -0,0 +1,154 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
+import gc
+import itertools
+import random
+
+import khmer
+from khmer._oxli.streaming import Sequence, FastxParser, BrokenPairedReader
+from khmer.khmer_args import estimate_optimal_with_K_and_f as optimal_fp
+from khmer import reverse_complement as revcomp
+from khmer import reverse_hash as revhash
+from . import khmer_tst_utils as utils
+
+import pytest
+import screed
+
+
+def teardown():
+    utils.cleanup()
+
+
+def gather(filename, **kw):
+    stream = FastxParser(str(filename))
+    itr = BrokenPairedReader(stream, **kw)
+
+    x = []
+    m = 0
+    num = 0
+    for num, is_pair, read1, read2 in itr:
+        if is_pair:
+            x.append((read1.name, read2.name))
+        else:
+            x.append((read1.name, None))
+        m += 1
+
+    return x, num, m
+
+
+@pytest.fixture
+def create_fastx(tmpdir):
+    def func(reads, fmt='fa'):
+        assert fmt in ['fa','fq']
+        fastx_fn = tmpdir.join('test.'+fmt)
+        for record in reads:
+            if fmt == 'fa':
+                fastx_fn.write('>{0}\n{1}\n'.format(record.name,
+                                                    record.sequence),
+                               mode='a')
+            else:
+                fastx_fn.write('@{0}\n{1}\n+\n{2}\n'.format(record.name,
+                                                        record.sequence,
+                                                        record.quality),
+                               mode='a')
+        print(fastx_fn.read())
+        return fastx_fn
+    return func
+
+
+def test_BrokenPairedReader_force_single(create_fastx):
+    reads = [Sequence.create('seq1/1', 'A' * 5),
+              Sequence.create('seq1/2', 'A' * 4),
+              Sequence.create('seq2/1', 'A' * 5),
+              Sequence.create('seq3/1', 'A' * 3),
+              Sequence.create('seq3/2', 'A' * 5)]
+
+    x, n, m = gather(create_fastx(reads), force_single=True)
+
+    expected = [('seq1/1', None),
+                ('seq1/2', None),
+                ('seq2/1', None),
+                ('seq3/1', None),
+                ('seq3/2', None)]
+    assert x == expected, x
+    assert m == 5
+    assert n == 4, n
+
+
+def test_BrokenPairedReader_OnPairs_filter_length_require_paired(create_fastx):
+    reads = [Sequence.create('seq1/1', 'A' * 5),
+              Sequence.create('seq1/2', 'A' * 4),
+              Sequence.create('seq3/1', 'A' * 3),
+              Sequence.create('seq3/2', 'A' * 5)]
+
+
+    x, n, m = gather(create_fastx(reads), min_length=4, require_paired=True)
+
+    expected = [('seq1/1', 'seq1/2')]
+    assert x == expected, x
+    assert m == 1
+    assert n == 0, n
+
+
+def test_BrokenPairedReader_OnPairs_filter_length_require_paired_2(create_fastx):
+    reads = [Sequence.create('seq1/1', 'A' * 5),
+              Sequence.create('seq1/2', 'A' * 4),
+              Sequence.create('seq3/1', 'A' * 5),
+              Sequence.create('seq3/2', 'A' * 3)]
+    x, n, m = gather(create_fastx(reads), min_length=4, require_paired=True)
+
+    expected = [('seq1/1', 'seq1/2')]
+    assert x == expected, x
+    assert m == 1
+    assert n == 0, n
+
+
+def test_BrokenPairedReader_OnPairs_filter_length_required_paired_3(create_fastx):
+    reads = [Sequence.create('seq1/1', 'A' * 5),
+              Sequence.create('seq1/2', 'A' * 4),
+              Sequence.create('seq3/1', 'A' * 3),
+              Sequence.create('seq3/2', 'A' * 3)]
+    x, n, m = gather(create_fastx(reads), min_length=4, require_paired=True)
+
+    expected = [('seq1/1', 'seq1/2')]
+    assert x == expected, x
+    assert m == 1
+    assert n == 0, n
+
+
+def test_BrokenPairedReader_OnPairs_filter_length_require_paired_4(create_fastx):
+    reads = [Sequence.create('seq1/1', 'A' * 3),
+              Sequence.create('seq1/2', 'A' * 3),
+              Sequence.create('seq3/1', 'A' * 5),
+              Sequence.create('seq3/2', 'A' * 5)]
+
+    x, n, m = gather(create_fastx(reads), min_length=4, require_paired=True)
+
+    expected = [('seq3/1', 'seq3/2')]
+    assert x == expected, x
+    assert m == 1
+    assert n == 0, n
+
+'''
+def test_BrokenPairedReader_lowercase():
+    reads = [Sequence.create('seq1/1', 'acgtn'),
+              Sequence.create('seq1/2', 'AcGtN'),
+              Sequence.create('seq1/2', 'aCgTn')]
+
+    results = []
+    parser = FastxParser(create_fastx(reads))
+    for num, is_pair, read1, read2 in broken_paired_reader(parser):
+        results.append((read1, read2))
+
+    a, b = results[0]
+    assert a.sequence == 'acgtn'
+    assert a.cleaned_seq == 'ACGTA'
+    assert b.sequence == 'AcGtN'
+    assert b.cleaned_seq == 'ACGTA'
+
+    c, d = results[1]
+    assert c.sequence == 'aCgTn'
+    assert c.cleaned_seq == 'ACGTA'
+    assert d is None
+'''
