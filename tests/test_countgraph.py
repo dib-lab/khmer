9959259247576add159f55932f0534c667329801
Tim Head <betatim@gmail.com>
2016-12-08 13:49:52 +0100

    Add parametrised tests instead of for loops

--- tests/test_countgraph.py
+++ tests/test_countgraph.py
@@ -614,68 +614,59 @@ def test_get_kmers():
 
 
 @pytest.mark.huge
-def test_save_load_large():
-    def do_test(ctfile):
-        inpath = utils.get_test_data('random-20-a.fa')
-        savepath = utils.get_temp_filename(ctfile)
+@pytest.mark.parametrize("ctfile", ['temp.ct', 'temp.ct.gz'])
+def test_save_load_large(ctfile):
+    inpath = utils.get_test_data('random-20-a.fa')
+    savepath = utils.get_temp_filename(ctfile)
 
-        sizes = khmer.get_n_primes_near_x(1, 2 ** 31 + 1000)
+    sizes = khmer.get_n_primes_near_x(1, 2 ** 31 + 1000)
 
-        orig = khmer._Countgraph(12, sizes)
-        orig.consume_fasta(inpath)
-        orig.save(savepath)
+    orig = khmer._Countgraph(12, sizes)
+    orig.consume_fasta(inpath)
+    orig.save(savepath)
 
-        loaded = khmer.load_countgraph(savepath)
+    loaded = khmer.load_countgraph(savepath)
 
-        orig_count = orig.n_occupied()
-        loaded_count = loaded.n_occupied()
-        assert orig_count == 3966, orig_count
-        assert loaded_count == orig_count, loaded_count
-
-    for ctfile in ['temp.ct.gz', 'temp.ct']:
-        do_test(ctfile)
+    orig_count = orig.n_occupied()
+    loaded_count = loaded.n_occupied()
+    assert orig_count == 3966, orig_count
+    assert loaded_count == orig_count, loaded_count
 
 
-def test_save_load_occupied():
-    def do_test(ctfile):
-        print('working with', ctfile)
-        inpath = utils.get_test_data('random-20-a.fa')
-        savepath = utils.get_temp_filename(ctfile)
+@pytest.mark.parametrize("ctfile", ['temp.ct', 'temp.ct.gz'])
+def test_save_load_occupied(ctfile):
+    print('working with', ctfile)
+    inpath = utils.get_test_data('random-20-a.fa')
+    savepath = utils.get_temp_filename(ctfile)
 
-        orig = khmer.Countgraph(12, 1e5, 4)
-        orig.consume_fasta(inpath)
-        orig.save(savepath)
+    orig = khmer.Countgraph(12, 1e5, 4)
+    orig.consume_fasta(inpath)
+    orig.save(savepath)
 
-        loaded = khmer.load_countgraph(savepath)
+    loaded = khmer.load_countgraph(savepath)
 
-        orig_count = orig.n_occupied()
-        loaded_count = loaded.n_occupied()
-        assert orig_count == 3886, orig_count
-        assert loaded_count == orig_count, loaded_count
-
-    for ctfile in ['temp.ct', 'temp.ct.gz']:
-        do_test(ctfile)
+    orig_count = orig.n_occupied()
+    loaded_count = loaded.n_occupied()
+    assert orig_count == 3886, orig_count
+    assert loaded_count == orig_count, loaded_count
 
 
-def test_save_load_occupied_small():
-    def do_test(ctfile):
-        print('working with', ctfile)
-        inpath = utils.get_test_data('random-20-a.fa')
-        savepath = utils.get_temp_filename(ctfile)
+@pytest.mark.parametrize("ctfile", ['temp.ct', 'temp.ct.gz'])
+def test_save_load_occupied_small(ctfile):
+    print('working with', ctfile)
+    inpath = utils.get_test_data('random-20-a.fa')
+    savepath = utils.get_temp_filename(ctfile)
 
-        orig = khmer.SmallCountgraph(12, 1e5, 4)
-        orig.consume_fasta(inpath)
-        orig.save(savepath)
+    orig = khmer.SmallCountgraph(12, 1e5, 4)
+    orig.consume_fasta(inpath)
+    orig.save(savepath)
 
-        loaded = khmer.load_countgraph(savepath, small=True)
+    loaded = khmer.load_countgraph(savepath, small=True)
 
-        orig_count = orig.n_occupied()
-        loaded_count = loaded.n_occupied()
-        assert orig_count == 3881, orig_count
-        assert loaded_count == orig_count, loaded_count
-
-    for ctfile in ['temp.ct', 'temp.ct.gz']:
-        do_test(ctfile)
+    orig_count = orig.n_occupied()
+    loaded_count = loaded.n_occupied()
+    assert orig_count == 3881, orig_count
+    assert loaded_count == orig_count, loaded_count
 
 
 def test_save_load():
@@ -791,15 +782,12 @@ def test_save_load_gz():
     assert x == y, (x, y)
 
 
-def test_load_empty_files():
-    def do_load_ct(fname):
-        with pytest.raises(OSError):
-            khmer.load_countgraph(fname)
-
+@pytest.mark.parametrize("ext", ['', '.gz'])
+def test_load_empty_files(ext):
     # Check empty files, compressed or not
-    for ext in ['', '.gz']:
-        fn = utils.get_test_data('empty-file' + ext)
-        do_load_ct(fn)
+    fname = utils.get_test_data('empty-file' + ext)
+    with pytest.raises(OSError):
+        khmer.load_countgraph(fname)
 
 
 def test_trim_full():
