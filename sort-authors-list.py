0092b72f06a33111c0c0d853d6bb4999d85b89ae
Tim Head <betatim@gmail.com>
2017-02-27 17:04:05 +0100

    Switch shebang line to python3

--- sort-authors-list.py
+++ sort-authors-list.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # This file is part of khmer, https://github.com/dib-lab/khmer/, and is
 # Copyright (C) 2015, The Regents of the University of California.
 #
