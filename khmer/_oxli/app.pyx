c98a3312043d75260049215639e561e66a9a2ec1
Camille Scott <camille.scott.w@gmail.com>
2017-03-01 17:46:18 -0800

    Change output format

--- khmer/_oxli/app.pyx
+++ khmer/_oxli/app.pyx
@@ -1,6 +1,10 @@
+# -*- coding: UTF-8 -*-
+# cython: c_string_type=unicode, c_string_encoding=utf8
+
 from __future__ import print_function
 import argparse
 import itertools
+import json
 import os
 import sys
 
@@ -17,6 +21,7 @@ from partitioning import StreamingPartitioner, Component
 
 from parsing cimport BrokenPairedReader, SplitPairedReader, FastxParser, Sequence
 from parsing import BrokenPairedReader, SplitPairedReader, FastxParser, Sequence
+from utils cimport _bstring
 
 def grouper(n, iterable):
     iterable = iter(iterable)
@@ -26,7 +31,7 @@ cdef class PartitioningApp:
 
     def __init__(self, args=sys.argv[1:]):
         self.args = self.parse_args(args)
-        self.args.write_stats = self.args.stats_interval > 0
+        self.args.write_results = self.args.output_interval > 0
 
         self.graph = create_countgraph(self.args)
         self.partitioner = StreamingPartitioner(self.graph, tag_density=self.args.tag_density)
@@ -34,37 +39,53 @@ cdef class PartitioningApp:
     def parse_args(self, args):
         parser = build_counting_args(descr='Partition a sample',
                                      citations=['counting', 'SeqAn'])
-        parser.add_argument('--stats-dir', default='component-stats')
+        parser.add_argument('--output-dir', default='partitioned')
         parser.add_argument('samples', nargs='+')
-        parser.add_argument('--save', default=None)
+        parser.add_argument('--save', action='store_true', default=False)
         parser.add_argument('--pairing-mode', 
                             choices=['split', 'interleaved', 'single'],
                             default='split')
         parser.add_argument('-Z', dest='norm', default=10, type=int)
-        parser.add_argument('--stats-interval', default=0, type=int)
+        parser.add_argument('--output-interval', default=0, type=int)
         parser.add_argument('--tag-density', default=None, type=int)
         
         return parser.parse_args(args)
 
-    def write_components(self, folder, n, sample, new_kmers):
-        sample = os.path.basename(sample)
-        filename = os.path.join(folder,
-                                '{0}.{1}.stats.csv'.format(n, sample))
+    def write_results(self, folder, n, new_kmers):
+        filename = os.path.join(folder, '{0}.csv'.format(n))
         print('# {0}: {1} tags, {2} components.'.format(n, self.partitioner.n_tags, 
                                                         self.partitioner.n_components))
         print('  writing results to file -> {0}'.format(filename))
         self.partitioner.write_components(filename)
-        with open(os.path.join(folder, 'global-stats.csv'), 'a') as fp:
+        with open(os.path.join(folder, 'global.csv'), 'a') as fp:
             fp.write('{0}, {1}, {2}, {3}\n'.format(n, self.partitioner.n_components,
                                                  self.partitioner.n_tags, new_kmers))
 
+    def prep_results_dir(self):
+        try:
+            os.mkdir(self.args.output_dir)
+        except OSError as e:
+            pass
+
+        if self.args.save:
+            self.args.save = os.path.join(self.args.output_dir, 'partitioner')
+
+    def write_meta(self, n_sequences, n_kmers):
+        meta = {'samples': self.args.samples,
+                'pairing': self.args.pairing_mode,
+                'K': self.args.ksize,
+                'tag-density': self.args.tag_density,
+                'n_sequences': n_sequences,
+                'n_unique_kmers': n_kmers}
+        if self.args.save:
+            meta['partitioner'] = self.args.save
+
+        with open(os.path.join(self.args.output_dir, 'meta'), 'w') as fp:
+            json.dump(meta, fp, indent=4)
+
     def run(self):
 
-        if self.args.write_stats:
-            try:
-                os.mkdir(self.args.stats_dir)
-            except OSError as e:
-                pass
+        self.prep_results_dir()
 
         if self.args.pairing_mode == 'split':
             samples = list(grouper(2, self.args.samples))
@@ -75,9 +96,12 @@ cdef class PartitioningApp:
             samples = self.args.samples
         
         cdef int n
+        cdef int n_sequences = 0
         cdef bool paired
         cdef Sequence first, second
         cdef int new_kmers = 0
+        cdef int n_kmers = 0
+        cdef int print_interval = self.args.output_interval if self.args.write_results else 10000
         last = 0
         for group in samples:
             if self.args.pairing_mode == 'split':
@@ -92,22 +116,27 @@ cdef class PartitioningApp:
                 reader = BrokenPairedReader(FastxParser(group), min_length=self.args.ksize)
             for n, paired, first, second in reader:
 
-                if n % 10000 == 0:
+                if n % print_interval == 0:
                     print (n, self.partitioner.n_components, self.partitioner.n_tags)
-                if self.args.write_stats and n > 0 and n % self.args.stats_interval == 0:
-                    self.write_components(self.args.stats_dir, last+n, sample_name, new_kmers)
-                    new_kmers = 0
+                if self.args.write_results and n > 0 and n % self.args.output_interval == 0:
+                    self.write_results(self.args.output_dir, last+n, new_kmers)
+                    total_kmers += new_kmers
+                    n_kmers = 0
                 if paired:
                     new_kmers += self.partitioner.consume_pair(first.sequence,
-                                                  second.sequence)
+                                                               second.sequence)
                 else:
                     new_kmers += self.partitioner.consume(first.sequence)
             last = n
-            if self.args.write_stats:
-                self.write_components(self.args.stats_dir, last, sample_name, new_kmers)
+            n_sequences += last
+            if self.args.write_results:
+                self.write_components(self.args.output_dir, last, new_kmers)
+                n_kmers += new_kmers
                 new_kmers = 0
 
-        if self.args.save is not None:
+        if self.args.save:
             self.partitioner.save(self.args.save)
+
+        self.write_meta(n_sequences, n_kmers)
 
         return self.partitioner
