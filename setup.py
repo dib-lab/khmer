aa16ad5e5aa0ff82bce4417160aff0cc78d5b114
Daniel Standage <daniel.standage@gmail.com>
2017-09-13 10:09:03 -0700

    Oops, forgot this important detail

--- setup.py
+++ setup.py
@@ -299,7 +299,7 @@ SETUP_METADATA = \
         "include_package_data": True,
         "zip_safe": False,
         "classifiers": CLASSIFIERS,
-        "python_requires": '>=3.5'
+        "python_requires": '>=3.4'
     }
 
 
