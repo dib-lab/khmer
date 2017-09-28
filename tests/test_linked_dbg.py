bf16d16b84b4d95ac13db71b4e5fe6a730eb12ce
Camille Scott <camille.scott.w@gmail.com>
2017-09-28 15:43:25 -0700

    expand test to check count

--- tests/test_linked_dbg.py
+++ tests/test_linked_dbg.py
@@ -17,7 +17,7 @@ def teardown():
     utils.cleanup()
 
 
-def test_add_links_one_junction(right_tip_structure):
+def test_get_junctions_single(right_tip_structure):
     '''Should have no links. Need two junctions.
     '''
     graph, contig, L, HDN, R, tip = right_tip_structure
@@ -31,8 +31,17 @@ def test_add_links_one_junction(right_tip_structure):
     
     junctions = list(linker.get_junctions(contig))
     assert len(junctions) == 1
+    junction = junctions.pop()
+    assert junction['count'] == 1
+    assert junction['u'] == forward_hash(HDN, K)
+    assert junction['v'] == forward_hash(R, K)
 
-    print(junctions)
-
+    linker.add_links(contig)
+    links = list(linker.get_links(contig))
+    assert len(links) == 0
+    junctions = list(linker.get_junctions(contig))
+    assert len(junctions) == 1
+    junction = junctions.pop()
+    assert junction['count'] == 2
 
 
