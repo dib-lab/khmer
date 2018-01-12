92705814cbeed57167f1389c1841ae60db6139e7
Saghi Nojoomi <saghinojoomi@campus-048-222.ucdavis.edu>
2018-01-12 13:18:03 -0800

    Finally works

--- examples/python-api/mask.py
+++ examples/python-api/mask.py
@@ -5,7 +5,7 @@ import khmer
 d1 = "ATGTACGGGCATTACGATTACCGATGTAG"
 d2 = "ATGACCAAACTCATTACGATTAGATATAG"
 ksize = 5
-target_table_size = 5e8
+target_table_size = 5e5
 num_tables = 4
 bf = khmer.Nodetable(ksize, target_table_size, num_tables)
 bf.consume(d1)
@@ -16,10 +16,10 @@ for kmer in cms.get_kmers(d2):
 
 # If kmer is in both sequences it should not be in cms but in bf
 assert cms.get('CATTA') == 0
-assert bf.get('CATTA') == 0
+assert bf.get('CATTA') > 0
 # If kmer is in d1 but not d2 it should not be in cms but be in bf
 assert cms.get('ATGTA') == 0
-assert bf.get('ATGTA') == 0
+assert bf.get('ATGTA') > 0
 # If kmer is in d2 but not d1 it should be in cms and not in bf
 assert cms.get('TATAG') > 0
 assert bf.get('TATAG') == 0
