3471f86eeccfbdef05543d7278ff0c7fba572c3b
Julie Chow <julie.c.chow@gmail.com>
2018-01-11 19:01:51 -0800

    updated cms mask asserts

--- cms_w_mask.py
+++ cms_w_mask.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 
-
-# A demonstration of using khmer to populate a count-min sketch (cms) with a mask. 
-# Put all the kmers from dataset2 into cms except those that are shared with dataset1. 
-# Typically khmer accrues a small false positive rate in order to save substantially on
-# memory requirements. 
+# A demonstration of using khmer to populate a count-min sketch (cms)
+# with a mask.
+# Put all the kmers from dataset2 into cms except those that
+# are shared with dataset1.
+# Typically khmer accrues a small false positive rate in order to save
+# substantially on memory requirements.
 
 import khmer
 
@@ -18,17 +20,20 @@ dataset2 = 'CCTGCACCGACGTACGCTATGCTATTGAAGACCATTAGTAGGCTCACTCC'
 # create a bloom filter
 bloomfilter = khmer.Nodetable(ksize, target_table_size, num_tables)
 # load dataset1 and store all the kmers
-bloomfilter.consume(dataset1);
+bloomfilter.consume(dataset1)
 
 cms = khmer.Counttable(ksize, target_table_size, num_tables)
 
 # for every kmer in dataset2
 for kmer in cms.get_kmers(dataset2):
-	if bloomfilter.get(kmer) == 0: # kmers unique to cms
-		cms.consume(kmer)
+    if bloomfilter.get(kmer) == 0:  # kmers unique to cms
+        cms.consume(kmer)
 
 # this kmer is in dataset2 (cms), but not dataset1
-assert cms.get('CCTGCA')
+assert cms.get('CCTGCA') > 0
 
 # this kmer is in dataset1 (bloomfilter), but not dataset2
-assert bloomfilter.get('GCTGCA')
+assert bloomfilter.get('GCTGCA') > 0
+
+# this kmer is in both datasets, should not be in cms
+assert cms.get('GTACGC') == 0
