0473f6dc5578255817d77c3ab1de3735b2e344d7
Saghi Nojoomi <saghinojoomi@campus-048-222.ucdavis.edu>
2018-01-12 12:43:08 -0800

    additinal tests final changes

--- examples/python-api/mask.py
+++ examples/python-api/mask.py
@@ -13,4 +13,13 @@ cms = khmer.Counttable(ksize, target_table_size, num_tables)
 for kmer in cms.get_kmers(d2):
     if bf.get(kmer) == 0:
         cms.consume(kmer)
+
+# If kmer is in both sequences it should not be in cms but in bf
 assert cms.get('CATTA') == 0
+assert bf.get('CATTA') == 0
+# If kmer is in d1 but not d2 it should not be in cms but be in bf
+assert cms.get('ATGTA') == 0
+assert bf.get('ATGTA') == 0
+# If kmer is in d2 but not d1 it should be in cms and not in bf
+assert cms.get('TATAG') > 0
+assert bf.get('TATAG') == 0
