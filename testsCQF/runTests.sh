6918199d195ce46a808a1ce5d4e0f62e00bd541b
Tamer Mansour <drtamermansour@gmail.com>
2018-01-20 16:09:55 -0600

    Annotation of testing steps

--- testsCQF/runTests.sh
+++ testsCQF/runTests.sh
@@ -1,10 +1,11 @@
-NoKmers=1000000
-K=20
-NoUnseenKmers=10000
-outputPrefix=new
+## initialize the parameters needed to generate the test data 
+K=20                  ## k-mer size
+NoKmers=1000000       ## number of unique kmers used for the Unique and Zipifan datasets
+NoUnseenKmers=10000   ## number of unique kmers used to generate the Unseen dataset
+outputPrefix="cqfTest"
 
 ###DATASet Generation
-echo "Generate DataSet"
+echo "Generate test dataSets"
 python3 generateSeq.py $NoKmers $K  $NoUnseenKmers $outputPrefix
 python3 plotGoldHist.py $outputPrefix
 cat $outputPrefix.dat |uniq > $outputPrefix.uniq.dat
@@ -16,17 +17,19 @@ make
 
 
 ### CQF construction test
+echo ""
+echo "CQF Construction Test"
 ./cqfConstruction
 
 ### CQF UNIT TEST
 echo ""
-echo "Unit Test"
+echo "CQF Unit Test"
 ln -s ../tests ./
 py.test test_CQF.py
 
 ### Load Factor Test
 echo ""
-echo "Load Factor"
+echo "Load Factor Test"
 echo "M\tMaximum Number of Unique Kmers" > loadfactor1-10.res.tsv
 seq 1 10  | parallel --gnu -k "python3 testLoadFactorCQF.py $outputPrefix.uniq.dat  8192  {} 2>> LoadFactor1.log |tail -n1" >> loadfactor1-10.res.tsv
 echo "M\tMaximum Number of Unique Kmers" > loadfactor_log1-16.res.tsv
@@ -36,9 +39,11 @@ seq 1 16| awk '{print (2^$1)-1}' | parallel --gnu -k "python3 testLoadFactorCQF.
 echo ""
 echo "Accuracy Test"
 ##Exp1
+echo "Experiment 1 Quotient Filter Vs Bloom filter"
 python testSketchesAccuracy.py $outputPrefix.uniq.dat $outputPrefix.none.dat 2>> $outputPrefix.log > $outputPrefix.bloom_vs_cqf.tsv
 
 ##Exp2
+echo "Experiment 2 (CQF Vs Count-min sketch)"
 parallel --gnu  -k "python3 testPerfomance.py $outputPrefix {1} {2} 2>> $outputPrefix.log" :::     23 24 25 26 27 28  :::  --cqf --cm > $outputPrefix.result
 python3 plotPerformanceBoxPlot.py $outputPrefix
 
